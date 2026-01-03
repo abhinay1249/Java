c2db339fb2a3015347c9a148bbde4bdfb625c186
abhinay1249 <abhinaymarise@gmail.com>
2026-01-03 19:37:47 +0530

    Modifications

--- DSA/Arrays/1_LargestElement.java
+++ DSA/Arrays/1_LargestElement.java
@@ -1,7 +1,22 @@
-// ======================= Optimal Approach =======================================
+// Problem Statement: Given an array, we have to find the largest element in the array.
+
+import java.util.*;
 
 class Main {
+
+// ======================= Brute Force Approach =======================================
+
     static int largest(int[] arr){
+
+        Arrays.sort(arr);
+
+        return arr[arr.length-1];
+    }
+
+// ======================= Optimal Approach =======================================
+
+
+    static int largest_1(int[] arr){
         int n = arr.length;
         int largest = arr[0];
         
@@ -12,7 +27,10 @@ class Main {
     }
     public static void main(String[] args) {
         int [] arr = {2,5,1,32,10};
+        int [] arr_1 = {2,5,1,32,10};
         int result = largest(arr);
+        int result_1 = largest_1(arr);
         System.out.println(result);
+        System.out.println(result_1);
     }
 }
--- DSA/Arrays/2_SecondLargestOrSmallest.java
+++ DSA/Arrays/2_SecondLargestOrSmallest.java
@@ -1,3 +1,7 @@
+// Problem Statement: Given an array, 
+// find the second smallest and second largest element in the array. Print ‘-1’ in the event that either of them doesn’t exist.
+
+
 import java.util.*;
 
 class Main {
