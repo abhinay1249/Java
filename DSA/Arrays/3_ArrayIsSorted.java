0e55008961e457d6a404b92ce835238f03bff3c2
abhinay1249 <abhinaymarise@gmail.com>
2026-01-03 19:39:17 +0530

    Modifications

--- DSA/Arrays/3_ArrayIsSorted.java
+++ DSA/Arrays/3_ArrayIsSorted.java
@@ -1,3 +1,7 @@
+//Problem Statement: Given an array of size n, 
+// write a program to check if the given array is sorted in (ascending / Increasing / Non-decreasing) order or not. 
+// If the array is sorted then return True, Else return False.
+
 class Main {
 
     // ======================= Brute-Force Approach =======================================
--- DSA/Arrays/4_RemoveDuplicates.java
+++ DSA/Arrays/4_RemoveDuplicates.java
@@ -1,3 +1,10 @@
+//Problem Statement: Given an integer array sorted in non-decreasing order, 
+// remove the duplicates in place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+// If there are k elements after removing the duplicates, 
+// then the first k elements of the array should hold the final result. It does not matter what you leave beyond the first k elements.
+
+
 import java.util.*;
 
 class Main {
