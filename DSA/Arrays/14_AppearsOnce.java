6c5fc11bb8cc48b3111ebf29e4ddecc64a9ab7bc
abhinay1249 <abhinaymarise@gmail.com>
2026-01-05 19:24:55 +0530

    Arrays-14

---
+++ DSA/Arrays/14_AppearsOnce.java
@@ -0,0 +1,92 @@
+import java.util.*;
+
+class Main {
+    
+    // ======================= Brute Force Approach =======================================
+
+    static int appearsOnce(int[] nums){
+
+        int appearOnce = 0;
+        for(int i = 0 ; i < nums.length; i++){
+            int count = 0;
+            for(int j = 0; j < nums.length ; j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
+            }
+            if(count == 1){
+                appearOnce = nums[i];
+            }
+        }
+        return appearOnce;
+    }
+
+    // ======================= Brute Force Approach Using Array =======================================
+
+    static int appearsOnce_1(int[] nums){
+            
+        int appearOnce = 0;
+        int[] temp = new int[nums.length];
+        
+        for(int i = 0 ; i < nums.length ; i++){
+            for(int j = i+1 ; j < nums.length ; j++){
+                if(nums[i] == nums[j]){
+                    temp[i] = 1;
+                    temp[j] = 1;
+                    break;
+                }
+            }
+        }
+        
+        for(int i=0;i<temp.length;i++){
+            if(temp[i]==0){
+                appearOnce = nums[i];
+            } 
+        }
+        return appearOnce;
+    }
+    
+    // ======================= Better Approach Using MAP =======================================
+
+    static int appearsOnce_2(int[] nums){
+
+        int arrayLength = nums.length;
+        int appearOnce = 0;
+        Map<Integer,Integer> freqCount = new HashMap<>();
+    
+        for(int i = 0; i < arrayLength ; i++){
+            freqCount.put(nums[i],freqCount.getOrDefault(nums[i]    ,0)+1);
+        }
+    
+        for(Map.Entry<Integer,Integer> value : freqCount.entrySet()){
+            if(value.getValue() == 1){
+                 appearOnce = value.getKey();
+            }
+        }
+        return appearOnce;
+    }
+
+    // ======================= Optimal Approach  =======================================
+    
+    static int appearsOnce_3(int[] nums){
+        
+        int appearOnce = 0;
+        
+        for(int i = 0; i < nums.length ; i++){
+            appearOnce^=nums[i];
+        }
+        return appearOnce;
+    }   
+    
+    public static void main(String[] args) {
+        int[] nums = {1,1,2,2,3};
+        int result = appearsOnce(nums);
+        int result_1 = appearsOnce_1(nums);
+        int result_2 = appearsOnce_2(nums);
+        int result_3 = appearsOnce_3(nums);
+        System.out.println(result);
+        System.out.println(result_1);
+        System.out.println(result_2);
+        System.out.println(result_3);
+    }
+}
