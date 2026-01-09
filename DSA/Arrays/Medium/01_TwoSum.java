83e3b4b0796c4e8dce281b8d1126dbadb8c32932
abhinay1249 <abhinaymarise@gmail.com>
2026-01-09 07:21:56 +0530

    Arrays-M-2

--- DSA/Arrays/Medium/01_TwoSum.java
+++ DSA/Arrays/Medium/01_TwoSum.java
@@ -12,92 +12,92 @@ class Main {
 
 	// ======================= Brute Force Approach - Variant - 1 =======================================
     
-    static boolean twoSum(int[] nums, int target){
+    	static boolean twoSum(int[] nums, int target){
 	
-	    int sumOfNumbers = 0;
- 	    	
-	    for(int i = 0 ; i < nums.length ; i++){
-	    	for(int j = i+1 ; j < nums.length ; j++){
-	    		if(nums[i]+nums[j] == target){
-	    			return true;
+	    	int sumOfNumbers = 0;
+	
+	    	for(int i = 0 ; i < nums.length ; i++){
+	    		for(int j = i+1 ; j < nums.length ; j++){
+	    			if(nums[i]+nums[j] == target){
+	    				return true;
+	    			}
 	    		}
 	    	}
-	    }
-	    return false;
-    }
+	    	return false;
+    	}
 
 	// ======================= Brute Force Approach - Variant - 2 =======================================
     
-	static List<Integer> twoSum_1(int[] nums, int target){
-	    List<Integer> indices = new ArrayList<>();
-	    
-	    for(int i = 0 ; i < nums.length ; i++){
-	    	for(int j = i+1 ; j < nums.length ; j++){
-	    		if(nums[i]+nums[j] == target){
-	    			indices.add(i);
-	    			indices.add(j);
-	    			return indices;
-	    		}
-	    	}
-	    }
-	    indices.add(-1);
-	    indices.add(-1);
-	    return indices;
-    }
+		static List<Integer> twoSum_1(int[] nums, int target){
+		    List<Integer> indices = new ArrayList<>();
+
+		    for(int i = 0 ; i < nums.length ; i++){
+		    	for(int j = i+1 ; j < nums.length ; j++){
+		    		if(nums[i]+nums[j] == target){
+		    			indices.add(i);
+		    			indices.add(j);
+		    			return indices;
+		    		}
+		    	}
+		    }
+		    indices.add(-1);
+		    indices.add(-1);
+		    return indices;
+    	}
 
 	// ======================= Better Approach - 1 =======================================
     
-    static Boolean twoSum_2(int[] nums, int target){
-	
-	    Map<Integer, Integer> twoSumMap = new TreeMap<>();
-	    
-	    for(int i = 0 ; i < nums.length ; i++){
-	        if(twoSumMap.containsKey(target - nums[i])){
-	    		return true;
-	    	}else{
-	    	    twoSumMap.put(nums[i],i);
-	    	}
-	    }
-	    return false;
-    }
+    	static Boolean twoSum_2(int[] nums, int target){
+		
+		    Map<Integer, Integer> twoSumMap = new TreeMap<>();
+
+		    for(int i = 0 ; i < nums.length ; i++){
+		        if(twoSumMap.containsKey(target - nums[i])){
+		    		return true;
+		    	}else{
+		    	    twoSumMap.put(nums[i],i);
+		    	}
+		    }
+		    return false;
+    	}
 
 	// ======================= Better Approach - 1 and Optimal Approach -2 =======================================
     
 
-    static int[] twoSum_3(int[] nums, int target){
+    	static int[] twoSum_3(int[] nums, int target){
+		
+		    Map<Integer, Integer> twoSumMap = new TreeMap<>();
+
+		    for(int i = 0 ; i < nums.length ; i++){
+		        if(twoSumMap.containsKey(target - nums[i])){
+		    		return new int[]{twoSumMap.get(target-nums[i]),i};
+		    	}else{
+		    	    twoSumMap.put(nums[i],i);
+		    	}
+		    }
+		    return new int[]{-1,-1};
+    	}
 	
-	    Map<Integer, Integer> twoSumMap = new TreeMap<>();
-	    
-	    for(int i = 0 ; i < nums.length ; i++){
-	        if(twoSumMap.containsKey(target - nums[i])){
-	    		return new int[]{twoSumMap.get(target-nums[i]),i};
-	    	}else{
-	    	    twoSumMap.put(nums[i],i);
-	    	}
-	    }
-	    return new int[]{-1,-1};
-    }
-    
 	// ======================= Optimal Approach - 1 =======================================
     
 
-    static Boolean twoSum_4(int[] nums, int target){
-	    Arrays.sort(nums);
-	    int left = 0, right = nums.length-1;
-    
-	    while(left<right){
-	    	int sum = nums[left]+nums[right];
-    
-	    	if(sum == target){
-	    		return true;
-	    	}else if(sum<target){
-	    		left++;
-	    	}else{
-	    		right--;
-	    	}
-	    }
-	    return false;
-    }
+    	static Boolean twoSum_4(int[] nums, int target){
+		    Arrays.sort(nums);
+		    int left = 0, right = nums.length-1;
+	
+		    while(left<right){
+		    	int sum = nums[left]+nums[right];
+	
+		    	if(sum == target){
+		    		return true;
+		    	}else if(sum<target){
+		    		left++;
+		    	}else{
+		    		right--;
+		    	}
+		    }
+		    return false;
+    	}
     
     public static void main(String[] args) {
         int[] nums = {2,6,5,8,11};
---
+++ DSA/Arrays/Medium/02_Sort0,1,2.java
@@ -0,0 +1,88 @@
+// Problem Statement: Given an array nums consisting of only 0, 1, or 2. 
+// Sort the array in non-decreasing order. The sorting must be done in-place, without making a copy of the original array.
+
+
+import java.util.*;
+
+class Main {
+
+    // ======================= Brute Force Approach =======================================
+
+        static int[] sortZeroOneTwo(int[] nums){
+            Arrays.sort(nums);
+
+            for(int i = 0 ; i <nums.length;i++){
+                System.out.print(nums[i]+" ");
+            }
+            return nums;
+        }
+
+    // ======================= Better Approach  ============================================
+
+    static int[] sortZeroOneTwo_1(int[] nums){
+        int countOfZero = 0;
+        int countOfOne = 0;
+        int countOfTwo = 0;
+        
+        for(int i : nums){
+            if(i==0) countOfZero++;
+            else if(i==1) countOfOne++;
+            else countOfTwo++;
+        }
+        
+        for(int i = 0 ; i < countOfZero ; i++){
+            nums[i] = 0;
+        }
+        for(int i = countOfZero ; i < countOfZero+countOfOne ; i++){
+            nums[i] = 1;
+        }
+        for(int i = countOfZero+countOfOne ; i < nums.length ; i++){
+            nums[i] = 2;
+        }
+        for(int i = 0 ; i <nums.length;i++){
+            System.out.print(nums[i]+" ");
+        }
+       return nums; 
+    }
+
+    // ======================= Optimal Approach =======================================
+    
+    static void swap(int num1, int num2, int[] nums){
+	    int temp = nums[num1];
+	    nums[num1] = nums[num2];
+	    nums[num2] = temp;
+    }
+
+    static int[] sortZeroOneTwo_2(int[] nums){
+	
+	    int low = 0;
+	    int high = nums.length - 1;
+	    int mid = 0;
+    
+	    while(mid<=high){
+	    	if(nums[mid]==0){
+	    		swap(low,mid,nums);
+	    		low++;
+	    		mid++;
+	    	}else if(nums[mid]==1){
+	    		mid++;
+	    	}else{
+	    		swap(mid,high,nums);
+	    		high--;
+	    	}
+	    }
+        for(int i = 0 ; i <nums.length;i++){
+            System.out.print(nums[i]+" ");
+        }
+	    return nums;
+    }   
+    
+    public static void main(String[] args) {
+        int[] nums = {2,1,2,1,2,0,0,0,1};
+        sortZeroOneTwo(nums);
+        System.out.println();
+        sortZeroOneTwo_1(nums);
+        System.out.println();
+        sortZeroOneTwo_2(nums);
+    }
+}
