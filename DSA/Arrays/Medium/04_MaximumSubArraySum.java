9f3426f797b51649c2fd4096433ac8e527a84060
abhinay1249 <abhinaymarise@gmail.com>
2026-01-11 08:58:59 +0530

    Arrays-M-4-Optimal

--- DSA/Arrays/Medium/04_MaximumSubArraySum.java
+++ DSA/Arrays/Medium/04_MaximumSubArraySum.java
@@ -36,6 +36,27 @@ class Main {
                 return maxSum;
         }
 
+    // ======================= Optimal Approach [KADANE'S ALGORITHM] =======================
+
+        static int maxSubArraySum_2(int[] nums){
+	        int sum = 0;
+    	    int maxSum = Integer.MIN_VALUE;
+
+	        for(int i = 0 ; i < nums.length ; i++){
+
+		        sum+=nums[i];
+
+		        if(sum > maxSum){
+		    	    maxSum = sum;
+		        }
+
+		        if(sum < 0){
+		    	    sum = 0;
+	        	}
+	        }
+	        return maxSum;
+        }
+
     public static void main(String[] args) {
 
         int[] nums = {2, 3, 5, -2, 7, -4};
