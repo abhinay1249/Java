a7b13b7df47b847de84b780bfcfd9c24d1d83f9d
abhinay1249 <abhinaymarise@gmail.com>
2026-01-11 08:56:33 +0530

    Arrays-M-4

--- DSA/Arrays/Medium/04_MaximumSubArraySum.java
+++ DSA/Arrays/Medium/04_MaximumSubArraySum.java
@@ -2,23 +2,39 @@ class Main {
     
 
     // ======================= Brute Force Approach =======================================
+
+        static int maxSubArraySum(int[] nums){
+        
+	        int maxSum = Integer.MIN_VALUE;
     
-    static int maxSubArraySum(int[] nums){
-	
-	    int maxSum = Integer.MIN_VALUE;
-	
-	    for(int i = 0 ; i < nums.length ; i++){
-		    for(int j = i ; j < nums.length ; j++){
-			    int sum = 0;
-		
-			    for(int k = i ; k <= j ; k++){
-				    sum+=nums[k];
-			    }
-			    maxSum = Math.max(sum, maxSum);
-		    }
-	    }
-	    return maxSum;
-    }
+	        for(int i = 0 ; i < nums.length ; i++){
+	    	    for(int j = i ; j < nums.length ; j++){
+	    		    int sum = 0;
+    
+	    		    for(int k = i ; k <= j ; k++){
+	    			    sum+=nums[k];
+	    		    }
+	    		    maxSum = Math.max(sum, maxSum);
+	    	    }
+	        }
+	        return maxSum;
+        }
+
+	// ======================= Better Approach =======================================
+
+        static int maxSubArraySum_1(int[] nums){
+        
+	        int maxSum = Integer.MIN_VALUE;
+    
+	        for(int i = 0 ; i < nums.length ; i++){
+	            int sum = 0;
+	    	    for(int j = i ; j < nums.length ; j++){
+	    			    sum+=nums[j];
+	    			    maxSum = Math.max(sum, maxSum);
+	    		}
+            }   
+                return maxSum;
+        }
 
     public static void main(String[] args) {
 
