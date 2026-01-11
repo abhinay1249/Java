9f1218013c1ac25a90f32a5232f148db212893f1
abhinay1249 <abhinaymarise@gmail.com>
2026-01-11 08:54:36 +0530

    Initial Commit

---
+++ DSA/Arrays/Medium/04_MaximumSubArraySum.java
@@ -0,0 +1,34 @@
+class Main {
+    
+
+    // ======================= Brute Force Approach =======================================
+    
+    static int maxSubArraySum(int[] nums){
+	
+	    int maxSum = Integer.MIN_VALUE;
+	
+	    for(int i = 0 ; i < nums.length ; i++){
+		    for(int j = i ; j < nums.length ; j++){
+			    int sum = 0;
+		
+			    for(int k = i ; k <= j ; k++){
+				    sum+=nums[k];
+			    }
+			    maxSum = Math.max(sum, maxSum);
+		    }
+	    }
+	    return maxSum;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {2, 3, 5, -2, 7, -4};
+        int[] nums_1 = {-2,1};
+        int result = maxSubArraySum(nums);
+        int result_1 = maxSubArraySum_1(nums);
+        int result_2 = maxSubArraySum_2(nums_1);
+        System.out.println(result);
+        System.out.println(result_1);
+        System.out.println(result_2);
+    }
+}
