ae9281070abc9427c31f049bc77f862a4ba90ae4
abhinay1249 <abhinaymarise@gmail.com>
2026-01-15 14:02:59 +0530

    Initial Commit

--- DSA/Arrays/Medium/06_RearrangePos&NegElements.java
+++ DSA/Arrays/Medium/06_RearrangePos&NegElements.java
@@ -1,3 +1,7 @@
+// Problem Statement: There’s an array ‘A’ of size ‘N’ with an equal number of positive and negative elements. 
+// Without altering the relative order of positive and negative elements, you must return an array of alternately positive and negative values.
+
+
 import java.util.*;
 
 class Main {
