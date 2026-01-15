33d6fcb36f2dde9cb3703357d27148eba4ad73e9
abhinay1249 <abhinaymarise@gmail.com>
2026-01-15 14:53:27 +0530

    Arrays-7-M

---
+++ DSA/Arrays/Medium/07_Next_Permutation.java
@@ -0,0 +1,121 @@
+// Problem Statement: Given an array Arr[] of integers, rearrange the numbers of the given array into the lexicographically next greater permutation of numbers.
+// If such an arrangement is not possible, it must rearrange to the lowest possible order (i.e., sorted in ascending order).
+
+
+import java.util.*;
+
+class Main {
+        
+    // ======================= Brute Force Approach =======================================
+
+        static List<Integer> permute(int[] nums){
+        
+            List<List<Integer>> ans = new ArrayList<>();
+        
+            // Arrays.sort(nums);  --> Ony if the array is the first permutation this should be used.
+        
+            nextPermutation(nums,0,ans);
+        
+            List<Integer> current = new ArrayList<>();
+        
+            for(int i : nums) current.add(i);
+        
+            for(int i = 0 ; i < ans.size() ; i++){
+                if(ans.get(i).equals(current)){                 // T.C = O(N! * N), S.C = O(N!)  
+                    if(i == ans.size()-1){
+                        return ans.get(0);
+                    }
+                    return ans.get(i+1);
+                }
+            }
+            return current;
+        }
+    
+        private static void swapNum(int left, int right, int[] nums){
+        
+	        int temp = nums[left];
+	        nums[left] = nums[right];
+	        nums[right] = temp;
+        }
+
+        private static void nextPermutation(int[] nums,int index,List<List<Integer>> ans){
+
+            int n = nums.length;
+
+            if(index == n){
+
+                List<Integer> ds = new ArrayList<>();
+
+                for(int i = 0 ; i < n ; i++){
+                    ds.add(nums[i]);    
+                }
+                ans.add(new ArrayList<>(ds));
+                return;
+            }
+
+            for(int i = index ; i < n ; i++){
+                swapNum(i,index,nums);
+                nextPermutation(nums,index+1,ans);
+                swapNum(i,index,nums);
+            }
+        }
+
+    // ======================= Optimal Approach =======================================
+
+        private static void reverse(int start, int end, int[] nums){
+        
+	        while(start < end){
+                swap(start, end, nums);
+	    	    start++;
+	    	    end--;
+	        }
+        }
+
+        private static void swap(int left, int right, int[] nums){
+        
+	        int temp = nums[left];
+	        nums[left] = nums[right];                                           // T.C = O(N! * N), S.C = O(1) 
+	        nums[right] = temp;
+        }
+
+        static int[] nextPermutation(int[] nums){
+        
+	        int n = nums.length;
+	        int index = -1;
+
+	        for(int i = n-2 ; i >= 0 ; i--){
+            
+	    	    if(nums[i] < nums[i+1]){
+	    		    index = i;
+	    		    break;
+	    	    }
+	        }
+
+	        if(index == -1){
+	    	    reverse(0,n-1,nums);
+	    	    return nums;
+	        }
+
+	        for(int i = n-1 ; i > index ; i--){
+            
+	    	    if(nums[i]>nums[index]){
+                
+	    		    swap(i, index, nums);
+	    		    break;		
+	    	    }
+	        }
+	        reverse(index+1,n-1,nums);
+    
+	        return nums;
+        }
+
+
+    public static void main(String[] args) {
+        int[] nums = {2,1,5,4,3,0,0};
+        int[] res = nextPermutation(nums);
+        for(int i : res){
+            System.out.print(i+" ");  
+        }
+
+    }
+}
