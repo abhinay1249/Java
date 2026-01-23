c2ce570e940f8f03288fc1a3c2e2807e0bc93e49
abhinay1249 <abhinaymarise@gmail.com>
2026-01-23 19:13:47 +0530

    Arrays-H-1

--- DSA/Arrays/Hard/01_PascalTriangle.java
+++ DSA/Arrays/Hard/01_PascalTriangle.java
@@ -40,26 +40,11 @@ class Main {
 
     // ==================================== Brute Force Approach ========================================
 
-        static void NthRowOfTraingle(int n){
-            for(int i = n-1 ; i >= 0 ; i--){
-                for(int j = 0 ; j < i ; j++){
-                    System.out.print("* ");
-                }
-                System.out.println();
-            }
-        }
+        
 
     // ==================================== Optimal Approach ===========================================
 
-        static void NthRowOfTraingle_1(int n){
-            int res = 1;
-            System.out.print(res+" ");
-            for(int i = n-1 ; i > 0 ; i--){
-                res = res * i;
-                res= res / (n-i);
-                System.out.print(res+" ");
-            }
-        }
+
     public static void main(String[] args) {
         int n = 6;
         int row = 6;
