cd6bd017e2d041f031069dd6c95bd00e9e3d3d50
abhinay1249 <abhinaymarise@gmail.com>
2026-01-26 23:12:09 +0530

    Arrays-H-4 Brute Force Method

--- DSA/Arrays/Hard/04_FourSum.java
+++ DSA/Arrays/Hard/04_FourSum.java
@@ -1,25 +1,29 @@
 import java.util.*;
 
 class Main {
-    static List<List<Integer>> fourSum(int[] nums, int target){
-        int n = nums.length;
-        Set<List<Integer>> elementList = new HashSet<>();
-        
-        for(int i = 0 ; i < n-3 ; i++){
-            for(int j = i+1 ; j < n-2 ; j++){
-                for(int k = j+1 ;  k < n -1 ; k++){
-                    for(int l = k+1 ; l < n ; l++){
-                        if(nums[i]+nums[j]+nums[k]+nums[l] == target){
-                            List<Integer> elements = Arrays.asList(nums[i],nums[j],nums[k],nums[l]);
-                            Collections.sort(elements);
-                            elementList.add(elements);
+
+    // ==================================== Brute Force Approach ===========================================
+    
+        static List<List<Integer>> fourSum(int[] nums, int target){
+            int n = nums.length;
+            Set<List<Integer>> elementList = new HashSet<>();
+
+            for(int i = 0 ; i < n-3 ; i++){
+                for(int j = i+1 ; j < n-2 ; j++){
+                    for(int k = j+1 ;  k < n -1 ; k++){
+                        for(int l = k+1 ; l < n ; l++){
+                            if(nums[i]+nums[j]+nums[k]+nums[l] == target){
+                                List<Integer> elements = Arrays.asList(nums[i],nums[j],nums[k],nums[l]);
+                                Collections.sort(elements);
+                                elementList.add(elements);
+                            }
                         }
                     }
                 }
             }
+            return new ArrayList<>(elementList);
         }
-        return new ArrayList<>(elementList);
-    }
+
     public static void main(String[] args) {
         int[] nums = {4,3,3,4,4,2,1,2,1,1};
         int target = 9;
