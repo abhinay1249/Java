415db1f1fc30849773810dadcabe488e14b7b92c
abhinay1249 <abhinaymarise@gmail.com>
2026-01-26 11:33:18 +0530

    Arrays-H-3 Brute Force Method

--- DSA/Arrays/Hard/03_ThreeSum.java
+++ DSA/Arrays/Hard/03_ThreeSum.java
@@ -6,16 +6,18 @@ import java.util.*;
 
 class Main {
     
+    // ==================================== Brute Force Approach ===========================================
+
         static List<List<Integer>> threeSum(int[] nums){
         
             int length =  nums.length;
-    
+
             Set<List<Integer>> unqTriplets = new HashSet<>();
-    
+
             for(int index_1 = 0 ; index_1 < length - 2 ; index_1++){
                 for(int index_2 = index_1+1 ; index_2 < length - 1 ; index_2++){
-                    for(int index_3 = index_2+1 ; index_3 < length ; index_3++){
-	                    if(nums[index_1]+nums[index_2]+nums[index_3] == 0){
+                    for(int index_3 = index_2+1 ; index_3 < length ; index_3++){         // T.C = O(N^3 * log(no of unique triplets))
+	                    if(nums[index_1]+nums[index_2]+nums[index_3] == 0){              // S.C = 2 * O(no of triplets)
 	    	                    List<Integer> triplets = Arrays.asList(nums[index_1],nums[index_2],nums[index_3]);
 	    	                    Collections.sort(triplets);
 	                        	unqTriplets.add(triplets);
