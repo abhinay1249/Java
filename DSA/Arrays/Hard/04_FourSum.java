fffeadd82c6c735833e7d8fdf5a8b74825b7b573
abhinay1249 <abhinaymarise@gmail.com>
2026-01-26 23:10:44 +0530

    Arrays-H-4 Brute Force Method

---
+++ DSA/Arrays/Hard/04_FourSum.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Main {
+    static List<List<Integer>> fourSum(int[] nums, int target){
+        int n = nums.length;
+        Set<List<Integer>> elementList = new HashSet<>();
+        
+        for(int i = 0 ; i < n-3 ; i++){
+            for(int j = i+1 ; j < n-2 ; j++){
+                for(int k = j+1 ;  k < n -1 ; k++){
+                    for(int l = k+1 ; l < n ; l++){
+                        if(nums[i]+nums[j]+nums[k]+nums[l] == target){
+                            List<Integer> elements = Arrays.asList(nums[i],nums[j],nums[k],nums[l]);
+                            Collections.sort(elements);
+                            elementList.add(elements);
+                        }
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(elementList);
+    }
+    public static void main(String[] args) {
+        int[] nums = {4,3,3,4,4,2,1,2,1,1};
+        int target = 9;
+        List<List<Integer>> res = fourSum(nums,target);
+        for(List<Integer> num: res){
+            System.out.println(num);
+        }
+    }
+}
