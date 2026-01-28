8591bcadb7d605d50cb58df380dfe92a44152819
abhinay1249 <abhinaymarise@gmail.com>
2026-01-28 23:50:49 +0530

    Arrays-H-5

--- DSA/Arrays/Hard/05_LongestSubArrayWithSumZero.java
+++ DSA/Arrays/Hard/05_LongestSubArrayWithSumZero.java
@@ -21,9 +21,38 @@ class Main {
             return maxLength;
         }
 
+
+    // ==================================== Optimal Approach ===========================================
+
+        static int longestSubArrayOfSumZero_1(int[] nums){
+	        int length = nums.length;
+	        Map<Integer,Integer> prefixCount = new TreeMap<>();
+	        int sum = 0;
+	        int prefixSum = 0;
+	        int target = 0;
+	        int maxLength = -1;
+    
+            if(length == 0) return -1;
+	        for(int index = 0 ; index < length ; index++){
+	            prefixSum+=nums[index];                             // T.C = O(N * Log N), S.C = O(N)
+	            sum = prefixSum - target;
+	            if(sum == 0){
+		            maxLength = Math.max(maxLength,index+1); 
+                }else if(prefixCount.containsKey(sum)){
+		            maxLength = Math.max(maxLength,index - prefixCount.get(sum)); 
+                }
+                if(!prefixCount.containsKey(prefixSum)){
+	                prefixCount.put(prefixSum,index);
+                }
+            }
+            return maxLength;
+        }
+
     public static void main(String[] args) {
         int[] nums = {1,2,-3,0,0,0,0,4,-4};
         int result = longestSubArrayOfSumZero(nums);
+        int result_1 = longestSubArrayOfSumZero_1(nums);
         System.out.println(result);
+        System.out.println(result_1);
     }
 }
