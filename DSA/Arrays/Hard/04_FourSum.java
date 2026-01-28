814b913cac3f7f72a1fb7d6a5f0f8c618f054b16
abhinay1249 <abhinaymarise@gmail.com>
2026-01-28 08:21:05 +0530

    Arrays-H-4

--- DSA/Arrays/Hard/04_FourSum.java
+++ DSA/Arrays/Hard/04_FourSum.java
@@ -1,3 +1,7 @@
+// Problem Statement: Given an array of N integers, your task is to find unique quads that add up to give a target value. 
+// In short, you need to return an array of all the unique quadruplets [arr[a], arr[b], arr[c], arr[d]] such that their sum is equal to a given target.
+
+
 import java.util.*;
 
 class Main {
