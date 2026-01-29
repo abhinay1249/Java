552c0d7c3104a20b8377a03fb17e75f589696967
abhinay1249 <abhinaymarise@gmail.com>
2026-01-29 23:27:04 +0530

    Arrays-H-5

--- DSA/Arrays/Hard/05_LongestSubArrayWithSumZero.java
+++ DSA/Arrays/Hard/05_LongestSubArrayWithSumZero.java
@@ -1,3 +1,6 @@
+// Problem Statement: Given an array containing both positive and negative integers, 
+// we have to find the length of the longest subarray with the sum of all elements equal to zero.
+
 
 class Main {
     
