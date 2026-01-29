b65a454c79e27a0488c6bb74f18e1867e34ebd6e
abhinay1249 <abhinaymarise@gmail.com>
2026-01-29 23:23:42 +0530

    Arrays-H-6 Better Method

--- DSA/Arrays/Hard/06_CountSubArrayForXorK.java
+++ DSA/Arrays/Hard/06_CountSubArrayForXorK.java
@@ -21,6 +21,25 @@ class Main {
             }
             return count;
         }
+
+    // ==================================== Better Approach ===========================================
+
+        static int subArraysOfXor_1(int[] nums,int target){
+            int length = nums.length;
+            int count = 0;
+
+            for(int idx_1 = 0 ; idx_1 < length ; idx_1++){
+                int xor = 0;
+                for(int idx_2 = idx_1 ; idx_2 < length ; idx_2++){
+                    xor^=nums[idx_2];
+                    if(xor == target){
+                        count++;
+                    }
+                }
+
+            }
+            return count;
+        }
     
     public static void main(String[] args) {
         int[] nums = {4,2,2,6,4};
