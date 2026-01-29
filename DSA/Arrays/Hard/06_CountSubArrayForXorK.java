7371f60432310354922c3089b482298611d977ad
abhinay1249 <abhinaymarise@gmail.com>
2026-01-29 23:22:44 +0530

    Arrays-H-6-Brute-Force Method

---
+++ DSA/Arrays/Hard/06_CountSubArrayForXorK.java
@@ -0,0 +1,31 @@
+class Main {
+
+
+    // ==================================== Brute Force Approach ===========================================
+
+        static int subArraysOfXor(int[] nums, int target){
+
+	        int length = nums.length;
+	        int count = 0;
+    
+	        for(int idx_1 = 0; idx_1 < length ; idx_1++){
+	    	    for(int idx_2 = idx_1; idx_2 < length ; idx_2++){
+	    		    int xor = 0;
+	    		    for(int values = idx_1 ; values <= idx_2 ; values++){
+	    			    xor ^= nums[values];
+	    			    if(xor == target){
+	    				    count++;
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+    
+    public static void main(String[] args) {
+        int[] nums = {4,2,2,6,4};
+        int target = 6;
+        int res = subArraysOfXor_1(nums,target);
+        System.out.println(res);
+    }
+}
