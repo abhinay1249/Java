07d06591b0229879bb00c5ddb9c43e4175d8e791
abhinay1249 <abhinaymarise@gmail.com>
2026-02-01 17:18:49 +0530

    Arrays-H-8

--- DSA/Arrays/Hard/08_MergeSortedArrays.java
+++ DSA/Arrays/Hard/08_MergeSortedArrays.java
@@ -4,30 +4,63 @@
 
 import java.util.*;
 
-class Main {
+class Main{
 
     // ==================================== Brute Force Approach ===========================================
 
         static int[] mergeTwoSortedArray(int[] nums1, int[] nums2, int m , int n){
         
             for(int idx = 0 ; idx < n ; idx++){
-                nums1[m+i] = nums2[i];
+                nums1[m+idx] = nums2[idx];
             }                                                           //T.C = O(M)+ O(M LOG M), S.C = O(1)
 
             Arrays.sort(nums1);
 
             return nums1;
+            // return nums2;
     
         }
     
     // ==================================== Optimal Approach ===========================================
 
+        static int[] mergeTwoSortedArray_1(int[] nums1,int[] nums2, int m,int n) {
+
+            if(n==0){
+                return nums1;
+            }
+
+            int left = m-1;
+            int right = n-1;
+            int index = (m+n)-1;
+
+            while(left>=0 && right >=0){
+                if(nums1[left]<nums2[right]){
+                    nums1[index] = nums2[right];
+                    right--;
+                    index--;
+                }else if(nums1[left]>=nums2[right]){
+                    nums1[index] = nums1[left];
+                    index--;
+                    left--;
+                }
+            } 
+
+            while(right>=0){
+                nums1[index] = nums2[right];
+                index--;
+                right--;
+            }
+            return nums1;
+            // return nums2;
+        }
+
+
     public static void main(String[] args) {
         int[] nums1 = {0, 2, 7, 8, 0, 0, 0};
         int[] nums2 = {-7, -3, -1};
-        int m = nums1.length;
-        int n = nums2.length;
-        int[] result = mergeTwoSortedArray(nums1,nums2,m,n);
+        int m = 4;
+        int n = 3;
+        int[] result = mergeTwoSortedArray_1(nums1,nums2,m,n);
         for(int values: result){
             System.out.print(values+" ");
         }
