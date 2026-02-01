86b742dd10ceae4bae2d36b7f857a2dbf9f77178
abhinay1249 <abhinaymarise@gmail.com>
2026-02-01 14:23:18 +0530

    Arrays-H-8 Brute Force

---
+++ DSA/Arrays/Hard/08_MergeSortedArrays.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+class Main {
+    static int[] mergeTwoSortedArray(int[] nums, int[] nums1, int m , int n){
+	    
+	    int left = 0 ;
+	    int right = 0;
+	    int index = 0;
+	    int[] result = new int[m+n];
+	    
+	    while(left< m && right < n){
+	        if(nums[left]<nums1[right]){
+	            result[index] = nums[left];
+	            index++;
+	            left++;
+	        }else{
+	            result[index] = nums1[right];
+	            index++;
+	            right++;
+	        }
+	    }
+	    
+	    while(left < m){
+	        result[index++] = nums[left++];
+	    }
+	    while(right < n){
+	        result[index++] = nums1[right++];
+	    }
+	    
+	    for(int idx = 0 ; idx < m+n ; idx++){
+	        if(idx < m){
+	            nums[idx] = result[idx];
+	        }else{
+	            nums1[idx-m] = result[idx];
+	        }
+	    }
+	    return nums1;
+	   // return nums1;
+    }
+    
+    
+
+    public static void main(String[] args) {
+        int[] nums = {1,3,5,7};
+        int[] nums1 = {0,2,6,8,9};
+        int m = nums.length;
+        int n = nums1.length;
+        int[] result = mergeTwoSortedArray_1(nums,nums1,m,n);
+        for(int values: result){
+            System.out.print(values+" ");
+        }
+        
+    }
+}
