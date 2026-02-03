5ea71ae4dc5b754778cffda2a90499c23838ed27
abhinay1249 <abhinaymarise@gmail.com>
2026-02-03 23:49:01 +0530

    Arrays-H-10

--- DSA/Arrays/Hard/10_MissingandRepeatedNumber.java
+++ DSA/Arrays/Hard/10_MissingandRepeatedNumber.java
@@ -1,3 +1,9 @@
+// Problem Statement: Given an integer array nums of size n containing values from [1, n] and each value appears exactly once in the array, except for A, which appears twice and B which is missing.
+// Return the values A and B, as an array of size 2, where A appears in the 0-th index and B in the 1st index.
+
+// Note: You are not allowed to modify the original array.
+
+
 import java.util.*;
 
 class Main {
