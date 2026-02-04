6ed402ffebdc03cc872cfb0545baecc9bbd11dae
abhinay1249 <abhinaymarise@gmail.com>
2026-02-04 10:51:42 +0530

    Arrays-H-11

--- DSA/Arrays/Hard/11_CountInversions.java
+++ DSA/Arrays/Hard/11_CountInversions.java
@@ -21,6 +21,61 @@ class Main {
             }
             return count;
         }
+    // ==================================== Optimal Approach ===========================================
+
+        static int merge(int[] nums, int low, int mid, int high){
+	        List<Integer> temp = new ArrayList<>();
+	        int count = 0;
+	        int length = nums.length;
+	        int left = low;
+	        int right = mid+1;
+
+	        while(left<=mid && right<=high){
+	    	    if(nums[left] <= nums[right]){
+	    		    temp.add(nums[left]);
+	    		    left++;
+                }else if(nums[left]>nums[right]){
+	                temp.add(nums[right]);
+	                count+=(mid-left+1);
+	                right++;                                             // T.C = O(N LOG N), S.C = O(N) 
+                }
+            }
+
+            while(left<=mid){
+	            temp.add(nums[left]);
+	            left++;
+            }
+
+            while(right<=high){
+	            temp.add(nums[right]);
+	            right++;
+            }
+
+            for(int idx = low ; idx <= high ; idx++){
+                nums[idx] = temp.get(idx - low);
+            }
+
+            return count;
+        }
+
+        static int mergeSort(int[] nums, int low, int high){
+	        int length = nums.length;
+	        int mid = low + ((high - low)/2);
+	        int count = 0;
+
+	        if(low >= high) return count;
+    
+	        count+=mergeSort(nums,low,mid);
+	        count+=mergeSort(nums,mid+1,high);
+	        count+=merge(nums,low,mid,high);
+
+            return count;
+        }
+
+        static int countInversions_1(int[]nums){
+	        int length = nums.length;
+	        return mergeSort(nums,0,length-1);
+        }
 
     public static void main(String[] args) {
         int[] nums = {5,3,2,1,4};
