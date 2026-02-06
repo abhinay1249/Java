18e00347f234aa181f4fc9688d85b5c29ef8d2f3
abhinay1249 <abhinaymarise@gmail.com>
2026-02-06 06:22:45 +0530

    Arrays-M-04

--- DSA/Arrays/Medium/04_MaximumSubArraySum.java
+++ DSA/Arrays/Medium/04_MaximumSubArraySum.java
@@ -37,11 +37,11 @@ class Main {
 	        for(int i = 0 ; i < nums.length ; i++){
 	            int sum = 0;
 	    	    for(int j = i ; j < nums.length ; j++){
-	    			    sum+=nums[j];
-	    			    maxSum = Math.max(sum, maxSum);
+	    			sum+=nums[j];
+	    			maxSum = Math.max(sum, maxSum);
 	    		}
             }   
-                return maxSum;
+            return maxSum;
         }
 
     // ======================= Optimal Approach [KADANE'S ALGORITHM] =======================
