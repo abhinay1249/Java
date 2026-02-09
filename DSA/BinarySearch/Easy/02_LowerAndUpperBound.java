d8e7ec2253ffbb246ad927491b3358799b0ac6c5
abhinay1249 <abhinaymarise@gmail.com>
2026-02-09 20:30:15 +0530

    Lower Bound and Upper Bound

--- DSA/BinarySearch/Easy/02_LowerAndUpperBound.java
+++ DSA/BinarySearch/Easy/02_LowerAndUpperBound.java
@@ -5,26 +5,46 @@ class Main {
     
     // ------------------------------ Lower Bound [Smallest Index] --------------------------------------
 
-    static int lowerBound(int[] nums, int target){
-        
-        int length = nums.length;
-        int low = 0, high = length - 1;
-        int ans = length;
-        
-        while(low <= high){                                             // T.C = O(log N)
-            int mid = low + ((high-low)/2);
-            
-            if(nums[mid] >= target){
-                ans = mid;
-                high = mid - 1;
-            }else{
-                low = mid+1;
+        static int lowerBound(int[] nums, int target){
+
+            int length = nums.length;
+            int low = 0, high = length - 1;
+            int ans = length;
+
+            while(low <= high){                                             // T.C = O(log N) S.C = O(1)
+                int mid = low + ((high-low)/2);
+
+                if(nums[mid] >= target){
+                    ans = mid;
+                    high = mid - 1;
+                }else{
+                    low = mid+1;
+                }
             }
+            return ans;
         }
-        return ans;
-    }
 
     // -------------------------------- Upper Bound [Smallest Index] --------------------------------------
+
+
+        static int upperBound(int[] nums, int target){
+        
+            int length = nums.length;
+            int low = 0, high = length - 1;
+            int ans = length;
+
+            while(low <= high){
+                int mid = low + ((high-low)/2);                                  // T.C = O(log N) S.C = O(1)
+
+                if(nums[mid] > target){
+                    ans = mid;
+                    high = mid - 1;
+                }else{
+                    low = mid+1;
+                }
+            }
+            return ans;
+        }
 
     public static void main(String[] args) {
         int[] nums = {1,3,4,5,7,9,10,13,15};
