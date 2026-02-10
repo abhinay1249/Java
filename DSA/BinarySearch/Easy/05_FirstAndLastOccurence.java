15eb365ce37c349b34687560194799efc3db2d0f
abhinay1249 <abhinaymarise@gmail.com>
2026-02-10 08:13:59 +0530

    BinarySearch-1DArray-5

--- DSA/BinarySearch/Easy/05_FirstAndLastOccurence.java
+++ DSA/BinarySearch/Easy/05_FirstAndLastOccurence.java
@@ -4,13 +4,13 @@ class Main {
 
     // ======================= Brute Force Approach =======================================
     
-        static int firstAnsLastOccurence(int[] nums, int target){
+        static int[] firstAnsLastOccurence(int[] nums, int target){
 
             int length = nums.length;
             int lastOccurence = -1;
             int firstOccurence = -1;
 
-            for(int index = 0 ; index < length ; index++){                  // T.C = O(N) S.C = O(1)
+            for(int index = 0 ; index < length ; index++){                  // T.C = O(2N) S.C = O(1)
                 if(nums[index] == target){
                     firstOccurence = index;
                     break;
@@ -25,12 +25,84 @@ class Main {
             }
             return new int[]{firstOccurence,lastOccurence};
         }
+    // ======================= Better Approach =======================================
+    
+        static int[] firstAnsLastOccurence(int[] nums, int target){
 
+            int length = nums.length;
+            int lastOccurence = -1;
+            int firstOccurence = -1;
 
+            for(int index = 0 ; index < length ; index++){                  // T.C = O(N) S.C = O(1)
+                if(nums[index] == target){
+                    if(firstOccurence == -1){
+                        firstOccurence = index;
+                    }
+                    lastOccurence = index;
+                }
+            }
+            return new int[]{firstOccurence,lastOccurence};
+        }
+
+    // ======================= Optimal Approach =======================================
+
+        static int lowerBound(int[] nums, int target){
+                
+            int length = nums.length;
+            int low = 0, high = length - 1;
+            int firstOccurence = length;
+
+            while(low <= high){
+                int mid = low + ((high - low)/2);
+
+                if(nums[mid]>=target){
+                    firstOccurence = mid;
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
+                }
+            }
+
+            return firstOccurence;
+        }
+
+        static int upperBound(int[] nums, int target){
+
+            int length = nums.length;
+            int low = 0, high = length - 1;
+            int lastOccurence = length;
+
+            while(low <= high){
+                int mid = low + ((high - low)/2);                           // T.C = 2*O(log N) S.C = O(1)
+
+                if(nums[mid]>target){
+                    lastOccurence = mid;
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
+                }
+            }
+
+            return lastOccurence;
+        }
+
+        static int[] firstAnsLastOccurence_2(int[] nums, int target){
+
+            int length = nums.length;
+            int first = lowerBound(nums,target);
+            if(first == length || nums[first]!=target){
+                return new int[]{-1,-1};
+            }
+            return new int[]{first,upperBound(nums,target)-1};
+        }
+    
     public static void main(String[] args) {
         int[] nums = {1,2,4,4,4,4,4,9,11};
         int target = 5;
-        int result = firstAnsLastOccurence(nums,target);
-        System.out.println(result);
+        int[] result = firstAndLastOccurence_2(nums,target);
+
+        for(int val : result){
+            System.out.println(val);
+        }
     }
 }
