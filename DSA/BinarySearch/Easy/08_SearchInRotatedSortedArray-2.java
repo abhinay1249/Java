dcacf7d93bb08a860166b1a90c341a4a8af5ae7c
abhinay1249 <abhinaymarise@gmail.com>
2026-02-11 23:52:49 +0530

    BinarySearch-1D-08

---
+++ DSA/BinarySearch/Easy/08_SearchInRotatedSortedArray-2.java
@@ -0,0 +1,66 @@
+// Problem Statement: Given an integer array nums, sorted in ascending order (with distinct values) and a target value k.
+// The array is rotated at some pivot point that is unknown. 
+// Find the index at which k is present and if k is not present return -1.
+
+
+
+class Main {
+
+    // ======================= Brute Force Approach =======================================
+
+        static int rotatedSortedArray(int[] nums, int target){
+
+            int length = nums.length;
+            boolean result = false;
+    
+            for(int index = 0 ; index < length ; index++){
+                if(nums[index] == target){
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+
+    // ======================= Optimal Approach =======================================
+
+        static int rotatedSortedArray(int[] nums, int target){
+
+            int length = nums.length;
+            int ans = -1;
+            int low = 0, high = length - 1;
+
+            while(low <= high){
+
+                int mid = low + ((high - low)/2);
+
+                if(nums[mid]==target){
+                    ans = mid;
+                    break;
+                }
+
+                if(nums[low] <= nums[mid]){                                     // T.C = O(LOG N), S.C = O(1)
+                    if(nums[low] <= target && target <= nums[mid]){
+                        high = mid - 1;
+                    }else{
+                        low = mid + 1;
+                    }
+                }else{
+                    if(nums[mid] <= target && target <= nums[high]){
+                        low = mid + 1;
+                    }else{
+                        high = mid - 1;
+                    }
+                }
+            }
+            return ans;
+        }
+
+    public static void main(String[] args) {
+
+        int[] nums = {4,5,6,7,0,1,2};
+        int target = 0;
+        int result = rotatedSortedArray(nums,target);
+        System.out.println(result);
+    }
+}
