c4dedf5f1bd35fb043b219f112215b8b8005b093
abhinay1249 <abhinaymarise@gmail.com>
2026-02-12 08:52:48 +0530

    BinarySearch-1D-09

--- DSA/BinarySearch/Easy/08_SearchInRotatedSortedArray-2.java
+++ DSA/BinarySearch/Easy/08_SearchInRotatedSortedArray-2.java
@@ -63,8 +63,8 @@ class Main {
 
     public static void main(String[] args) {
 
-        int[] nums = {4,5,6,7,0,1,2};
-        int target = 0;
+        int[] nums = {7, 8, 1, 2, 3, 3, 3, 4, 5, 6};
+        int target = 3;
         int result = rotatedSortedArray(nums,target);
         System.out.println(result);
     }
