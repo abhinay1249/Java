d1cbeb9979827737ceff05655117e7012f28c703
abhinay1249 <abhinaymarise@gmail.com>
2026-02-13 23:08:19 +0530

    BinarySearch-1D-Array-10

--- DSA/BinarySearch/Easy/10_RotatedTimesOfSortedArray.java
+++ DSA/BinarySearch/Easy/10_RotatedTimesOfSortedArray.java
@@ -1,3 +1,7 @@
+// Problem Statement: Given an integer array arr of size N, sorted in ascending order (with distinct values). 
+// Now the array is rotated between 1 to N times which is unknown. 
+// Find how many times the array has been rotated.
+
 
 class Main {
     
