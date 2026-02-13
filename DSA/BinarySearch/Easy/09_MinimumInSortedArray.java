5de3ff30a467223d588b3c2cb4bcd82e5736ee93
abhinay1249 <abhinaymarise@gmail.com>
2026-02-13 08:31:57 +0530

    BinarySearch-09-1D-Array

--- DSA/BinarySearch/Easy/09_MinimumInSortedArray.java
+++ DSA/BinarySearch/Easy/09_MinimumInSortedArray.java
@@ -3,46 +3,50 @@
 
 
 class Main {
-    
-    static int minimumInSortedArray(int[] nums){
-        
-        int length = nums.length;
-        int minValue = Integer.MAX_VALUE;                       // T.C = O(N), S.C = O(1)
-        
-        for(int index = 0 ; index < length ; index++){
-            
-            minValue = Math.min(nums[index],minValue);
-        }
-        return minValue;
-    }
-    
-    static int minimumInSortedArray_1(int[] nums){
-        
-        int length = nums.length;
-        int minValue = Integer.MAX_VALUE;
-        int low = 0, high = length - 1;
-        
-        while(low <= high){
-            
-            int mid = low + ((high - low)/2);                       // T.C = O(Log N), S.C = O(1)
-            
-            // Optional-->[For more optimized time complexity]
-            
-            if(nums[low] <= nums[high]){
-                minValue = Math.min(nums[low],minValue);
-                break;
+
+    // ======================= Brute Force Approach =======================================
+
+        static int minimumInSortedArray(int[] nums){
+
+            int length = nums.length;
+            int minValue = Integer.MAX_VALUE;                       // T.C = O(N), S.C = O(1)
+
+            for(int index = 0 ; index < length ; index++){
+
+                minValue = Math.min(nums[index],minValue);
             }
-            
-            if(nums[low] <= nums[mid]){
-                minValue = Math.min(nums[low],minValue);
-                low = mid + 1;
-            }else{
-                minValue = Math.min(nums[mid],minValue);
-                high = mid - 1;
-            }
+            return minValue;
         }
-        return minValue;
-    }
+    
+    // ======================= Optimal Approach ============================================
+
+        static int minimumInSortedArray_1(int[] nums){
+
+            int length = nums.length;
+            int minValue = Integer.MAX_VALUE;
+            int low = 0, high = length - 1;
+
+            while(low <= high){
+
+                int mid = low + ((high - low)/2);                       // T.C = O(Log N), S.C = O(1)
+
+                // Optional-->[For more optimized time complexity]
+
+                if(nums[low] <= nums[high]){
+                    minValue = Math.min(nums[low],minValue);
+                    break;
+                }
+
+                if(nums[low] <= nums[mid]){
+                    minValue = Math.min(nums[low],minValue);
+                    low = mid + 1;
+                }else{
+                    minValue = Math.min(nums[mid],minValue);
+                    high = mid - 1;
+                }
+            }
+            return minValue;
+        }
 
     public static void main(String[] args) {
         int[] nums = {4,5,6,7,0,1,2,3};
