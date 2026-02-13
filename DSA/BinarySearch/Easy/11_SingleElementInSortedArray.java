3a868fe94bd9280558263ed0ea0fe78f75c0e926
abhinay1249 <abhinaymarise@gmail.com>
2026-02-13 23:51:49 +0530

    BinarySearch-1D-Array-11

--- DSA/BinarySearch/Easy/11_SingleElementInSortedArray.java
+++ DSA/BinarySearch/Easy/11_SingleElementInSortedArray.java
@@ -7,42 +7,45 @@ class Main {
     
     // ======================= Brute Force Approach ===========================
 
-    static int singleElement(int[] nums){
-        int length = nums.length;
-        
-        if(length == 1) return nums[0];
-        
-        Map<Integer,Integer> freqCount = new HashMap<>();                     // T.C = O(N LOG M)+ O(N), S.C = O(N/2+1)        
-        
-        for(int index = 0 ; index < length ; index++){                                      
-            freqCount.put(nums[index],freqCount.getOrDefault(nums[index],0)+1);
-        }
-        
-        int singleElement = 0;
-        
-        for(Map.Entry<Integer,Integer> count : freqCount.entrySet()){
-            if(count.getValue() == 1){
-                singleElement = count.getKey();
-                break;
+        static int singleElement(int[] nums){
+            int length = nums.length;
+
+            if(length == 1) return nums[0];
+
+            Map<Integer,Integer> freqCount = new HashMap<>();                     // T.C = O(N LOG M)+ O(N), S.C = O(N/2+1)        
+
+            for(int index = 0 ; index < length ; index++){                                      
+                freqCount.put(nums[index],freqCount.getOrDefault(nums[index],0)+1);
             }
+
+            int singleElement = 0;
+
+            for(Map.Entry<Integer,Integer> count : freqCount.entrySet()){
+                if(count.getValue() == 1){
+                    singleElement = count.getKey();
+                    break;
+                }
+            }
+            return singleElement;
         }
-        return singleElement;
-    }
     
     // ======================= Better Approach =================================
 
-    static int singleElement_1(int[] nums){
-        int length = nums.length;
-        int singleElement = 0;
-       
-       if(length == 1) return nums[0];
-       
-        for(int index = 0 ; index < length ; index++){                      // T.C = O(N), S.C = O(1)
-            singleElement ^= nums[index];
+        static int singleElement_1(int[] nums){
+            int length = nums.length;
+            int singleElement = 0;
+
+           if(length == 1) return nums[0];
+
+            for(int index = 0 ; index < length ; index++){                      // T.C = O(N), S.C = O(1)
+                singleElement ^= nums[index];
+            }
+            return singleElement;
         }
-        return singleElement;
-    }
     
+    // ======================= Optimal Approach =================================
+
+
     public static void main(String[] args) {
         int[] nums = {1,1,2,2,3,3,4,5,5,6,6};
         int result = singleElement_1(nums);
