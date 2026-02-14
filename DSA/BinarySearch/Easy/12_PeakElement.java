bd2a17d3111f17b4b63e8a5b0a52a3bd26358e27
abhinay1249 <abhinaymarise@gmail.com>
2026-02-14 17:27:35 +0530

    BinarySearch-1D-12 Brute-Force

--- DSA/BinarySearch/Easy/12_PeakElement.java
+++ DSA/BinarySearch/Easy/12_PeakElement.java
@@ -20,9 +20,39 @@ class Main {
         return peakElementIdx;
     }
     
+    static int singlePeakElement(int[] nums){
+        
+        int length= nums.length;
+            
+        if(length == 1) return 0;
+        
+        if(nums[0]> nums[1]) return 0;
+        
+        if(nums[length-2]<nums[length-1]) return length -1;
+        
+        int peakElementIndex = 0;
+        
+        int low = 1, high = length - 2;
+        
+        while(low <= high){
+            int mid = low + ((high - low)/2);
+            
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+                peakElementIndex = mid;
+            }
+            
+            if(nums[mid-1]> nums[mid] && nums[mid]>nums[mid+1]){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return peakElementIndex;
+    }
+    
     public static void main(String[] args) {
-        int[] nums = {1,2,1,3,5,6,4};
-        int result = peakElement(nums);
+        int[] nums = {1,10,13,7,6,5,4,2,1,0};
+        int result = singlePeakElement(nums);
         System.out.println(result);
     }
 }
