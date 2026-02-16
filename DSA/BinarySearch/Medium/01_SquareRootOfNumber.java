e502895571b64785f90e8facc96cd88d356c7600
abhinay1249 <abhinaymarise@gmail.com>
2026-02-16 23:51:49 +0530

    BinarySearch-M-01 Brute Force

--- DSA/BinarySearch/Medium/01_SquareRootOfNumber.java
+++ DSA/BinarySearch/Medium/01_SquareRootOfNumber.java
@@ -2,24 +2,26 @@
 
 class Main {
     
-    static int sqrtOfNumber(int value){
-        
-        if(value == 0) return 0;
-        int result = -1;
-        
-        for(int index = 1 ; index <= value ; index++){
-            int squares = index * index;
-            
-            if(squares == value){
-                result = index;
-                break;
-            }else if(squares > value){
-                result = index - 1;
-                break;
+    // ==================================== Brute Force Approach ===========================================
+
+        static int sqrtOfNumber(int value){
+
+            if(value == 0) return 0;
+            int result = -1;
+
+            for(int index = 1 ; index <= value ; index++){
+                int squares = index * index;                                 //T.C = O(N), S.C = O(1)
+
+                if(squares == value){
+                    result = index;
+                    break;
+                }else if(squares > value){
+                    result = index - 1;
+                    break;
+                }
             }
+            return result;
         }
-        return result;
-    }
     public static void main(String[] args) {
         int number = 1024;
         int result = sqrtOfNumber(number);
