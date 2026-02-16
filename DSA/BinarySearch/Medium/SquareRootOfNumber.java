2eaf56e8d4219669e63aeeb3eddefe8987f66f0d
abhinay1249 <abhinaymarise@gmail.com>
2026-02-16 10:22:19 +0530

    BinarySearch-M-01

---
+++ DSA/BinarySearch/Medium/SquareRootOfNumber.java
@@ -0,0 +1,26 @@
+class Main {
+    
+    static int sqrtOfNumber(int value){
+        
+        if(value == 0) return 0;
+        int result = -1;
+        
+        for(int index = 1 ; index <= value ; index++){
+            int squares = index * index;
+            
+            if(squares == value){
+                result = index;
+                break;
+            }else if(squares > value){
+                result = index - 1;
+                break;
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int number = 1024;
+        int result = sqrtOfNumber(number);
+        System.out.println(result);
+    }
+}
