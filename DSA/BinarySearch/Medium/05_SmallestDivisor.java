15ddf9ad8a0cc93cd09593bb54f4ebe709a3479b
abhinay1249 <abhinaymarise@gmail.com>
2026-02-20 07:46:08 +0530

    BinarySearch-M-05 Brute-Force

--- DSA/BinarySearch/Medium/05_SmallestDivisor.java
+++ DSA/BinarySearch/Medium/05_SmallestDivisor.java
@@ -2,40 +2,40 @@ import java.util.*;
 
 class Main {
 
+    // =========================== Brute Force Approach ==============================
     
-    
-    static boolean minimumDivisor(int[] nums,int divisor,int limit){
-        
-        int sum = 0;
-        int div = 0;
-        int length = nums.length;
-        boolean flag = false;
-                
-        for(int index = 0 ; index < length ; index++){
-            div = (int) Math.ceil((double)nums[index]/divisor);
-            sum += div;
-            
-            if(sum <= limit){
-                flag = true;
-            }else{
-                flag = false;
-                break;
+        static boolean minimumDivisor(int[] nums,int divisor,int limit){
+
+            int sum = 0;
+            int div = 0;
+            int length = nums.length;
+            boolean flag = false;
+
+            for(int index = 0 ; index < length ; index++){
+                div = (int) Math.ceil((double)nums[index]/divisor);         
+                sum += div;
+
+                if(sum <= limit){                           // T.C = O(N) * O(MaxNumber) S.C = O(1)
+                    flag = true;
+                }else{
+                    flag = false;
+                    break;
+                }
             }
+            return flag;
         }
-        return flag;
-    }
-    
-    static int smallestDivisor(int[] nums, int limit){
-        
-        int maxNumber = Arrays.stream(nums).max().getAsInt();
-        
-        for(int index = 1 ; index <= maxNumber ; index++){
-            if(minimumDivisor(nums,index,limit) == true){
-                return index;
+
+        static int smallestDivisor(int[] nums, int limit){
+
+            int maxNumber = Arrays.stream(nums).max().getAsInt();
+
+            for(int index = 1 ; index <= maxNumber ; index++){
+                if(minimumDivisor(nums,index,limit) == true){
+                    return index;
+                }
             }
+            return -1;
         }
-        return -1;
-    }
     
 
     public static void main(String[] args) {
