189079b51fa5ca14f51936a9e1f3131046a2f68a
abhinay1249 <abhinaymarise@gmail.com>
2026-02-20 07:44:40 +0530

    BinarySearch-M-05 Brute-Force

---
+++ DSA/BinarySearch/Medium/05_SmallestDivisor.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Main {
+
+    
+    
+    static boolean minimumDivisor(int[] nums,int divisor,int limit){
+        
+        int sum = 0;
+        int div = 0;
+        int length = nums.length;
+        boolean flag = false;
+                
+        for(int index = 0 ; index < length ; index++){
+            div = (int) Math.ceil((double)nums[index]/divisor);
+            sum += div;
+            
+            if(sum <= limit){
+                flag = true;
+            }else{
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+    
+    static int smallestDivisor(int[] nums, int limit){
+        
+        int maxNumber = Arrays.stream(nums).max().getAsInt();
+        
+        for(int index = 1 ; index <= maxNumber ; index++){
+            if(minimumDivisor(nums,index,limit) == true){
+                return index;
+            }
+        }
+        return -1;
+    }
+    
+
+    public static void main(String[] args) {
+        int[] nums = {44,22,33,11,1};
+        int limit = 5;
+        int result = smallestDivisor_1(nums,limit);
+        System.out.println(result);
+    }
+}
