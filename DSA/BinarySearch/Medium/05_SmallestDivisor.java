7ff209cc012710598e060e4e54d56e1b53c0ef04
abhinay1249 <abhinaymarise@gmail.com>
2026-02-20 07:48:35 +0530

    BinarySearch-M-05

--- DSA/BinarySearch/Medium/05_SmallestDivisor.java
+++ DSA/BinarySearch/Medium/05_SmallestDivisor.java
@@ -37,6 +37,48 @@ class Main {
             return -1;
         }
     
+    // =========================== Optimal Approach ==============================
+
+        static boolean minimumDivisor_1(int[] nums,int divisor,int limit){
+        
+            int sum = 0;
+            int div = 0;
+            int length = nums.length;
+            boolean flag = false;
+
+            for(int index = 0 ; index < length ; index++){
+                div = (int) Math.ceil((double)nums[index]/divisor);
+                sum += div;
+
+                if(sum <= limit){
+                    flag = true;                                        
+                }else{
+                    flag = false;                          // T.C = O(log MaxNumber) * O(N) S.C = O(1)
+                    break;
+                }
+            }
+            return flag;
+        }
+    
+        static int smallestDivisor_1(int[] nums, int limit){
+
+            int length = nums.length;
+
+            if(threshold < length) return -1;
+
+            int low = 1;
+            int high = Arrays.stream(nums).max().getAsInt();
+
+            while(low <= high){
+                int mid = low + ((high - low)/2);
+                if(minimumDivisor(nums,mid,limit) == true){
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
+                }
+            }
+            return low;
+        }
 
     public static void main(String[] args) {
         int[] nums = {44,22,33,11,1};
