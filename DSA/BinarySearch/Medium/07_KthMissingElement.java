b3b2ca88cef0d0cb5282fcdc63628348d8b6b50d
abhinay1249 <abhinaymarise@gmail.com>
2026-02-21 16:04:35 +0530

    BinarySearch-M-07

--- DSA/BinarySearch/Medium/07_KthMissingElement.java
+++ DSA/BinarySearch/Medium/07_KthMissingElement.java
@@ -1,22 +1,44 @@
 class Main {
     
-    static int kthMissingElement(int[] nums, int k){
-        int missingElement = -1;
-        int length = nums.length;
-        
-        if(nums[0]>k){
-            missingElement = k;
+    // =========================== Brute Force Approach ==============================
+
+        static int kthMissingElement(int[] nums, int k){
+            int missingElement = -1;
+            int length = nums.length;
+
+            if(nums[0]>k){
+                missingElement = k;
+                return missingElement;                            // T.C = O(N) , S.C = O(1)
+            }
+            for(int index = 0 ; index < length ; index++){
+                if(nums[index]>k){
+                    missingElement = k;
+                    break;
+                }
+                k++;
+            }
             return missingElement;
         }
-        for(int index = 0 ; index < length ; index++){
-            if(nums[index]>k){
-                missingElement = k;
-                break;
+
+    // =========================== Optimal Approach ==============================
+
+        static int kthMissingElement_1(int[] nums, int k){
+            int length = nums.length;
+
+            int low = 0, high = length - 1;
+
+            while(low <= high){
+                int mid = low + ((high-low)/2);
+                int missing = nums[mid]-(mid+1);            // T.C = O(Log N) , S.C = O(1)
+
+                if(missing < k){
+                    low = mid + 1;
+                }else{
+                    high = mid - 1;
+                }
             }
-            k++;
+            return low + k;
         }
-        return missingElement;
-    }
     public static void main(String[] args) {
         int[] nums = {1,3,5,10,15};
         int k = 9;
