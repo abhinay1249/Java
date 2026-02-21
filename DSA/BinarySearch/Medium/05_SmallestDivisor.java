5dd5d32f390ae6ae61ce1fb79c80a752dddcabdc
abhinay1249 <abhinaymarise@gmail.com>
2026-02-21 12:08:59 +0530

    BinarySearch-M-05

--- DSA/BinarySearch/Medium/05_SmallestDivisor.java
+++ DSA/BinarySearch/Medium/05_SmallestDivisor.java
@@ -1,8 +1,7 @@
-// Problem Statement: You are the owner of a Shipment company. 
-// You use conveyor belts to ship packages from one port to another. The packages must be shipped within 'd' days. 
-// The weights of the packages are given in an array 'of weights'. The packages are loaded on the conveyor belts every day in the same order as they appear in the array. 
-// The loaded weights must not exceed the maximum weight capacity of the ship.
-//  Find out the least-weight capacity so that you can ship all the packages within 'd' days .
+// Problem Statement: You are given an array of integers 'arr' and an integer i.e. a threshold value 'limit'. 
+// Your task is to find the smallest positive integer divisor, 
+// such that upon dividing all the elements of the given array by it, the sum of the division's result is less than or equal to the given threshold value.
+
 
 import java.util.*;
 
