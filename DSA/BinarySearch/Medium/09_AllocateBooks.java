38c61c5ea6004570b5b19061c2e4fb886a1737b4
abhinay1249 <abhinaymarise@gmail.com>
2026-02-22 17:25:46 +0530

    BinarySearch-M-09 BruteForce

---
+++ DSA/BinarySearch/Medium/09_AllocateBooks.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class Main {
+
+        static int noOfStudents(int[]pages,int maxPages,int students){
+
+            int length = pages.length;
+            int studentsCount = 1;
+            int sumOfPages = 0;
+
+            for(int totalPages = 0 ; totalPages < length ; totalPages++){
+
+                if(sumOfPages + pages[totalPages] <= maxPages){
+                    sumOfPages += pages[totalPages];
+                }else{
+                    studentsCount++;
+                    sumOfPages = pages[totalPages];
+                }
+            }
+            return studentsCount;
+        }
+
+        static int allocateBooks(int[] pages, int students){
+
+            int length = pages.length;
+
+            if(length<students) return -1;
+            if(length == 1) return Arrays.stream(pages).sum();
+
+            int low = Arrays.stream(pages).max().getAsInt();
+            int high = Arrays.stream(pages).sum();
+
+            for(int maxPages = low ; maxPages <= high ; maxPages++){
+
+                int countOfStudents = noOfStudents(pages,maxPages,students);
+                
+                if(countOfStudents == students){
+                    return maxPages;
+                }
+            }
+            return -1;
+        }
+    public static void main(String[] args) {
+        int[] pages = {12, 34, 67, 90};
+        int students = 2;
+        int result = allocateBooks(pages,students);
+        System.out.println(result);
+    }
+}
