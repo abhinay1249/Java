338776f1af78e14c5fd7f9ed5b373e5114bcfa19
abhinay1249 <abhinaymarise@gmail.com>
2026-02-23 00:08:50 +0530

    BinarySearch-M-10

--- DSA/BinarySearch/Medium/10_SplitArray.java
+++ DSA/BinarySearch/Medium/10_SplitArray.java
@@ -2,80 +2,84 @@ import java.util.*;
 
 class Main {
     
-    static int subArraySum(int[] nums,int largestSum ,int subArrays){
-        
-        int length = nums.length;
-        int sum = 0;
-        int countOfSubArrays = 1;
-        
-        for(int index = 0 ; index < length ; index++){
-            if(sum+nums[index]<=largestSum){
-                sum+=nums[index];
-            }else{                                                  // T.C = O(sumOfElements - maxNumber) * O(N) + O(2N), S.C = O(1)
-                countOfSubArrays++;
-                sum = nums[index];
-            }
-        }
-        return countOfSubArrays;
-    }
-    
-    static int minimumLargestSum(int[] nums, int noOfSubArrays){
-        
-        int length = nums.length;
-        if(length < noOfSubArrays) return -1;
-        
-        int max = Arrays.stream(nums).max().getAsInt();
-        int sumOfElements = Arrays.stream(nums).sum(); 
-        
-        if(length == noOfSubArrays) return max;
-        
-        for(int largestSum = max ; largestSum <= sumOfElements ; largestSum++){
-            int subArrays = subArraySum(nums,largestSum,noOfSubArrays);
-            if(subArrays <= noOfSubArrays){
-                return largestSum;
-            }
-        }
-        return -1;
-    }
+    // =========================== Brute Force Approach ============================== 
 
-    static int subArraySum_1(int[] nums,int largestSum ,int subArrays){
-        
-        int length = nums.length;
-        int sum = 0;
-        int countOfSubArrays = 1;
-        
-        for(int index = 0 ; index < length ; index++){
-            if(sum+nums[index]<=largestSum){
-                sum+=nums[index];
-            }else{
-                countOfSubArrays++;
-                sum = nums[index];
+        static int subArraySum(int[] nums,int largestSum ,int subArrays){
+
+            int length = nums.length;
+            int sum = 0;
+            int countOfSubArrays = 1;
+
+            for(int index = 0 ; index < length ; index++){
+                if(sum+nums[index]<=largestSum){
+                    sum+=nums[index];
+                }else{                                           // T.C = O(sumOfElements - maxNumber) * O(N) + O(2N), S.C = O(1)
+                    countOfSubArrays++;
+                    sum = nums[index];
+                }
             }
+            return countOfSubArrays;
         }
-        return countOfSubArrays;
-    }
-    
-    static int minimumLargestSum_1(int[] nums, int noOfSubArrays){
-        
-        int length = nums.length;
-        if(length < noOfSubArrays) return -1;
-        
-        int low = Arrays.stream(nums).max().getAsInt();
-        int high = Arrays.stream(nums).sum(); 
-        
-        if(length == noOfSubArrays) return low;
-        
-        while(low <= high){                                     // T.C = O(Log(sumOfElements - maxNumber)) * O(N) + O(2N) ,S.C = O(1) 
-            int mid = low + ((high - low)/2);
-            int subArrays = subArraySum(nums,mid,noOfSubArrays);
-            if(subArrays <= noOfSubArrays){
-                high = mid - 1;
-            }else{
-                low = mid +1;
+
+        static int minimumLargestSum(int[] nums, int noOfSubArrays){
+
+            int length = nums.length;
+            if(length < noOfSubArrays) return -1;
+
+            int max = Arrays.stream(nums).max().getAsInt();
+            int sumOfElements = Arrays.stream(nums).sum(); 
+
+            if(length == noOfSubArrays) return max;
+
+            for(int largestSum = max ; largestSum <= sumOfElements ; largestSum++){
+                int subArrays = subArraySum(nums,largestSum,noOfSubArrays);
+                if(subArrays <= noOfSubArrays){
+                    return largestSum;
+                }
             }
+            return -1;
         }
-        return low;
-    }
+
+    // =========================== Optimal Approach ============================== 
+
+        static int subArraySum_1(int[] nums,int largestSum ,int subArrays){
+
+            int length = nums.length;
+            int sum = 0;
+            int countOfSubArrays = 1;
+
+            for(int index = 0 ; index < length ; index++){
+                if(sum+nums[index]<=largestSum){
+                    sum+=nums[index];
+                }else{
+                    countOfSubArrays++;
+                    sum = nums[index];
+                }
+            }
+            return countOfSubArrays;
+        }
+
+        static int minimumLargestSum_1(int[] nums, int noOfSubArrays){
+
+            int length = nums.length;
+            if(length < noOfSubArrays) return -1;
+
+            int low = Arrays.stream(nums).max().getAsInt();
+            int high = Arrays.stream(nums).sum(); 
+
+            if(length == noOfSubArrays) return low;
+
+            while(low <= high){                                  // T.C = O(Log(sumOfElements - maxNumber)) * O(N) + O(2N) ,S.C = O(1) 
+                int mid = low + ((high - low)/2);
+                int subArrays = subArraySum(nums,mid,noOfSubArrays);
+                if(subArrays <= noOfSubArrays){
+                    high = mid - 1;
+                }else{
+                    low = mid +1;
+                }
+            }
+            return low;
+        }
 
     public static void main(String[] args) {
         int[] nums = {2,3,1,1,1,1,1};
