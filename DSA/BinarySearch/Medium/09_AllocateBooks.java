17a17a3fbf03f3ff2a18a2972cf6734f441f39ef
abhinay1249 <abhinaymarise@gmail.com>
2026-02-22 17:57:21 +0530

    BinarySearch-M-09

--- DSA/BinarySearch/Medium/09_AllocateBooks.java
+++ DSA/BinarySearch/Medium/09_AllocateBooks.java
@@ -26,7 +26,7 @@ class Main {
 
             int length = pages.length;
 
-            if(length<students) return -1;                              // T.C = O(sumOfElements - maxNumber) * O(N) S.C = O(1)
+            if(length<students) return -1;                           // T.C = O(sumOfElements - maxNumber) * O(N) + O(2N), S.C = O(1)
             if(length == 1) return Arrays.stream(pages).sum();
 
             int low = Arrays.stream(pages).max().getAsInt();
@@ -42,6 +42,51 @@ class Main {
             }
             return -1;
         }
+
+    // =========================== Optimal Approach ============================== 
+
+        static int noOfStudents_1(int[]pages,int maxPages,int students){
+        
+            int length = pages.length;
+            int studentsCount = 1;
+            int sumOfPages = 0;
+
+            for(int totalPages = 0 ; totalPages < length ; totalPages++){
+
+                if(sumOfPages + pages[totalPages] <= maxPages){
+                    sumOfPages += pages[totalPages];
+                }else{
+                    studentsCount++;
+                    sumOfPages = pages[totalPages];
+                }
+            }
+            return studentsCount;
+        }
+
+        static int allocateBooks_1(int[] pages, int students){
+
+            int length = pages.length;
+
+            if(length<students) return -1;                      
+            if(length == 1) return Arrays.stream(pages).sum();
+
+            int low = Arrays.stream(pages).max().getAsInt();          
+            int high = Arrays.stream(pages).sum();
+    
+            while(low <= high){                             // T.C = O(Log(sumOfElements - maxNumber)) * O(N) + O(2N) ,S.C = O(1) 
+                int mid = low + ((high-low)/2);
+
+                int countOfStudents = noOfStudents_1(pages,mid,students);
+
+                if(countOfStudents <= students){
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
+                }
+            }
+            return low;
+        }
+
     public static void main(String[] args) {
         int[] pages = {12, 34, 67, 90};
         int students = 2;
