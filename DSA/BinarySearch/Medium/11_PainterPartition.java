5dac4ed0a8940027e27ea8a7877d15fcf094b32a
abhinay1249 <abhinaymarise@gmail.com>
2026-02-23 07:15:43 +0530

    BinarySearch-M-11

--- DSA/BinarySearch/Medium/11_PainterPartition.java
+++ DSA/BinarySearch/Medium/11_PainterPartition.java
@@ -2,43 +2,90 @@ import java.util.*;
 
 class Main {
     
-    static int noOfPainters(int[] boards, int boardUnits, int painters){
-        
-        int length = boards.length;
-        int sumOfUnits = 0;
-        int paintersCount = 1;
-        
-        for(int index = 0 ; index < length ; index++){
-            
-            if(sumOfUnits + boards[index] <= boardUnits){
-                sumOfUnits+=boards[index];
-            }else{
-                paintersCount++;
-                sumOfUnits=boards[index];
+    // =========================== Brute Force Approach ============================== 
+
+        static int noOfPainters(int[] boards, int boardUnits, int painters){
+
+            int length = boards.length;
+            int sumOfUnits = 0;
+            int paintersCount = 1;
+
+            for(int index = 0 ; index < length ; index++){
+
+                if(sumOfUnits + boards[index] <= boardUnits){
+                    sumOfUnits+=boards[index];
+                }else{
+                    paintersCount++;
+                    sumOfUnits=boards[index];
+                }
             }
+            return paintersCount;
         }
-        return paintersCount;
-    }
-    
-    static int minimumTime(int[] boards, int painters){
-        
-        int length = boards.length;
-        
-        if(length<painters) return -1;
-        
-        int low = Arrays.stream(boards).max().getAsInt();
-        int high = Arrays.stream(boards).sum();
-        
-        for(int boardUnits = low ; boardUnits <= high ; boardUnits++){
-            
-            int paintersCount = noOfPainters(boards,boardUnits,painters);
-            
-            if(paintersCount == painters){
-                return boardUnits;
+
+        static int minimumTime(int[] boards, int painters){
+
+            int length = boards.length;
+
+            if(length<painters) return -1;                          // T.C = O(sumOfElements - maxNumber) * O(N) + O(2N), S.C = O(1)
+
+            int low = Arrays.stream(boards).max().getAsInt();
+            int high = Arrays.stream(boards).sum();
+
+            for(int boardUnits = low ; boardUnits <= high ; boardUnits++){
+
+                int paintersCount = noOfPainters(boards,boardUnits,painters);
+
+                if(paintersCount == painters){
+                    return boardUnits;
+                }
             }
+            return -1;
         }
-        return -1;
-    }
+
+    // =========================== Optimal Approach ============================== 
+
+        static int noOfPainters_1(int[] boards, int boardUnits, int painters){
+
+            int length = boards.length;
+            int sumOfUnits = 0;
+            int paintersCount = 1;
+
+            for(int index = 0 ; index < length ; index++){
+
+                if(sumOfUnits + boards[index] <= boardUnits){
+                    sumOfUnits+=boards[index];
+                }else{
+                    paintersCount++;
+                    sumOfUnits=boards[index];
+                }
+            }
+            return paintersCount;
+        }
+
+        static int minimumTime_1(int[] boards, int painters){
+
+            int length = boards.length;
+
+            if(length<painters) return -1;
+
+            int low = Arrays.stream(boards).max().getAsInt();
+            int high = Arrays.stream(boards).sum();
+
+            while(low <= high){                                  // T.C = O(Log(sumOfElements - maxNumber)) * O(N) + O(2N) ,S.C = O(1)                                   
+
+                int mid = low + ((high-low)/2);
+                int paintersCount = noOfPainters(boards,mid,painters);
+
+                if(paintersCount <= painters){
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
+                }
+            }
+            
+            return low;
+        }
+
     public static void main(String[] args) {
         int[] boards = {10,20,30,40};
         int painters = 2;
