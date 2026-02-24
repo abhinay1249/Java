35272d65168d838a0f90f68f474fa1547b69ae97
abhinay1249 <abhinaymarise@gmail.com>
2026-02-24 23:00:33 +0530

    BinarySearch-M-10

--- DSA/BinarySearch/Medium/10_SplitArray.java
+++ DSA/BinarySearch/Medium/10_SplitArray.java
@@ -1,3 +1,8 @@
+// Problem Statement: Given an integer array ‘A’ of size ‘N’ and an integer ‘K'. 
+// Split the array ‘A’ into ‘K’ non-empty subarrays such that the largest sum of any subarray is minimized. 
+// Your task is to return the minimized largest sum of the split. A subarray is a contiguous part of the array.
+
+
 import java.util.*;
 
 class Main {
