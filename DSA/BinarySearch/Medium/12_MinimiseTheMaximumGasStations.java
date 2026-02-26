d70a77c0d302154d20f4742ace4a239362411e69
abhinay1249 <abhinaymarise@gmail.com>
2026-02-26 23:07:27 +0530

    BinarySearch-M-12

---
+++ DSA/BinarySearch/Medium/12_MinimiseTheMaximumGasStations.java
@@ -0,0 +1,41 @@
+class Main {
+
+    // =========================== Brute Force Approach ============================== 
+
+        static double minimumGasStationsDistance(int[] distance,int gasStations){
+            int length = distance.length;
+            int[] howMany = new int[length-1];
+
+            for(int gasStation = 0 ; gasStation <= length-1 ; gasStation++){
+                double maxSection = -1;
+                int maxIndex = -1;
+
+                for(int index = 0 ; index < length - 1 ; index++){
+                    int difference = distance[index+1]-distance[index];
+                    double sectionLength = difference/(double)(howMany[index]+1);
+                    if(maxSection < sectionLength){
+                        maxSection = sectionLength;
+                        maxIndex = index;
+                    }
+                }
+                howMany[maxIndex]++;
+            }
+
+            double maxAns = -1;
+
+            for(int index = 0 ; index < length - 1 ; index++){
+                int difference = distance[index+1]-distance[index];
+                double sectionLength = difference/(double)(howMany[index]+1);
+
+                maxAns = Math.max(maxAns,sectionLength);
+            }
+            return maxAns;
+        }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5};
+        int k = 4;
+        double result = minimumGasStationsDistance(nums,k);
+        System.out.println(result);
+    }
+}
