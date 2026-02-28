e25c14bd9fbbe2922cab38dc50e00a3b3a997276
abhinay1249 <abhinaymarise@gmail.com>
2026-02-28 23:11:10 +0530

    BinarySearch-2D-Arrays-1 Brute-Force

--- DSA/BinarySearch/2D-Arrays/01_FindRowsWithMaximumOne.java
+++ DSA/BinarySearch/2D-Arrays/01_FindRowsWithMaximumOne.java
@@ -1,28 +1,30 @@
 class Main {
+
+    // =========================== Brute Force Approach ==============================
     
-    static int rowWithMaximumOne(int[][]mat,int rowsLength,int colsLength){
-        
-        int rowCount = -1;
-        int row = -1;
-        
-        for(int rows = 0 ; rows < rowsLength; rows++){
-            int count = 0;
-            for(int cols = 0 ; cols < colsLength ; cols++){
-                if(mat[rows][cols]==1){
-                    count++;
+        static int rowWithMaximumOne(int[][]mat,int rowsLength,int colsLength){
+
+            int rowCount = -1;
+            int row = -1;
+
+            for(int rows = 0 ; rows < rowsLength; rows++){
+                int count = 0;
+                for(int cols = 0 ; cols < colsLength ; cols++){
+                    if(mat[rows][cols]==1){
+                        count++;
+                    }
+                }
+                if(rowCount < count){                               // T.C = O(N^2) S.C = O(1)
+                    rowCount = count;
+                    row = rows;
                 }
             }
-            if(rowCount < count){
-                rowCount = count;
-                row = rows;
+            if(rowCount == 0){
+                return -1;
             }
+            return row;
         }
-        if(rowCount == 0){
-            return -1;
-        }
-        return row;
-    }
-    
+
     public static void main(String[] args) {
         int[][] mat = {{0,0,1,1,1},{0,0,0,0,0},{0,1,1,1,1},{0,0,0,0,0},{0,1,1,1,1}};
         int rows = 5;
