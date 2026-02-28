2a1375f54682745c18fe6339e75f0d66728d102c
abhinay1249 <abhinaymarise@gmail.com>
2026-02-28 23:59:22 +0530

    BinarySearch-2D-Arrays-1

--- DSA/BinarySearch/2D-Arrays/01_FindRowsWithMaximumOne.java
+++ DSA/BinarySearch/2D-Arrays/01_FindRowsWithMaximumOne.java
@@ -1,3 +1,7 @@
+// Problem Statement: You have been given a non-empty grid ‘mat’ with 'n' rows and 'm' columns consisting of only 0s and 1s. 
+// All the rows are sorted in ascending order. Your task is to find the index of the row with the maximum number of ones. 
+// Note: If two rows have the same number of ones, consider the one with a smaller index. If there's no row with at least single 1, return -1
+
 class Main {
 
     // =========================== Brute Force Approach ==============================
