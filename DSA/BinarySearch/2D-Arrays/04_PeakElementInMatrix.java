6cba7c64559f9118b8e9986d1f878a7039246bc6
abhinay1249 <abhinaymarise@gmail.com>
2026-03-01 16:45:01 +0530

    BinarySearch-2D-Arrays-04

--- DSA/BinarySearch/2D-Arrays/04_PeakElementInMatrix.java
+++ DSA/BinarySearch/2D-Arrays/04_PeakElementInMatrix.java
@@ -17,11 +17,52 @@ class Main {
         }
 
         static int[] peakElement2(int[][] matrix){
-            
+
             int largestElement = Arrays.stream(matrix).flatMapToInt(Arrays::stream).max().getAsInt();
             int[] indices = searchElement(matrix,largestElement);
 
             return indices;
+        }
+    
+    // =========================== Optimal Approach ==============================
+
+        static int largestElementIndex(int[][] matrix, int rowLength, int col){
+
+            int maxValue = -1;
+            int index = -1;
+
+            for(int row = 0 ; row < rowLength ; row++){
+                if(matrix[row][col] > maxValue){
+                    maxValue = matrix[row][col];
+                    index = row;
+                }
+            }
+            return index;
+        }
+
+        static int[] peakElement2_1(int[][] matrix){
+            int rowLength = matrix.length;
+            int colLength = matrix[0].length;
+
+            int low = 0, high = colLength - 1;
+
+            while(low <= high){
+                int mid = low + ((high-low)/2);
+
+                int row = largestElementIndex(matrix,rowLength,mid);        // T.C = O(Rows Log(Cols)) , S.C = O(1)
+
+                int left = (mid-1)>=0 ? matrix[row][mid-1] : -1;
+                int right = (mid+1)<colLength ? matrix[row][mid+1] : -1;
+
+                if(left < matrix[row][mid] && matrix[row][mid] > right){
+                    return new int[]{row,mid};
+                }else if(matrix[row][mid] < right){
+                    low = mid + 1;
+                }else{
+                    high = mid - 1;
+                }
+            }
+            return new int[]{-1,-1};
         }
 
     public static void main(String[] args) {
