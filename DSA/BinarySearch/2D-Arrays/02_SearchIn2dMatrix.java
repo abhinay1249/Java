d2c68f5aa241073f2820d224e685e2c892672686
abhinay1249 <abhinaymarise@gmail.com>
2026-03-01 12:28:33 +0530

    BinarySearch-2D-Arrays-02 Better

--- DSA/BinarySearch/2D-Arrays/02_SearchIn2dMatrix.java
+++ DSA/BinarySearch/2D-Arrays/02_SearchIn2dMatrix.java
@@ -16,6 +16,14 @@ class Main {
             }
             return elementFound;
         }
+
+    // =========================== Better Approach ==============================
+
+        static boolean searchIn2DMatrix_1(int[][] matrix, int target){
+            
+        }
+
+
     public static void main(String[] args) {
         int[][] matrix = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
         int target = 8;
