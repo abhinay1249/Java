33a36fd5ec3c1e52545204fec6403b7d7bb5f78d
abhinay1249 <abhinaymarise@gmail.com>
2026-03-03 07:14:31 +0530

    Strings-E-01

--- DSA/Strings/Easy/01_RemoveOuterParantheses.java
+++ DSA/Strings/Easy/01_RemoveOuterParantheses.java
@@ -23,12 +23,35 @@ class Main {
             }
             return result;
         }
-    
+
+    // =========================== Optimal Approach ==============================
+
+        static int removeOuterParantheses_1(String s){
+            
+            StringBuilder result = new StringBuilder();
+            int counter = 0;
+
+            for(char ch : s.toCharArray()){
+
+                if(ch == '('){
+                    if(counter > 0){
+                        result.append('(');                            // T.C = O(N), S.C = O(1)
+                    }
+                    counter++;
+                }else if(ch == ')'){
+                    counter--;
+                    if(counter > 0){
+                        result.append(')');
+                    }
+                }
+            }
+            return result;
+        }
 
     public static void main(String[] args) {
 
         String s = "(()())(())";
-        String result = removeOuterParantheses(s);
+        String result = removeOuterParantheses_1(s);
         System.out.println(result);
     }
 }
