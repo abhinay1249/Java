209e7050a9e3d4a92fb2413c1ac89d9a5019a0d6
abhinay1249 <abhinaymarise@gmail.com>
2026-03-04 20:48:24 +0530

    Strings-E-02 Brute-Force

--- DSA/Strings/Easy/02_Reverse_Words_In_String.java
+++ DSA/Strings/Easy/02_Reverse_Words_In_String.java
@@ -1,3 +1,20 @@
 // Problem Statement: Given an input string, containing upper-case and lower-case letters, digits, and spaces( ' ' ). 
 // A word is defined as a sequence of non-space characters. The words in s are separated by at least one space. 
 // Return a string with the words in reverse order, concatenated by a single space.
+
+import java.util.*;
+
+
+class Main {
+
+    // ======================= Brute-Force Approach ===============================
+
+    static String reverseWordsInString(String s){
+        
+    }
+
+    public static void main(String[] args) {
+       String s = "welcome to the jungle";
+       String result = reverseWordsInString(s);
+    }
+}
