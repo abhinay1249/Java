3d64d6b7a9760bce28106d38793a73cdc4887238
abhinay1249 <abhinaymarise@gmail.com>
2026-03-04 21:05:10 +0530

    Strings-E-03

---
+++ DSA/Strings/Easy/03_Largest_Odd_Number_in_String.java
@@ -0,0 +1,22 @@
+// Problem Statement: Given a string s, representing a large integer, the task is to return the largest-valued odd integer (as a string) that is a substring of the given string s.
+// The number returned should not have leading zero's. But the given input string may have leading zero.
+
+class Main {
+
+    // ======================= Brute-Force Approach ===============================
+
+    static String largestOddNumberInString(String s){
+        
+    }
+
+    // ======================= Optimal Approach ===============================
+
+    static String largestOddNumberInString_1(String s){
+        
+    }
+
+    public static void main(String[] args) {
+       String s =  s = "5347";
+       String result = largestOddNumberInString_1(s);
+    }
+}
