b679bbdbece313c5beda8208371e85595fe14eac
abhinay1249 <abhinaymarise@gmail.com>
2026-03-05 21:32:45 +0530

    Strings-E-02

--- DSA/Strings/Easy/02_Reverse_Words_In_String.java
+++ DSA/Strings/Easy/02_Reverse_Words_In_String.java
@@ -4,13 +4,37 @@
 
 import java.util.*;
 
-
 class Main {
 
     // ======================= Brute-Force Approach ===============================
 
     static String reverseWordsInString(String s){
         
+        int length = s.length();
+
+        if(length == 1){
+            return s;
+        }
+        String word = "";
+        
+        List<String> words = new ArrayList<>();
+        
+        for(int character = 0 ; character < length ; character++){
+            
+            while(character < length && s.charAt(character) != ' '){
+                word+=s.charAt(character);
+                character++;
+            }
+            
+            if(word.length() > 0){
+                words.add(word);
+                word="";
+            }
+        }
+        Collections.reverse(words);
+        String reverseWords = String.join(" ",words);
+        
+        return reverseWords; 
     }
 
     // ======================= Optimal Approach ===============================
