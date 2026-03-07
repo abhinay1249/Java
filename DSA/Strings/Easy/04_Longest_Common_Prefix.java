4914b7e39c4cefde7a833e86593693edc6d0b96d
abhinay1249 <abhinaymarise@gmail.com>
2026-03-07 13:27:03 +0530

    Strings-E-04 Brute-Force

---
+++ DSA/Strings/Easy/04_Longest_Common_Prefix.java
@@ -0,0 +1,28 @@
+
+class Main {
+
+    static String longestCommonPrefix(String[] s){
+        
+        int elementLength = s[0].length();
+        int length = s.length;
+        StringBuilder prefix = new StringBuilder();
+        
+        for(int character = 0 ; character < elementLength ; character++){
+            char ch = s[0].charAt(character);
+            
+            for(int strings = 1 ; strings < length ; strings++){
+               if(character >= s[strings].length() || s[strings].charAt(character) != ch){
+                    return prefix.toString();
+                }
+            }
+            prefix.append(ch);
+        }
+        return prefix.toString();
+    }
+
+    public static void main(String[] args) {
+       String[] strings = {"flower", "flow", "flight"};
+       String result = longestCommonPrefix(strings);
+       System.out.println(result);
+    }
+}
