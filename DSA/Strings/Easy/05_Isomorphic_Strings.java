f762b02f8b97b95ba78f259648bc55db34ea12eb
abhinay1249 <abhinaymarise@gmail.com>
2026-03-07 16:55:11 +0530

    Strings-E-05 Brute-Force

---
+++ DSA/Strings/Easy/05_Isomorphic_Strings.java
@@ -0,0 +1,38 @@
+class Main{
+
+    // ======================= Brute-Force Approach ===============================
+
+        static boolean isIsomorphic(String s, String t) {
+            int firstLength = s.length();
+            int secondLength = t.length();
+
+            for(int index = 0 ; index < firstLength ; index++){
+                for(int character = index+1 ; character < secondLength ; character++){
+                    char firstCharacter_s = s.charAt(index);
+                    char secondCharacter_s = s.charAt(character);
+                    char firstCharacter_t = t.charAt(index);
+                    char secondCharacter_t = t.charAt(character);           
+
+                    if(firstCharacter_s == secondCharacter_s){
+                        if(firstCharacter_t != secondCharacter_t){
+                            return false;
+                        }
+                    }else{
+                        if(firstCharacter_t == secondCharacter_t){
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+        
+    public static void main(String[] args){
+        String s = "kite";
+        String t = "tart";
+        boolean result = isIsomorphic(s,t);
+        
+        System.out.println(result);
+
+    }
+}
