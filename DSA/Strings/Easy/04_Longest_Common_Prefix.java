26435b21a9c2fd3fe1668059c7f8e2559aa3dc46
abhinay1249 <abhinaymarise@gmail.com>
2026-03-08 21:05:36 +0530

    Strings-Easy-04

--- DSA/Strings/Easy/04_Longest_Common_Prefix.java
+++ DSA/Strings/Easy/04_Longest_Common_Prefix.java
@@ -1,3 +1,5 @@
+// Problem Statement: Write a function to find the longest common prefix string amongst an array of strings. 
+// If there is no common prefix, return an empty string "".
 
 class Main {
 
