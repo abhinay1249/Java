15842530e416412d4d06f1429475bdb0468a026c
abhinay1249 <abhinaymarise@gmail.com>
2026-03-08 22:07:52 +0530

    Strings-Easy-06 Brute-Force

--- DSA/Strings/Easy/06_Rotate_String.java
+++ DSA/Strings/Easy/06_Rotate_String.java
@@ -1,3 +1,19 @@
 // Problem Statement: Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
 // A shift on s consists of moving the leftmost character of s to the rightmost position. 
 // For example, if s = "abcde", then it will be "bcdea" after one shift.
+
+class Main{
+
+    static boolean rotateString(String s, String goal){
+
+    }
+
+    public static void main(String[] args){
+        String s = "rotation";
+        String goal = 'tionrota';
+
+        boolean result = rotateString(s, goal);
+
+        System.out.println(result);
+    }
+}
