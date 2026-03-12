3d3f91fcb8b298f4c925ab63ed70071483351504
abhinay1249 <abhinaymarise@gmail.com>
2026-03-12 07:00:25 +0530

    Strings-E-07 Brute-Force

--- DSA/Strings/Easy/07_Two_Strings_Anagram_Of_EachOther.java
+++ DSA/Strings/Easy/07_Two_Strings_Anagram_Of_EachOther.java
@@ -6,9 +6,41 @@ class Main{
     // ======================= Brute-Force Approach ===============================
 
         static boolean validateStringsAsAnagram(String s1, String s2){
-                                    
+            int sLength = s.length();
+            int tLength = t.length();
+
+            if(sLength != tLength){
+                return false;
+            }
+
+            char[] arr = s.toCharArray();
+            char[] arr_1 = t.toCharArray();
+
+            Arrays.sort(arr);
+            Arrays.sort(arr_1);
+
+            String word = new String(arr);
+            String word_1 = new String(arr_1);
+
+            int index_1 = 0, index_2 = 0;
+
+            while(index_1 < sLength && index_2 < tLength){
+                char ch = word.charAt(index_1);
+                char sh = word_1.charAt(index_2);
+
+                if(ch != sh){
+                    return false;
+                }
+                index_1++;
+                index_2++;
+            }
+            return true;                        
         }
+
     public static void main(String[] args){
-        
+        String s = "caac";
+        String t = "ccaa";
+        boolean result = twoStringsAnagram(s,t);
+        System.out.println(result);
     }
 }
