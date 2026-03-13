13a5fe10c520b979031b111abd686cf2714500d2
abhinay1249 <abhinaymarise@gmail.com>
2026-03-13 23:58:34 +0530

    Strings-M-01

---
+++ DSA/Strings/Medium/01_SortCharactersByFrequency.java
@@ -0,0 +1,2 @@
+// Problem Statement: You are given a string s. Return the array of unique characters, sorted by highest to lowest occurring characters.
+// If two or more characters have same frequency then arrange them in alphabetic order.
