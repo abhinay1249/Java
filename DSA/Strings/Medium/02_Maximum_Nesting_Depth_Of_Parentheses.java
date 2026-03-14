b890ae3ee8619fbcccc248420414446a846cc11a
abhinay1249 <abhinaymarise@gmail.com>
2026-03-14 23:37:43 +0530

    Strings-M-03

--- DSA/Strings/Medium/02_Maximum_Nesting_Depth_Of_Parentheses.java
+++ DSA/Strings/Medium/02_Maximum_Nesting_Depth_Of_Parentheses.java
@@ -1,6 +1,9 @@
+// Problem Statement: Given a valid parentheses string s, return the nesting depth of s. 
+// The nesting depth is the maximum number of nested parentheses.
+
 class Main{
 
     public static void main(String[] args){
-        
+
     }
 }
