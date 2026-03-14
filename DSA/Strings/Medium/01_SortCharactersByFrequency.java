f4547f28ba60800cee2bbfd7cb7497c1c112c6ae
abhinay1249 <abhinaymarise@gmail.com>
2026-03-14 23:50:23 +0530

    Strings-M-02 Brute-Force

--- DSA/Strings/Medium/01_SortCharactersByFrequency.java
+++ DSA/Strings/Medium/01_SortCharactersByFrequency.java
@@ -5,9 +5,33 @@
 class Main{
 
     static boolean sortedCharactersNyFrequency(String s){
+                
+        Map<Character,Integer> charCount = new HashMap<>();
+
+        int length = s.length();
+
+        for(int index = 0 ; index < length ; index++){
+            char ch = s.charAt(index);
+            charCount.put(ch,charCount.getOrDefault(ch,0)+1);
+        }
+
+        List<Map.Entry<Character,Integer>> chars = new ArrayList<>(charCount.entrySet());
+
+        Collections.sort(chars, (charCount_1,charCount_2) -> charCount_2.getValue().compareTo(charCount_1.getValue()));
+
+        StringBuilder word = new StringBuilder();
+
+        for(Map.Entry<Character,Integer> letters : chars){
+            int letterCount = letters.getValue();
+            for(int number = 0 ; number < letterCount ; number++){
+                word.append(letters.getKey());
+            }
+        }
+
+        return word.toString();
 
     } 
-    
+
     public static void main(String[] args){
         
     }
