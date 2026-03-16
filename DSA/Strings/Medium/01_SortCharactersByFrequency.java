bf6b1ff5a0b1768a759515c8e382e6b02d8b14c6
abhinay1249 <abhinaymarise@gmail.com>
2026-03-16 07:51:48 +0530

    Strings-M-01

--- DSA/Strings/Medium/01_SortCharactersByFrequency.java
+++ DSA/Strings/Medium/01_SortCharactersByFrequency.java
@@ -5,70 +5,74 @@ import java.util.*;
 
 class Main{
 
-    static String sortCharactersByFrequency(String s){
-                
-        Map<Character,Integer> charCount = new HashMap<>();
+    // ======================= Brute-Force Approach ===============================
 
-        int length = s.length();
+        static String sortCharactersByFrequency(String s){
 
-        for(int index = 0 ; index < length ; index++){
-            char ch = s.charAt(index);
-            charCount.put(ch,charCount.getOrDefault(ch,0)+1);            // T.C = O(N)+O(K)+O(K LOG K)+O(N), S.C = O(K)+O(K)+O(N) 
-        }
+            Map<Character,Integer> charCount = new HashMap<>();
 
-        List<Map.Entry<Character,Integer>> chars = new ArrayList<>(charCount.entrySet());
+            int length = s.length();
 
-        Collections.sort(chars, (charCount_1,charCount_2) -> charCount_2.getValue().compareTo(charCount_1.getValue()));
-
-        StringBuilder word = new StringBuilder();
-
-        for(Map.Entry<Character,Integer> letters : chars){
-            int letterCount = letters.getValue();
-            for(int number = 0 ; number < letterCount ; number++){
-                word.append(letters.getKey());
+            for(int index = 0 ; index < length ; index++){
+                char ch = s.charAt(index);
+                charCount.put(ch,charCount.getOrDefault(ch,0)+1);            // T.C = O(N)+O(K)+O(K LOG K)+O(N), S.C = O(K)+O(K)+O(N) 
             }
-        }
 
-        return word.toString();
+            List<Map.Entry<Character,Integer>> chars = new ArrayList<>(charCount.entrySet());
 
-    }
+            Collections.sort(chars, (charCount_1,charCount_2) -> charCount_2.getValue().compareTo(charCount_1.getValue()));
 
-    static String sortCharactersByFrequency_1(String s){
-        
-        int length = s.length();
-        
-        Map<Character,Integer> charCount = new HashMap<>();
-        
-        for(char ch : s.toCharArray()){
-            charCount.put(ch,charCount.getOrDefault(ch,0)+1);  
-        }
-        
-        List<Character>[] buckets = new ArrayList[length+1];
-        
-        for(int index = 0 ; index <= length ; index++){
-            buckets[index] = new ArrayList<>();                             // T.C = O(N), S.C = O(N)
-        }
-        
-        for(Map.Entry<Character,Integer> chars : charCount.entrySet()){
-            char ch = chars.getKey();
-            int count = chars.getValue();
-            
-            buckets[count].add(ch);
-        }
-        
-        StringBuilder word = new StringBuilder();
-        
-        for(int index = length; index >= 1 ; index--){
-            for(char ch : buckets[index]){
-            
-                for(int count = 0 ; count < index ; count++){
-                    word.append(ch);
+            StringBuilder word = new StringBuilder();
+
+            for(Map.Entry<Character,Integer> letters : chars){
+                int letterCount = letters.getValue();
+                for(int number = 0 ; number < letterCount ; number++){
+                    word.append(letters.getKey());
                 }
             }
+
+            return word.toString();
+
         }
 
-        return word.toString();
-    } 
+    // ======================= Optimal Approach [BUCKET SORT ALGORITHM ] ===============================
+
+        static String sortCharactersByFrequency_1(String s){
+
+            int length = s.length();
+
+            Map<Character,Integer> charCount = new HashMap<>();
+
+            for(char ch : s.toCharArray()){
+                charCount.put(ch,charCount.getOrDefault(ch,0)+1);  
+            }
+
+            List<Character>[] buckets = new ArrayList[length+1];
+
+            for(int index = 0 ; index <= length ; index++){
+                buckets[index] = new ArrayList<>();                             // T.C = O(N), S.C = O(N)
+            }
+
+            for(Map.Entry<Character,Integer> chars : charCount.entrySet()){
+                char ch = chars.getKey();
+                int count = chars.getValue();
+
+                buckets[count].add(ch);
+            }
+
+            StringBuilder word = new StringBuilder();
+
+            for(int index = length; index >= 1 ; index--){
+                for(char ch : buckets[index]){
+                
+                    for(int count = 0 ; count < index ; count++){
+                        word.append(ch);
+                    }
+                }
+            }
+
+            return word.toString();
+        } 
 
     public static void main(String[] args){
         String s = "Aabb";
