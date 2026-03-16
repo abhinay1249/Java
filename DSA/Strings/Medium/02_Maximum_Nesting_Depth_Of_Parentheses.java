8549c23b72d4ba8c731c7369886c2996561d844a
abhinay1249 <abhinaymarise@gmail.com>
2026-03-16 09:11:42 +0530

    Strings-M-02

--- DSA/Strings/Medium/02_Maximum_Nesting_Depth_Of_Parentheses.java
+++ DSA/Strings/Medium/02_Maximum_Nesting_Depth_Of_Parentheses.java
@@ -1,10 +1,32 @@
 // Problem Statement: Given a valid parentheses string s, return the nesting depth of s. 
 // The nesting depth is the maximum number of nested parentheses.
 
-class Main{
+class Main {
 
+    // ======================= Optimal Approach ===============================
+    
+        static int maxNestingDepth(String s){
+            int length = s.length();
+
+            int count = 0;
+            int maxNesting = Integer.MIN_VALUE;
+
+            for(int index = 0 ; index < length ; index++){
+                char ch = s.charAt(index);
+
+                if(ch == '('){                                     
+                    count++;
+                }else if(ch == ')'){
+                    count--;
+                }
+                maxNesting=Math.max(maxNesting,count);
+            }
+            return maxNesting;
+        }
 
     public static void main(String[] args){
-
+        String s = "(1+(2*3)+((8)/4))+1";
+        int result = maxNestingDepth(s);
+        System.out.println(result);
     }
 }
