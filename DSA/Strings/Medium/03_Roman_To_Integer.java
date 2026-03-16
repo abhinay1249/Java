8a7f19cc65362611626fbc9510f4492c48e1409d
abhinay1249 <abhinaymarise@gmail.com>
2026-03-16 23:53:06 +0530

    Strings-M-03

--- DSA/Strings/Medium/03_Roman_To_Integer.java
+++ DSA/Strings/Medium/03_Roman_To_Integer.java
@@ -1,4 +1,10 @@
-
+// Problem Statement: Roman numerals are represented by seven different symbols: I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000
+// For example: 2 is written as II, 12 is written as XII, 27 is written as XXVII.
+// Roman numerals are usually written largest to smallest from left to right. But in six special cases, subtraction is used instead of addition:
+// I before V or X → 4 and 9,
+// X before L or C → 40 and 90,
+// C before D or M → 400 and 900
+// Given a Roman numeral, convert it to an integer.
 
 
 import java.util.*;
