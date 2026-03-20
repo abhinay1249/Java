6dcb8884e2e6cbd199363c1b64e60e5c5b86e73f
abhinay1249 <abhinaymarise@gmail.com>
2026-03-20 23:55:29 +0530

    Strings-M-07

---
+++ DSA/Strings/Medium/07_Sum_of_Beauty_of_All_Substrings.java
@@ -0,0 +1,3 @@
+// Problem Statement: The beauty of a string is defined as the difference between the frequency of the most frequent character and 
+// the least frequent character (excluding characters that do not appear) in that string.
+// Given a string s, return the sum of beauty values of all possible substrings of s.
