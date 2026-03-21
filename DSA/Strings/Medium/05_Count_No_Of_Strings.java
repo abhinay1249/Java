48b559048cd2db89ad40227d9a027466c596c614
abhinay1249 <abhinaymarise@gmail.com>
2026-03-21 17:59:30 +0530

    Strings-M-05 Code Check

--- DSA/Strings/Medium/05_Count_No_Of_Strings.java
+++ DSA/Strings/Medium/05_Count_No_Of_Strings.java
@@ -3,43 +3,45 @@
 
 class Main {
     
-    static int atmost(String s, int k){
-        
-        int length = s.length();
-        
-        int[] nums = new int[26];
-        
-        int distinct = 0, left = 0;
-        int atmostPossible = 0;
-        
-        for(int index = 0 ; index < length ; index++){
-            char ch = s.charAt(index);
-            
-            nums[ch - 'a']++;
-            
-            if(nums[ch - 'a'] == 1){
-                distinct++;
-            }
-            
-            while(distinct > k){
-                char leftch = s.charAt(left);
-                
-                nums[leftch - 'a']--;
-                
-                if(nums[leftch - 'a'] == 0){
-                    distinct--;
+    // ======================= Optimal Approach ===============================
+
+        static int atmost(String s, int k){
+
+            int length = s.length();
+
+            int[] nums = new int[26];
+
+            int distinct = 0, left = 0;
+            int atmostPossible = 0;
+
+            for(int index = 0 ; index < length ; index++){
+                char ch = s.charAt(index);
+
+                nums[ch - 'a']++;
+
+                if(nums[ch - 'a'] == 1){
+                    distinct++;
                 }
-                left++;
+
+                while(distinct > k){
+                    char leftch = s.charAt(left);
+
+                    nums[leftch - 'a']--;
+
+                    if(nums[leftch - 'a'] == 0){
+                        distinct--;
+                    }
+                    left++;
+                }
+                atmostPossible = atmostPossible + (index - left +1);
             }
-            atmostPossible = atmostPossible + (index - left +1);
+            return atmostPossible;
         }
-        return atmostPossible;
-    }
-    
-    static int countNumberOfStrings(String s, int k){
-        return atmost(s,k) - atmost(s,k-1);
-    }
-    
+
+        static int countNumberOfStrings(String s, int k){
+            return atmost(s,k) - atmost(s,k-1);
+        }
+
     public static void main(String[] args) {
         String s = "pqpqs";
         int k = 2;
