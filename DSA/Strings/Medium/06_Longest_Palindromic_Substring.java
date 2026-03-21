0254ca5923a8bac79320a36aab6b5bbf21c30274
abhinay1249 <abhinaymarise@gmail.com>
2026-03-21 22:46:45 +0530

    Strings-M-06 Brute Force Code

--- DSA/Strings/Medium/06_Longest_Palindromic_Substring.java
+++ DSA/Strings/Medium/06_Longest_Palindromic_Substring.java
@@ -1,2 +1,60 @@
 // Given a string s, return the longest palindromic substring in s.
 // A palindromic substring is a contiguous sequence of characters within the string that reads the same forward and backward.
+
+class Main {
+    
+    static String longestPalindromeSubstring(String s){
+        
+        int length = s.length();
+        
+        if(length == 1){
+            return s;
+        }
+        
+        String lps = "";
+        
+        for(int index = 1 ; index < length ; index++){
+            int low = index;
+            int high = index;
+            
+            while(s.charAt(low) == s.charAt(high)){
+                low--;
+                high++;
+                
+                if(low == -1 || high == length){
+                    break;
+                }
+            }
+            
+            String palindrome = s.substring(low+1,high);
+            if(palindrome.length() > lps.length()){
+                lps = palindrome;
+            }
+            
+            low = index - 1;
+            high = index;
+            
+            while(s.charAt(low) == s.charAt(high)){
+                low--;
+                high++;
+                
+                if(low == -1 || high == length){
+                    break;
+                } 
+            }
+            
+            palindrome = s.substring(low+1,high);
+            if(palindrome.length() > lps.length()){
+                lps = palindrome;
+            }
+        }
+        return lps;
+    }
+    
+    public static void main(String[] args) {
+        String s = "aaaa";
+        String result = longestPalindromeSubstring(s);
+        StringBuilder ans = new StringBuilder(result);
+        System.out.println(ans);
+    }
+}
