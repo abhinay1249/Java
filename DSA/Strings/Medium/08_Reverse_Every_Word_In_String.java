81fc3154d4efb8d39ae14080d3c37bcaa7095581
abhinay1249 <abhinaymarise@gmail.com>
2026-03-22 23:41:42 +0530

    Strings-M-08

--- DSA/Strings/Medium/08_Reverse_Every_Word_In_String.java
+++ DSA/Strings/Medium/08_Reverse_Every_Word_In_String.java
@@ -2,3 +2,18 @@
 // A word is defined as a sequence of non-space characters. 
 // The words in s are separated by at least one space. 
 // Return a string with the words in reverse order, concatenated by a single space.
+
+
+class Main {
+    
+        static String reverseEveryWordInString(String s){
+
+        }
+
+
+    public static void main(String[] args) {
+        String s = "aabcbaa";
+        String result = reverseEveryWordInString(s);
+        System.out.println(result);
+    }
+}
