546b5d21d32df88b0ad4b9adc8867c584b82c25e
abhinay1249 <abhinaymarise@gmail.com>
2026-03-22 23:40:36 +0530

    Strings-M-07

--- DSA/Strings/Medium/07_Sum_of_Beauty_of_All_Substrings.java
+++ DSA/Strings/Medium/07_Sum_of_Beauty_of_All_Substrings.java
@@ -1,3 +1,20 @@
 // Problem Statement: The beauty of a string is defined as the difference between the frequency of the most frequent character and 
 // the least frequent character (excluding characters that do not appear) in that string.
 // Given a string s, return the sum of beauty values of all possible substrings of s.
+
+// Given a string s, return the longest palindromic substring in s.
+// A palindromic substring is a contiguous sequence of characters within the string that reads the same forward and backward.
+
+class Main {
+    
+        static String sumOfBeautyOfAllSubstrings(String s){
+
+        }
+
+
+    public static void main(String[] args) {
+        String s = "aabcbaa";
+        int result = sumOfBeautyOfAllSubstrings(s);
+        System.out.println(result);
+    }
+}
