c28795abe8e695fef69904fcffae308b0f1af691
abhinay1249 <abhinaymarise@gmail.com>
2026-03-23 23:05:46 +0530

    Strings-M-07 BRUTE FORCE

--- DSA/Strings/Medium/07_Sum_of_Beauty_of_All_Substrings.java
+++ DSA/Strings/Medium/07_Sum_of_Beauty_of_All_Substrings.java
@@ -8,36 +8,39 @@
 import java.util.*;
 
 class Main {
-    static int sumOfAllSubstrings(String s){
-        int length = s.length();
-        
-        if(length == 0){
-            return 0;
-        }
-        
-        if(length == 1){
-            return 1;
-        }
-        
-        Map<Character,Integer> freqCount = new HashMap<>();
-        
-        int totalSum = 0;
-        
-        for(int index = 0 ; index < length ; index++){
-            for(int index_1 = index ; index_1 < length ; index_1++){
-                for(int value = index ; value <= index_1 ; value++){
-                    char ch = s.charAt(value);
-                    freqCount.put(ch,freqCount.getOrDefault(ch,0)+1);
-                }
-                int max = Collections.max(freqCount.values());
-                int min = Collections.min(freqCount.values());
-                int diff = max - min;
-                totalSum += diff;
-                freqCount.clear();
+
+        static int sumOfAllSubstrings(String s){
+            int length = s.length();
+
+            if(length == 0){
+                return 0;
             }
+
+            if(length == 1){
+                return 1;
+            }
+
+            Map<Character,Integer> freqCount = new HashMap<>();
+
+            int totalSum = 0;                                                   //T.C = O(N^3), S.C = O(1)
+
+            for(int index = 0 ; index < length ; index++){
+                for(int index_1 = index ; index_1 < length ; index_1++){
+                    for(int value = index ; value <= index_1 ; value++){
+                        char ch = s.charAt(value);                              
+                        freqCount.put(ch,freqCount.getOrDefault(ch,0)+1);
+                    }
+                    int max = Collections.max(freqCount.values());
+                    int min = Collections.min(freqCount.values());
+                    int diff = max - min;
+                    totalSum += diff;
+                    freqCount.clear();
+                }
+            }
+            return totalSum;
         }
-        return totalSum;
-    }
+
+
     public static void main(String[] args) {
         String s = "aabcbaa";
         int result = sumOfAllSubstrings(s);
