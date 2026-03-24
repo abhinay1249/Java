0398bc8d7c83f5be733f7213dbb549a43dfe647a
abhinay1249 <abhinaymarise@gmail.com>
2026-03-24 08:17:23 +0530

    Strings-M-08

--- DSA/Strings/Medium/08_Reverse_Every_Word_In_String.java
+++ DSA/Strings/Medium/08_Reverse_Every_Word_In_String.java
@@ -4,16 +4,41 @@
 // Return a string with the words in reverse order, concatenated by a single space.
 
 
+import java.util.*;
+
 class Main {
-    
-        static String reverseEveryWordInString(String s){
-
+    static String reverseWordsInString(String s){
+        int length = s.length();
+        
+        if(length == 1 && s.charAt(0)!=' '){
+            return s;
         }
-
-
+        
+        StringBuilder word = new StringBuilder();
+        
+        List<String> words = new ArrayList<>();
+        
+        for(int character = 0 ; character < length ; character++){
+            
+            while(character < length && s.charAt(character)!=' '){
+                word.append(s.charAt(character));
+                character++;
+            }
+            
+            if(word.length() > 0){
+                words.add(word.toString());
+                word.setLength(0);
+            }
+        }
+        
+        Collections.reverse(words);
+        String reverseWord = String.join(" ",words);
+        
+        return reverseWord;
+    }
     public static void main(String[] args) {
-        String s = "aabcbaa";
-        String result = reverseEveryWordInString(s);
+        String s = " amazing coding skills ";
+        String result = reverseWordsInString(s);
         System.out.println(result);
     }
 }
