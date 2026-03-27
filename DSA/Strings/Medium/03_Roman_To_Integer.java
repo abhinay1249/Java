dd4da828aa8b0fb9504fc68e593c6d604cd7049f
abhinay1249 <abhinaymarise@gmail.com>
2026-03-27 23:21:27 +0530

    Strings-M-03

--- DSA/Strings/Medium/03_Roman_To_Integer.java
+++ DSA/Strings/Medium/03_Roman_To_Integer.java
@@ -6,6 +6,22 @@
 // C before D or M → 400 and 900
 // Given a Roman numeral, convert it to an integer.
 
+// Example 1:
+// Input:
+//  s = "LVIII"
+// Output:
+//  58
+// Explanation:
+//  L = 50, V= 5, III = 3.
+
+// Example 2:
+// Input:
+//  s = "MCMXCIV"
+// Output:
+//  1994
+// Explanation:
+//  M = 1000, CM = 900, XC = 90 and IV = 4.
+
 
 import java.util.*;
 
