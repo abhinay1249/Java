e51447003b58eabf6dbddd2fbc1633eab2fa0830
abhinay1249 <abhinaymarise@gmail.com>
2026-03-27 23:19:06 +0530

    Strings-M-01

--- DSA/Strings/Medium/01_Sort_Characters_By_Frequency.java
+++ DSA/Strings/Medium/01_Sort_Characters_By_Frequency.java
@@ -1,6 +1,33 @@
 // Problem Statement: You are given a string s. Return the array of unique characters, sorted by highest to lowest occurring characters.
 // If two or more characters have same frequency then arrange them in alphabetic order.
 
+// Example 1:
+// Input:
+//  s = "tree"
+// Output:
+//  ['e', 'r', 't']
+// Explanation:
+
+// e → 2
+// r → 1
+// t → 1
+// Since 'r' and 't' have the same frequency, they are sorted alphabetically → 'r' comes before 't'.
+
+// Example 2:
+// Input:
+//  s = "raaaajj"
+// Output:
+//  ['a', 'j', 'r']
+// Explanation:
+
+// a → 4
+// j → 2
+// r → 1
+// Characters are sorted by decreasing frequency. In case of ties, alphabetically.
+
+
+
+
 import java.util.*;
 
 class Main{
