74c89e0507fbe15948cef695598b204eaf6a7cf3
abhinay1249 <abhinaymarise@gmail.com>
2026-03-27 23:26:39 +0530

    Strings-M-08

--- DSA/Strings/Medium/08_Reverse_Every_Word_In_String.java
+++ DSA/Strings/Medium/08_Reverse_Every_Word_In_String.java
@@ -4,6 +4,19 @@
 // Return a string with the words in reverse order, concatenated by a single space.
 
 
+// Input: s = "welcome to the jungle"
+// Output: "jungle the to welcome"
+// Explanation: The words in the input string are "welcome", "to", "the", and "jungle". 
+// Reversing the order of these words gives "jungle", "the", "to", and "welcome". 
+// The output string should have exactly one space between each word.
+
+// Input: s = " amazing coding skills "
+// Output: "skills coding amazing"
+// Explanation: The input string has leading and trailing spaces, as well as multiple spaces between the words "amazing", "coding", and "skills". 
+// After trimming the leading and trailing spaces and reducing the multiple spaces between words to a single space, the words are "amazing", "coding", and "skills". 
+// Reversing the order of these words gives "skills", "coding", and "amazing". The output string should not have any leading or trailing spaces and should have exactly one space between each word.
+
+
 import java.util.*;
 
 class Main {
