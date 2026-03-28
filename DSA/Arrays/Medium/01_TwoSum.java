36f47dcd9b3fe9f8779daaecbd6e0dfcab67abe7
abhinay1249 <abhinaymarise@gmail.com>
2026-03-28 19:28:48 +0530

    Arrays-M-01

--- DSA/Arrays/Medium/01_TwoSum.java
+++ DSA/Arrays/Medium/01_TwoSum.java
@@ -5,6 +5,16 @@
 // 2nd variant: Return indices of the two numbers such that their sum is equal to the target. Otherwise, we will return {-1, -1}.
 
  
+// Examples
+
+// Input: N = 5, arr[] = {2,6,5,8,11}, target = 14
+// Output : YES
+// Explanation: arr[1] + arr[3] = 14. So, the answer is “YES” for first variant for second variant output will be : [1,3].
+
+// Input: N = 5, arr[] = {2,6,5,8,11}, target = 15
+// Output : NO.
+// Explanation: There exist no such two numbers whose sum is equal to the target.
+
 import java.util.*;
 
 
