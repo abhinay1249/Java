8cb6b7439295e81091e81513bf942647d211ed94
abhinay1249 <abhinaymarise@gmail.com>
2026-03-28 19:26:26 +0530

    Arrays-M-04

--- DSA/Arrays/Medium/04_MaximumSubArraySum.java
+++ DSA/Arrays/Medium/04_MaximumSubArraySum.java
@@ -6,6 +6,23 @@
 // Can you print the subarray that has the maximum sum?
 
 
+// Example 1:
+
+// Input: nums = [2, 3, 5, -2, 7, -4]  
+// Output: 15  
+// Explanation:
+//  The subarray from index 0 to index 4 has the largest sum = 15, which is the maximum sum of any contiguous subarray.
+
+// Example 2:
+
+// Input: nums = [-2, -3, -7, -2, -10, -4]  
+// Output: -2  
+// Explanation:
+// The largest sum is -2, which comes from taking the element at index 0 or index 3 as the subarray. 
+// Since all numbers are negative, the subarray with the least negative number gives the largest sum.
+
+
+
 class Main {
     
 
