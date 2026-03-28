1ac913d7b43606ee9a5733998548c4c5fd07ec5a
abhinay1249 <abhinaymarise@gmail.com>
2026-03-28 23:19:36 +0530

    Arrays-M-11

--- DSA/Arrays/Medium/11_RotateMatrixBy90.java
+++ DSA/Arrays/Medium/11_RotateMatrixBy90.java
@@ -2,6 +2,22 @@
 // The rotation must be done in place, meaning the input 2D matrix must be modified directly.
 
 
+// Examples
+// Input :matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+// Output : matrix = [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+// Explanation : First, we transpose the matrix: rows become columns. 
+// Then, we reverse each row to simulate 90° clockwise rotation. 
+// So element at (0,0) goes to (0,2), (0,1) goes to (1,2), and so on, achieving the rotated layout.
+
+// Input : matrix = [[0, 1, 1, 2], [2, 0, 3, 1], [4, 5, 0, 5], [5, 6, 7, 0]]
+// Output : matrix = [[5, 4, 2, 0], [6, 5, 0, 1], [7, 0, 3, 1], [0, 5, 1, 2]]
+// Explanation : First, the matrix is transposed: rows become columns. 
+// Then, each row is reversed. 
+// This moves the last column to the first row, the second last column to the second row, and so on. 
+// The original position of each element is rotated 90° clockwise into its new location.
+
+
+
 class Main {
     
     // ======================= Brute Force Approach =======================================
