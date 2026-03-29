de584d69d7c7e0b580dbd5414923541088d5f512
abhinay1249 <abhinaymarise@gmail.com>
2026-03-29 17:34:47 +0530

    Arrays-M-13

--- DSA/Arrays/Medium/13_CountSubArraySum.java
+++ DSA/Arrays/Medium/13_CountSubArraySum.java
@@ -24,7 +24,7 @@ class Main {
 
 	        for(int i = 0 ; i < n ; i++){
 	    	    for(int j = i ; j < n ; j++){
-	    	        int sum = 0;
+	    	        int sum = 0;										// T.C = O(N^3), S.C = O(1)
 	    	        for(int k = i ; k <= j ; k++){
         			    sum+=nums[k];
 	        		    if(sum == target){
@@ -45,7 +45,7 @@ class Main {
 	        for(int i = 0 ; i < n ; i++){
 	            int sum = 0;
 	    	    for(int j = i ; j < n ; j++){
-	    		    sum+=nums[j];
+	    		    sum+=nums[j];										// T.C = O(N^2), S.C = O(1)
 	    		    if(sum== target){
 	    			    count++;
 	    		    }
