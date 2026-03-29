a0e1efa5b56f42dc78ca657f50fb73c5fc752404
abhinay1249 <abhinaymarise@gmail.com>
2026-03-29 10:42:31 +0530

    Arrays-M-12

--- DSA/Arrays/Medium/12_SpiralMatrix.java
+++ DSA/Arrays/Medium/12_SpiralMatrix.java
@@ -1,3 +1,13 @@
+// Problem Statement: Given a Matrix, print the given matrix in spiral order.
+
+// Input: Matrix[][] = { { 1, 2, 3, 4 },{ 5, 6, 7, 8 },{ 9, 10, 11, 12 },{ 13, 14, 15, 16 } }
+// Outhput: 1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10.
+// Explanation: The output of matrix in spiral form.
+
+// Input: Matrix[][] = { { 1, 2, 3 }, { 4, 5, 6 },{ 7, 8, 9 } }
+// Output: 1, 2, 3, 6, 9, 8, 7, 4, 5.
+// Explanation: The output of matrix in spiral form.
+
 import java.util.*;
 
 class Main {
