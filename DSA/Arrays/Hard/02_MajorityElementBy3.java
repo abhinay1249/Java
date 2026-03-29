03d01f7b59022002fd36478921b4de9f71ab1ac2
abhinay1249 <abhinaymarise@gmail.com>
2026-03-29 23:50:25 +0530

    Arrays-H-02

--- DSA/Arrays/Hard/02_MajorityElementBy3.java
+++ DSA/Arrays/Hard/02_MajorityElementBy3.java
@@ -2,6 +2,20 @@
 // Return all elements which appear more than n/3 times in the array. 
 // The output can be returned in any order.
 
+// Example 1:
+
+// Input : nums = [1, 2, 1, 1, 3, 2]  
+// Output: [1]  
+// Explanation: Here, n / 3 = 6 / 3 = 2.  
+// Therefore, the elements appearing 3 or more times are: [1].
+
+// Example 2:
+
+// Input : nums = [1, 2, 1, 1, 3, 2, 2]  
+// Output : [1, 2]  
+// Explanation: Here, n / 3 = 7 / 3 = 2.  
+// Therefore, the elements appearing 3 or more times are: [1, 2].
+
 import java.util.*;
 
 class Main {
