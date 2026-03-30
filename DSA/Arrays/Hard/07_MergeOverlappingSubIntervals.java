a9cc9a75d9559c4deaaae2591c3f013bf7437e4d
abhinay1249 <abhinaymarise@gmail.com>
2026-03-30 23:46:56 +0530

    Arrays-H-07

--- DSA/Arrays/Hard/07_MergeOverlappingSubIntervals.java
+++ DSA/Arrays/Hard/07_MergeOverlappingSubIntervals.java
@@ -2,6 +2,17 @@
 // merge all overlapping intervals and return an array of the non-overlapping intervals that cover all the intervals in the input.
 
 
+// Examples
+
+// Input : intervals=[[1,3],[2,6],[8,10],[15,18]]
+// Output : [[1,6],[8,10],[15,18]]
+// Explanation : Since intervals [1,3] and [2,6] are overlapping we can merge them to form [1,6] intervals.
+
+// Input : [[1,4],[4,5]]
+// Output :  [[1,5]]
+// Explanation :  Since intervals [1,4] and [4,5] are overlapping we can merge them to form [1,5].
+
+
 import java.util.*;
 
 class Main{
