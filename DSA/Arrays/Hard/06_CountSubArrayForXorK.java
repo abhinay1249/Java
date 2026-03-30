6bb8a4e6536ebf71fd99df8f2417cd8173d81e87
abhinay1249 <abhinaymarise@gmail.com>
2026-03-30 23:44:38 +0530

    Arrays-H-06

--- DSA/Arrays/Hard/06_CountSubArrayForXorK.java
+++ DSA/Arrays/Hard/06_CountSubArrayForXorK.java
@@ -1,6 +1,18 @@
 // Problem Statement: Given an array of integers A and an integer B. 
 // Find the total number of subarrays having bitwise XOR of all elements equal to k.
 
+// Examples
+
+// Input: A = [4, 2, 2, 6, 4] , k = 6
+// Output: 4
+// Explanation: The subarrays having XOR of their elements as 6 are  [4, 2], [4, 2, 2, 6, 4], [2, 2, 6], [6]
+
+// Input: A = [5, 6, 7, 8, 9], k = 5
+// Output: 2
+// Explanation: The subarrays having XOR of their elements as 5 are [5] and [5, 6, 7, 8, 9]
+
+
+
 class Main {
 
     // ==================================== Brute Force Approach ======================================
