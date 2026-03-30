3bea54898a5a272480b6d4c05215659d3dde6c1e
abhinay1249 <abhinaymarise@gmail.com>
2026-03-30 23:42:37 +0530

    Arrays-H-04

--- DSA/Arrays/Hard/04_FourSum.java
+++ DSA/Arrays/Hard/04_FourSum.java
@@ -2,6 +2,19 @@
 // In short, you need to return an array of all the unique quadruplets [arr[a], arr[b], arr[c], arr[d]] such that their sum is equal to a given target.
 
 
+// Example 1:
+
+// Input :arr[] = [1,0,-1,0,-2,2], target = 0
+// Result: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+// Explanation:We have to find unique quadruplets from the array such that the sum of those elements is equal to the target sum given that is 0. The result obtained is such that the sum of the quadruplets yields 0.
+
+// Example 2:
+
+// Input : arr[] = [4,3,3,4,4,2,1,2,1,1], target = 9
+// Result: [[1,1,3,4],[1,2,2,4],[1,2,3,3]]
+// Explanation: The sum of all the quadruplets is equal to the target i.e. 9.
+
+
 import java.util.*;
 
 class Main {
