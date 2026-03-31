1ce5bf7f8e877b51fbf15e1c644a0650282d5ef4
abhinay1249 <abhinaymarise@gmail.com>
2026-03-31 23:19:41 +0530

    Arrays-H-11

--- DSA/Arrays/Hard/11_CountInversions.java
+++ DSA/Arrays/Hard/11_CountInversions.java
@@ -1,3 +1,11 @@
+// Problem Statement: Given an array of N integers, count the inversion of the array (using merge-sort).
+
+// Inversion of an array: for all i & j < size of array, if i < j then you have to find pair (A[i],A[j]) such that A[j] < A[i].
+
+
+
+
+
 import java.util.*;
 
 class Main {
