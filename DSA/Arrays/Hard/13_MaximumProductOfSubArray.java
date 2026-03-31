b3cc761df2537fd2f1f47520746a1ec02e39df40
abhinay1249 <abhinaymarise@gmail.com>
2026-03-31 23:23:53 +0530

    Arrays-H-13

--- DSA/Arrays/Hard/13_MaximumProductOfSubArray.java
+++ DSA/Arrays/Hard/13_MaximumProductOfSubArray.java
@@ -1,3 +1,16 @@
+// Problem Statement: Given an array that contains both negative and positive integers, find the maximum product subarray.
+
+// Examples
+
+// Input: Nums = [1,2,3,4,5,0]
+// Output: 120
+// Explanation: In the given array, 1×2×3×4×5 gives maximum product value.
+
+// Input: Nums = [1,2,-3,0,-4,-5]
+// Output: 20
+// Explanation : In the given array, (-4)×(-5) gives maximum product value.
+
+
 class Main {
     
     // ==================================== Brute Force Approach ===========================================
