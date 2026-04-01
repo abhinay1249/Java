4f65b845b40be3130d2cccb9396211f3e8fc0e97
abhinay1249 <abhinaymarise@gmail.com>
2026-04-01 22:09:17 +0530

    LinkedList -1DLL

---
+++ DSA/LinkedList/1D LinkedList/Basic.java
@@ -0,0 +1,26 @@
+class Node{
+
+    int data;
+    Node pointer;
+
+    Node(int data,Node pointer){
+        this.data = data;
+        this.pointer = pointer;
+    }
+
+    Node(int data){
+        this.data = data;
+        this.pointer = null;
+    }
+
+}
+
+class Main{
+    public static void main(String[] args) {
+        int[] nums = {4,1,5,3,2};
+        
+        Node y = new Node(nums[4],null);
+
+        System.out.println(y.data);
+    }
+}
