c5e5b7c63c4a5f31fdde224fb3d31ecd333be0af
abhinay1249 <abhinaymarise@gmail.com>
2026-04-01 23:32:58 +0530

    LinkedList -1DLL

---
+++ DSA/LinkedList/1D_LinkedList/ArrayListToLinkedList.java
@@ -0,0 +1,45 @@
+class Node{
+
+    int data;
+    Node pointer;
+
+    Node(int data, Node pointer){
+        this.data = data;
+        this.pointer = pointer;
+    }
+
+    Node(int data){
+        this.data = data;
+        this.pointer = null;
+    }
+    
+    
+    static Node convertArrayToLL(int[] nums){
+
+        int length = nums.length;
+
+        Node head = new Node(nums[0]);
+
+        Node mover = head;
+
+        for(int index = 1 ; index < length ; index++){
+            Node temp = new Node(nums[index]);
+            mover.pointer = temp;
+            mover = temp;
+        }
+
+        return head;
+    }
+
+}
+
+class Main{
+    public static void main(String[] args) {
+        int[] nums = {12,43,134,141,431,0};
+
+        Node head = Node.convertArrayToLL(nums); 
+
+        System.out.println(head.data);
+
+    }
+}
