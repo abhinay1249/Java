bd84db990472a16e5654debc189231069d5c6919
abhinay1249 <abhinaymarise@gmail.com>
2026-04-02 23:36:59 +0530

    Arrays-E-12

--- DSA/Arrays/Easy/12_MissingNumberInArray.java
+++ DSA/Arrays/Easy/12_MissingNumberInArray.java
@@ -1,6 +1,15 @@
 // Problem Statement: Given an integer N and an array of size N-1 containing N-1 numbers between 1 to N. 
 // Find the number(between 1 to N), that is not present in the given array..
 
+// Examples: 
+
+// Input: arr[] = [8, 2, 4, 5, 3, 7, 1]
+// Output: 6
+// Explanation: All the numbers from 1 to 8 are present except 6.
+
+// Input: arr[] = [1, 2, 3, 5]
+// Output: 4
+// Explanation: Here the size of the array is 4, so the range will be [1, 5]. The missing number between 1 to 5 is 4
 
 import java.util.*;
 
