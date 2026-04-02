160f5e7aefb0d217d717ca7b15a3c837f135bd50
abhinay1249 <abhinaymarise@gmail.com>
2026-04-02 23:34:21 +0530

    Arrays-E-08

--- DSA/Arrays/Easy/08_MoveZeroToEnd.java
+++ DSA/Arrays/Easy/08_MoveZeroToEnd.java
@@ -1,6 +1,15 @@
 // Problem Statement: You are given an array of integers, 
 // your task is to move all the zeros in the array to the end of the array and move non-negative integers to the front by maintaining their order.
 
+// Examples
+
+// Input: 1 ,0 ,2 ,3 ,0 ,4 ,0 ,1
+// Output: 1 ,2 ,3 ,4 ,1 ,0 ,0 ,0
+// Explanation: All the zeros are moved to the end and non-negative integers are moved to front by maintaining order
+
+// Input : 1,2,0,1,0,4,0
+// Output: 1,2,1,4,0,0,0
+// Explanation : All the zeros are moved to the end and non-negative integers are moved to front by maintaining order
 
 class Main {
 
