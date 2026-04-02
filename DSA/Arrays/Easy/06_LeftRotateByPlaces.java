eb24b473aab0674fb57752a97816efe7ebd2de5e
abhinay1249 <abhinaymarise@gmail.com>
2026-04-02 23:32:40 +0530

    Arrays-E-06

--- DSA/Arrays/Easy/06_LeftRotateByPlaces.java
+++ DSA/Arrays/Easy/06_LeftRotateByPlaces.java
@@ -1,5 +1,18 @@
 // Problem Statement: Given an array of integers, rotating array of elements by k elements left.
 
+// Examples
+
+// Input : nums = [1, 2, 3, 4, 5, 6, 7], k = 2, right
+// Output : [6, 7, 1, 2, 3, 4, 5]
+// Explanation : rotate 1 step to the right: [7, 1, 2, 3, 4, 5, 6]
+// rotate 2 steps to the right: [6, 7, 1, 2, 3, 4, 5] 
+
+// Input : nums = [1, 2, 3, 4, 5, 6], k=2, left
+// Output : [3, 4, 5, 6, 1, 2]
+// Explanation :rotate 1 step to the left: [2, 3, 4, 5, 6, 1]
+// rotate 2 steps to the left: [3, 4, 5, 6, 1, 2]
+
+
 class Main {
     
     // ============================ Brute Force Approach ============================================
