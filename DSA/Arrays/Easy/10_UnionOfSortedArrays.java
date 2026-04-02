d48b0909b05f9db5f68e6cec6f4934382b2dbd70
abhinay1249 <abhinaymarise@gmail.com>
2026-04-02 23:36:08 +0530

    Arrays-E-10

--- DSA/Arrays/Easy/10_UnionOfSortedArrays.java
+++ DSA/Arrays/Easy/10_UnionOfSortedArrays.java
@@ -4,6 +4,23 @@
 
 // NOTE: Elements in the union should be in ascending order.
 
+// Examples
+
+// Input:n = 5,m = 5 arr1[] = {1,2,3,4,5}  arr2[] = {2,3,4,4,5}
+// Output: {1,2,3,4,5}
+// Explanation: Common Elements in arr1 and arr2  are:  2,3,4,5
+// Distnict Elements in arr1 are : 1
+// Distnict Elemennts in arr2 are : No distinct elements.
+// Union of arr1 and arr2 is {1,2,3,4,5}
+
+// Input:n = 10,m = 7,arr1[] = {1,2,3,4,5,6,7,8,9,10}arr2[] = {2,3,4,4,5,11,12}
+// Output: {1,2,3,4,5,6,7,8,9,10,11,12}
+// Explanation: Common Elements in arr1 and arr2  are:  2,3,4,5
+// Distnict Elements in arr1 are : 1,6,7,8,9,10
+// Distnict Elemennts in arr2 are : 11,12
+// Union of arr1 and arr2 is {1,2,3,4,5,6,7,8,9,10,11,12}
+
+
 
 import java.util.*;
 
