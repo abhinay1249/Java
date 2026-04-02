3ee77aba2d4e6299c14ccbeef5e2c668007bac26
abhinay1249 <abhinaymarise@gmail.com>
2026-04-02 23:32:08 +0530

    Arrays-E-05

--- DSA/Arrays/Easy/05_LeftRotateByOne.java
+++ DSA/Arrays/Easy/05_LeftRotateByOne.java
@@ -3,6 +3,18 @@
 // Note: There is no need to return anything, just modify the given array.
 
 
+// Example 1:
+// Input : nums = [1, 2, 3, 4, 5]  
+// Output : [2, 3, 4, 5, 1]  
+// Explanation : Initially, nums = [1, 2, 3, 4, 5]  
+// Rotating once to the left results in nums = [2, 3, 4, 5, 1].
+
+// Example 2:
+// Input : nums = [-1, 0, 3, 6]  
+// Output : [0, 3, 6, -1]  
+// Explanation : Initially, nums = [-1, 0, 3, 6]  
+// Rotating once to the left results in nums = [0, 3, 6, -1].
+
 class Main {
     
     // ======================= Brute Force Approach =======================================
