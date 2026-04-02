286572109250b2937975845a6647af7b0027b1e9
abhinay1249 <abhinaymarise@gmail.com>
2026-04-02 23:33:57 +0530

    Arrays-E-07

--- DSA/Arrays/Easy/07_RightRotateByPlaces.java
+++ DSA/Arrays/Easy/07_RightRotateByPlaces.java
@@ -1,15 +1,5 @@
 // Problem Statement: Given an array of integers, rotating array of elements by k elements right.
 
-// Examples
-
-// Input: 1 ,0 ,2 ,3 ,0 ,4 ,0 ,1
-// Output: 1 ,2 ,3 ,4 ,1 ,0 ,0 ,0
-// Explanation: All the zeros are moved to the end and non-negative integers are moved to front by maintaining order
-
-// Input : 1,2,0,1,0,4,0
-// Output: 1,2,1,4,0,0,0
-// Explanation : All the zeros are moved to the end and non-negative integers are moved to front by maintaining order
-
 
 class Main {
     
