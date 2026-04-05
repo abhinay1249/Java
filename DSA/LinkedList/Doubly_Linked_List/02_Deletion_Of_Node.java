5fc21bb09fdccf3261509d82af2615684f2aae21
abhinay1249 <abhinaymarise@gmail.com>
2026-04-05 23:52:39 +0530

    Doubly LinkedList-DeletionOfValueNode

--- DSA/LinkedList/Doubly_Linked_List/02_Deletion_Of_Node.java
+++ DSA/LinkedList/Doubly_Linked_List/02_Deletion_Of_Node.java
@@ -122,7 +122,13 @@ class Node{
             return head;
         }
     
-    
+    // ================================ Removal Of Value Node ==============================================
+
+        private static Node deletionAtValue(Node k, int elementNode){
+            
+        }
+
+
     public static void main(String[] args){
         int[] nums = {1,2,3,4,5,6,7,8};
         // int[] nums = {1};
