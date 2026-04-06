926636f4118aeb7c606d28ce2f57c8639862a950
abhinay1249 <abhinaymarise@gmail.com>
2026-04-06 08:03:28 +0530

    Doubly Linked List Insertion  Of Nodes

---
+++ DSA/LinkedList/Doubly_Linked_List/03_Insertion_Of_Node.java
@@ -0,0 +1,86 @@
+class Node{
+    int data;
+    Node nextptr;
+    Node prevptr;
+    
+    Node(int data, Node nextptr, Node prevptr){
+        this.data = data;
+        this.nextptr = nextptr;
+        this.prevptr = prevptr;
+    }
+    
+    Node(int data){
+        this.data = data;
+        this.nextptr = null;
+        this.prevptr = null;
+    }
+    
+    // ================================ Convert Array into Doubly Linked List =====================================
+
+        private static Node convert(int[] nums){
+
+            int length = nums.length;
+
+            Node head = new Node(nums[0],null,null);
+
+            Node prev = head;
+
+            for(int i = 1 ; i < length ; i++){
+                Node temp = new Node(nums[i],null,prev);
+                prev.nextptr = temp;
+                prev = temp;
+            }
+
+            return head;
+        }
+
+    // ================================ Print Doubly Linked List ==============================================
+    
+        private static void print(Node head){
+
+            Node dll = head;
+
+            while(dll != null){
+                System.out.print(dll.data + " ");
+                dll = dll.nextptr;
+            }
+        }
+
+    // ================================ Insertion Of Node Before Head ==============================================
+
+        private static Node insertionAtBeforeHead(Node head, int val){
+            
+            Node newHead = new Node(val,head,null);
+            
+            head.prevptr = newHead;
+            
+            return newHead;
+        }
+
+    public static void main(String[] args){
+        int[] nums = {1,2,3,4,5,6,7,8};
+        // int[] nums = {1};
+        
+        Node head = convert(nums);
+        Node head_1 = convert(nums);
+        Node head_2 = convert(nums);
+        Node head_3 = convert(nums);
+        Node head_4 = convert(nums);
+        
+        System.out.print("Doubly Linked List --> ");
+        
+        Node dll = head;
+        
+        print(dll);
+        
+        System.out.println();
+        
+        System.out.print("Insertion Of Node Before Head --> ");
+        
+        Node insertionAtBeforeHead = insertionAtBeforeHead(head_1,10);
+        
+        print(insertionAtBeforeHead);
+        
+
+    }
+}
