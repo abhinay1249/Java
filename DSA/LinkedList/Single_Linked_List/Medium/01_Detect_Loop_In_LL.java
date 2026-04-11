9b75a7398e2668955e76623d58bc42c83a03fe5b
abhinay1249 <abhinaymarise@gmail.com>
2026-04-11 12:40:01 +0530

    SinglyLinkedList -M - 01

--- DSA/LinkedList/Single_Linked_List/Medium/01_Detect_Loop_In_LL.java
+++ DSA/LinkedList/Single_Linked_List/Medium/01_Detect_Loop_In_LL.java
@@ -1,1 +1,77 @@
+// Problem Statement: Given a Linked List, determine whether the linked list contains a cycle or not.
 
+
+
+import java.util.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int data, Node next){
+        this.data = data;
+        this.next = next;
+    }
+    
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+    
+    static Node convert(int[] nums){
+        int length = nums.length;
+        
+        Node head = new Node(nums[0]);
+        
+        Node mover = head;
+        
+        for(int index = 1 ; index < length ; index++){
+            Node temp = new Node(nums[index]);
+            mover.next = temp;
+            mover = temp;
+        }
+        
+        return head;
+    }
+    
+    static boolean detectLoop(Node head){
+        Node temp = head;
+        boolean flag = false;
+        
+        Map<Node,Integer> hm = new HashMap<>();
+        
+        while(temp != null){
+            if(hm.containsKey(temp)){
+                flag = true;
+                break;
+            }else{
+                hm.put(temp,hm.getOrDefault(temp,0)+1);
+            }
+            temp = temp.next;
+        }
+        return flag;
+    }
+    
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5};
+        
+        Node head = convert(nums);
+        
+        Node temp = head;
+        Node thirdNode = null;
+        int count = 1;
+        
+        while(temp.next != null){
+            if(count==3){
+                thirdNode = temp;
+            }
+            temp = temp.next;
+            count++;
+        }
+        
+        temp.next = thirdNode;
+        
+        boolean res = detectLoop(head);
+        System.out.println(res);
+    }
+}
