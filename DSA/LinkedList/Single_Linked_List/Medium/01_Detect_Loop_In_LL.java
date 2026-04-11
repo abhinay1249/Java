1488d6149e0d0bb76cc82c6e409cc9120191b763
abhinay1249 <abhinaymarise@gmail.com>
2026-04-11 12:43:25 +0530

    SinglyLinkedList -M - 01

--- DSA/LinkedList/Single_Linked_List/Medium/01_Detect_Loop_In_LL.java
+++ DSA/LinkedList/Single_Linked_List/Medium/01_Detect_Loop_In_LL.java
@@ -1,5 +1,15 @@
 // Problem Statement: Given a Linked List, determine whether the linked list contains a cycle or not.
 
+// Examples
+
+// Input: LL: 1 2 3 4 5
+// Output: True
+// Explanation: The last node with the value of 5 has its 'next' pointer pointing back to a previous node with the value of 3. 
+// This has resulted in a loop, hence we return true.
+
+// Input: LL: 1 2 3 4 9 9
+// Output: False
+// Explanation:  In this example, the linked list does not have a loop hence returns false.
 
 
 import java.util.*;
@@ -34,23 +44,23 @@ class Node{
         return head;
     }
     
-    static boolean detectLoop(Node head){
-        Node temp = head;
-        boolean flag = false;
-        
-        Map<Node,Integer> hm = new HashMap<>();
-        
-        while(temp != null){
-            if(hm.containsKey(temp)){
-                flag = true;
-                break;
-            }else{
-                hm.put(temp,hm.getOrDefault(temp,0)+1);
+        static boolean detectLoop(Node head){
+            Node temp = head;
+            boolean flag = false;
+            
+            Map<Node,Integer> hm = new HashMap<>();
+            
+            while(temp != null){
+                if(hm.containsKey(temp)){
+                    flag = true;
+                    break;
+                }else{
+                    hm.put(temp,hm.getOrDefault(temp,0)+1);
+                }
+                temp = temp.next;
             }
-            temp = temp.next;
+            return flag;
         }
-        return flag;
-    }
     
     public static void main(String[] args) {
         int[] nums = {1,2,3,4,5};
