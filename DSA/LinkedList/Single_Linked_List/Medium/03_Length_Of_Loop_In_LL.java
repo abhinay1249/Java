e2b8fd2f4f486f0308e4f3cc5a99004ebce3be7d
abhinay1249 <abhinaymarise@gmail.com>
2026-04-12 10:55:24 +0530

    Singly Linked List - E - 03 Brute Force

---
+++ DSA/LinkedList/Single_Linked_List/Medium/03_Length_Of_Loop_In_LL.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int data, Node next){
+        this.data = data;
+        this.next = next;
+    }
+    
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+    
+    // ============================== Convert Array into Linked List ==========================
+
+        static Node convert(int[] nums){
+            int length = nums.length;
+
+            Node head = new Node(nums[0]);
+
+            Node mover = head;
+
+            for(int index = 1 ; index < length ; index++){
+                Node temp = new Node(nums[index]);
+                mover.next = temp;
+                mover = temp;
+            }
+
+            return head;
+        }
+        
+    // ================================ Brute Force Approach ===================================
+
+        static int lengthOfCycle(Node head){
+            Node temp = head;
+
+            Map<Node,Integer> hs = new HashMap<>();
+            List<Node> al = new ArrayList<>();
+
+            while(temp != null){
+                if(hs.containsKey(temp)){                       // T.C = O(N) + O(N * K), S.C = O(2N)
+                    if(!(al.contains(temp))){
+                        al.add(temp);
+                    }else{
+                        break;   
+                    }
+                }else{
+                    hs.put(temp,1);
+                }
+                temp = temp.next;
+            }
+            return al.size();
+        }
+
+    
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5};
+        
+        Node head = convert(nums);
+        
+        Node temp = head;
+        Node thirdNode = null;
+        int count = 1;
+        
+        while(temp.next != null){
+            if(count==3){
+                thirdNode = temp;
+            }
+            temp = temp.next;
+            count++;
+        }
+        
+        temp.next = thirdNode;
+        
+        int res = lengthOfCycle(head);
+
+        System.out.print(res);
+        
+        System.out.println();
+    }
+}
