d74743eb2680a0a9d34b776cf6454b944e6ab75c
abhinay1249 <abhinaymarise@gmail.com>
2026-04-12 19:57:42 +0530

    Singly Linked List - E - 04 Brute Force

--- DSA/LinkedList/Single_Linked_List/Medium/04_LL_Is_Palindrome_Or_Not.java
+++ DSA/LinkedList/Single_Linked_List/Medium/04_LL_Is_Palindrome_Or_Not.java
@@ -14,3 +14,77 @@
 // Input: 1 -> 1 -> 2 -> 1
 // Output: false
 // Explanation: 1121 is not a palindrome.
+
+import java.util.*;
+
+class Node{
+    
+    int data;
+    Node next;
+    
+    Node(int data, Node next){
+        this.data = data;
+        this.next = next;
+    }
+    
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+    
+    // ============================== Convert Array into Linked List ==========================
+
+        public static Node convert(int[] nums){
+            int length = nums.length;
+
+            Node head = new Node(nums[0]);
+
+            Node mover = head;
+
+            for(int index = 1 ; index < length ; index++){
+                Node temp = new Node(nums[index]);
+                mover.next = temp;
+                mover = temp;
+            }
+            return head;
+        }
+
+    // ================================ Brute Force Approach ===================================
+
+        public static boolean isPalindrome(Node head){
+
+            if(head.next == null){
+                return true;
+            }
+
+            Node temp = head;
+            Stack<Integer> st = new Stack<>();
+
+            while(temp != null){
+                st.add(temp.data);
+                temp = temp.next;
+            }
+
+            temp = head;                                        // T.C = O(N) + O(N), S.C = O(N)
+
+            while(temp != null){
+                int num = st.peek();
+                if(num != temp.data){
+                    return false;
+                }
+                st.pop();
+                temp = temp.next;
+            }
+
+            return true;
+        }
+    
+    public static void main(String[] args) {
+        int[] nums = {1,2,2,1};
+        
+        Node head = convert(nums);
+        
+        boolean result = isPalindrome(head);
+        System.out.println(result);
+    }
+}
