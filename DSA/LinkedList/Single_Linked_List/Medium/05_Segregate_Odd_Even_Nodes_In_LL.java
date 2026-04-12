9fc3f5b3fb22e7e6adf707e8c8a5143bebe2fa04
abhinay1249 <abhinaymarise@gmail.com>
2026-04-12 22:40:46 +0530

    Singly Linked List-E-05

---
+++ DSA/LinkedList/Single_Linked_List/Medium/05_Segregate_Odd_Even_Nodes_In_LL.java
@@ -0,0 +1,3 @@
+// Problem Statement: Given the head of a singly linked list. Group all the nodes with odd indices followed by all the nodes with even indices and return the reordered list. 
+// Consider the 1st node to have index 1 and so on. 
+// The relative order of the elements inside the odd and even group must remain the same as the given input.
