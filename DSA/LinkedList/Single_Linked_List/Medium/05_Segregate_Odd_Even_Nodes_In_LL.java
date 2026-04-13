8d5c435c979f3071459e2c2d6a963413de8d8af1
abhinay1249 <abhinaymarise@gmail.com>
2026-04-13 15:56:51 +0530

    Singly Linked List-M-05

--- DSA/LinkedList/Single_Linked_List/Medium/05_Segregate_Odd_Even_Nodes_In_LL.java
+++ DSA/LinkedList/Single_Linked_List/Medium/05_Segregate_Odd_Even_Nodes_In_LL.java
@@ -99,6 +99,26 @@ class Node{
 
             return head;
         }
+
+    // ================================ Optimal Approach =====================================
+
+        public static Node segregateNodes_1(Node head){
+            Node odd = head;
+            Node even = head.next;
+
+            Node evenHead = head.next;
+
+            while(even != null && even.next != null){
+                odd.next = odd.next.next;
+                even.next = even.next.next;
+
+                odd = odd.next;
+                even = even.next;
+            }
+            odd.next = evenHead;
+
+            return head;
+        }
     
     public static void main(String[] args) {
         int[] nums = {1,2,3,4,5,6};
