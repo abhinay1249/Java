02860dd746eec11f12ee6c567ff125d17e931815
abhinay1249 <abhinaymarise@gmail.com>
2026-04-13 07:43:04 +0530

    Singly Linked List-M-05 Brute-Force

--- DSA/LinkedList/Single_Linked_List/Medium/05_Segregate_Odd_Even_Nodes_In_LL.java
+++ DSA/LinkedList/Single_Linked_List/Medium/05_Segregate_Odd_Even_Nodes_In_LL.java
@@ -13,3 +13,99 @@
 // Input: 1 → 3 →5 → Null
 // Output: 1 → 3 → 5 → Null
 // Explanation: As there are no Even Nodes in LinkedList, The Modified LinkedList is same as Original LinkedList.
+
+import java.util.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int data, Node next){
+        this.data = data;
+        this.next = next;
+    }
+    
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+    
+    // ============================== Convert Array into Linked List ==========================
+
+        public static Node convert(int[] nums){
+            int length = nums.length;
+
+            Node head = new Node(nums[0]);
+            Node mover = head;
+
+            for(int index = 1 ; index < length ; index++){
+                Node temp = new Node(nums[index]);
+                mover.next = temp;
+                mover = temp;
+            }
+            return head;
+        }
+
+    // ============================== Print the  Linked List ====================================
+
+        public static void print(Node head){
+            Node temp = head;
+
+            while(temp != null){
+                System.out.print(temp.data+" ");
+                temp = temp.next;
+            }
+        }
+
+    // ================================ Brute Force Approach =====================================
+
+        public static Node segregateNodes(Node head){
+
+            if(head == null || head.next == null){
+                return head;
+            }
+
+            Node temp = head;
+
+            List<Integer> al = new ArrayList<>();
+
+            while(temp != null && temp.next != null){
+                al.add(temp.data);
+                temp = temp.next.next;
+            }
+
+            if(temp != null){
+                al.add(temp.data);
+            }                                                       // T.C = O(N/2) + O(N/2) + O(N) = O(2N), S.C = O(N)
+
+            temp = head.next;
+
+            while(temp != null && temp.next != null){
+                al.add(temp.data);
+                temp = temp.next.next;
+            }
+            if(temp != null){
+                al.add(temp.data);
+            }
+
+            int index = 0;
+            temp = head;
+
+            while(temp != null){
+                temp.data = al.get(index);
+                index++;
+                temp = temp.next;
+            }
+
+            return head;
+        }
+    
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6};
+        
+        Node head = convert(nums);
+        
+        Node result = segregateNodes(head);
+        print(result);
+    }
+}
