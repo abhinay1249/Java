aa78e2eac35fb1e54e947b61d07964c9ffd1f0d3
abhinay1249 <abhinaymarise@gmail.com>
2026-04-13 22:55:59 +0530

    Singly Linked List -M-06 Brute Force

--- DSA/LinkedList/Single_Linked_List/Medium/06_Remove_Nth_Node_From_End.java
+++ DSA/LinkedList/Single_Linked_List/Medium/06_Remove_Nth_Node_From_End.java
@@ -1,3 +1,17 @@
+// Problem Statement: Given a linked list and an integer N, 
+// the task is to delete the Nth node from the end of the linked list and print the updated linked list.
+
+// Examples
+
+// Input:  5->1->2, N=2
+// Output: 5->2
+// Explanation: The 2nd node from the end of the linked list is 1. Therefore, we get this result after removing 1 from the linked list.
+
+// Input:  1->2->3->4->5, N=3
+// Output: 1->2->4->5
+// Explanation: The 3rd node from the end is 3, therefore, we remove 3 from the linked list.
+
+
 class Node{
     int data;
     Node next;
