d47d584270106a77eb98cdcf7c02c44847551fc9
abhinay1249 <abhinaymarise@gmail.com>
2026-04-14 08:11:02 +0530

    Singly Linked List-M-07 Brute Force

--- DSA/LinkedList/Single_Linked_List/Medium/07_Delete_Middle_Node_In_LL.java
+++ DSA/LinkedList/Single_Linked_List/Medium/07_Delete_Middle_Node_In_LL.java
@@ -18,59 +18,65 @@ class Node{
         this.next = null;
     }
     
-    public static Node convert(int[] nums){
-        int length = nums.length;
-        
-        Node head = new Node(nums[0]);
-        Node mover = head;
-        
-        for(int index = 1 ; index < length ; index++){
-            Node temp = new Node(nums[index]);
-            mover.next = temp;
-            mover = temp;
-        }
-        return head;
-    }
-    
-    public static void print(Node head){
-        Node temp = head;
-        
-        while(temp != null){
-            System.out.print(temp.data + " ");
-            temp = temp.next;
-        }
-    }
-    
-    public static Node removeMiddleNode(Node head){
-        
-        if(head.next == null){
-            return head.next;
-        }
-        
-        Node temp = head;
-        int count = 0;
-        
-        while(temp != null){
-            count++;
-            temp = temp.next;
-        }
-        
-        int middleNode = (count/2) + 1;
-        int counter = 0;
-        temp = head;
-        Node prev = null;
-        
-        while(temp != null){
-            counter++;
-            if(counter == middleNode){
-                prev.next = temp.next;
-                break;
+    // ============================== Convert Array into Linked List ==========================
+
+        public static Node convert(int[] nums){
+            int length = nums.length;
+
+            Node head = new Node(nums[0]);
+            Node mover = head;
+
+            for(int index = 1 ; index < length ; index++){
+                Node temp = new Node(nums[index]);
+                mover.next = temp;
+                mover = temp;
             }
-            prev = temp;
-            temp = temp.next;
+            return head;
         }
-        return head;
-    }
+
+    // ============================== Print the Linked List ====================================
+
+        public static void print(Node head){
+            Node temp = head;
+
+            while(temp != null){
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+    // ================================ Brute Force Approach =====================================
+
+        public static Node removeMiddleNode(Node head){
+
+            if(head.next == null){
+                return head.next;
+            }
+
+            Node temp = head;
+            int count = 0;
+
+            while(temp != null){
+                count++;
+                temp = temp.next;
+            }
+
+            int middleNode = (count/2) + 1;                         // T.C = O(N) + O(N/2), S.C = O(1)
+            int counter = 0;
+            temp = head;
+            Node prev = null;
+
+            while(temp != null){
+                counter++;
+                if(counter == middleNode){
+                    prev.next = temp.next;
+                    break;
+                }
+                prev = temp;
+                temp = temp.next;
+            }
+            return head;
+        }
     
     public static void main(String[] args) {
         int[] nums = {1,2,3,4,5,6};
