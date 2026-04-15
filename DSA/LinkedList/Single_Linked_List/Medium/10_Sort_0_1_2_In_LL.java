9beb3092e1c5136243df9dd7257476df9c461a0e
abhinay1249 <abhinaymarise@gmail.com>
2026-04-15 23:20:22 +0530

    Singly Linked List-M-10

--- DSA/LinkedList/Single_Linked_List/Medium/10_Sort_0_1_2_In_LL.java
+++ DSA/LinkedList/Single_Linked_List/Medium/10_Sort_0_1_2_In_LL.java
@@ -1,1 +1,9 @@
 // Problem Statement: Given a linked list containing only 0's, 1's, and 2's, sort the linked list by rearranging the links (not by changing the data values).
+
+// Examples
+
+// Input: 1 -> 2 -> 0 -> 1 -> 0 -> 2 -> NULL
+// Output: 0 -> 0 -> 1 -> 1 -> 2 -> 2 -> NULL
+
+// Input: 2 -> 1 -> 2 -> 0 -> 0 -> 1 -> NULL
+// Output: 0 -> 0 -> 1 -> 1 -> 2 -> 2 -> NULL
