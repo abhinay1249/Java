44241251999d110f9f211cbe9823c58b32c2d1da
abhinay1249 <abhinaymarise@gmail.com>
2026-04-15 23:16:22 +0530

    Singly Linked List-M-09

--- DSA/LinkedList/Single_Linked_List/Medium/09_Sort_LL.java
+++ DSA/LinkedList/Single_Linked_List/Medium/09_Sort_LL.java
@@ -30,7 +30,7 @@ class Node{
         this.next = null;
     }
     
-    // ============================== Convert Array into Linked List ==========================
+    // ============================== Convert Array into Linked List =========================
 
         public static Node convert(int[] nums){
             int length = nums.length;
@@ -46,7 +46,7 @@ class Node{
             return head;
         }
 
-    // ============================== Print the Linked List ====================================
+    // ============================== Print the Linked List ===================================
 
         public static void print(Node head){
             Node temp = head;
@@ -57,7 +57,7 @@ class Node{
             }
         }
 
-    // ============================== Brute Force Approach ======================================
+    // ============================== Brute Force Approach =====================================
 
         public static Node sortList(Node head) {
 
