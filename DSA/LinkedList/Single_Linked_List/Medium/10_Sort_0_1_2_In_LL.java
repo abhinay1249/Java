6f79714ff4fcbdf7173512884e6d4de38afe5b0a
abhinay1249 <abhinaymarise@gmail.com>
2026-04-17 07:23:58 +0530

    Singly Linked List -M-10 BruteForce

--- DSA/LinkedList/Single_Linked_List/Medium/10_Sort_0_1_2_In_LL.java
+++ DSA/LinkedList/Single_Linked_List/Medium/10_Sort_0_1_2_In_LL.java
@@ -7,3 +7,78 @@
 
 // Input: 2 -> 1 -> 2 -> 0 -> 0 -> 1 -> NULL
 // Output: 0 -> 0 -> 1 -> 1 -> 2 -> 2 -> NULL
+
+import java.util.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int data, Node next){
+        this.data = data;
+        this.next = next;
+    }
+    
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+    
+    // ============================== Convert Array into Linked List =======================
+
+        public static Node convert(int[] nums){
+            int length = nums.length;
+
+            Node head = new Node(nums[0]);
+            Node mover = head;
+
+            for(int index = 1 ; index < length ; index++){
+                Node temp = new Node(nums[index]);
+                mover.next = temp;
+                mover = temp;
+            }
+            return head;
+        }
+
+    // ============================== Print the Linked List ================================
+
+        public static void print(Node head){
+            Node temp = head;
+
+            while(temp != null){
+                System.out.print(temp.data+" ");
+                temp = temp.next;
+            }
+        }
+    
+    // ============================== Brute Force Approach ==================================
+
+        public static Node sort012(Node head){
+            Node temp = head;
+            List<Integer> al = new ArrayList<>();
+
+            while(temp != null){
+                al.add(temp.data);
+                temp=temp.next;
+            }
+
+            Collections.sort(al);                           // T.C = O(N) + O(N LOG N) + O(N), S.C = O(N)
+            temp = head;
+            int index = 0;
+
+            while(temp != null){
+                temp.data = al.get(index);
+                index++;
+                temp = temp.next;
+            }
+            return head;
+        }
+
+    public static void main(String[] args) {
+        int[] nums = {0,2,1,2,1,2,0,0,1,1,1,0,0,0,2,2,0};
+        
+        Node head = convert(nums);
+        Node result = sort012(head);
+        print(result);
+    }
+}
