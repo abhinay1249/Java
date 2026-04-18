8bbc9b4c01dbedabde0d8394ed780d35a28c206a
abhinay1249 <abhinaymarise@gmail.com>
2026-04-18 15:50:56 +0530

    Singly Linked List -M-11 Brute-Force

--- DSA/LinkedList/Single_Linked_List/Medium/11_Intersection_Of_Two_LL.java
+++ DSA/LinkedList/Single_Linked_List/Medium/11_Intersection_Of_Two_LL.java
@@ -22,3 +22,77 @@
 
 // Output: Null
 // Explanation: Here, both lists do not intersect and thus no intersection node is present.
+
+import java.util.*;
+
+class Node{
+    
+    int data;
+    Node next;
+    
+    Node(int data, Node next){
+        this.data = data;
+        this.next = next;
+    }
+    
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+    
+    // =========================== Convert Array into Linked List ===============================
+
+        public static Node convert(int[] nums){
+            int length = nums.length;
+
+            Node head = new Node(nums[0]);
+            Node mover = head;
+
+            for(int index = 1 ; index < length ; index++){
+                Node temp = new Node(nums[index]);
+                mover.next = temp;
+                mover = temp;
+            }
+            return head;
+        }
+
+    // ================================ Brute Force Approach =====================================
+
+        public static Node intersectionNode(Node headA, Node headB) {
+
+            Node temp = headA;
+            Map<Node,Integer> hm = new HashMap<>();
+
+            while(temp != null){
+                hm.put(temp,1);
+                temp = temp.next;
+            }
+
+            temp = headB;
+            Node intersectionPoint = null;
+
+            while(temp != null){
+                if(hm.containsKey(temp)){
+                    intersectionPoint = temp;
+                    break;
+                }
+                temp = temp.next;
+            }
+
+            if(intersectionPoint != null){
+                return intersectionPoint;
+            }
+            return null;
+        }
+    public static void main(String[] args) {
+        int[] nums1 = {1,9,1,2,4};
+        int[] nums2 = {3,2,4};
+        
+        Node head1 = convert(nums1);
+        Node head2 = convert(nums2);
+        
+        Node result = intersectionNode(head1, head2);
+        
+        System.out.print(result);
+    }
+}
