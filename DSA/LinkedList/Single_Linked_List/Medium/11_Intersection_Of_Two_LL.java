300abfd0b51715bfc877e7387f53d3625cdfbde2
abhinay1249 <abhinaymarise@gmail.com>
2026-04-18 12:54:57 +0530

    Singly Linked List -M-11

--- DSA/LinkedList/Single_Linked_List/Medium/11_Intersection_Of_Two_LL.java
+++ DSA/LinkedList/Single_Linked_List/Medium/11_Intersection_Of_Two_LL.java
@@ -1,1 +1,24 @@
+// Problem Statement: Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. 
+// If the two linked lists have no intersection at all, return null.
 
+// Example 1:
+// Input: List 1 = [1,3,1,2,4], List 2 = [3,2,4]
+
+// List A: 1 → 3 → 1
+//                    ↘
+//                      2 → 4
+//                    ↗
+// List B:     3 → →
+
+// Output: 2
+// Explanation: Here, both lists intersecting nodes start from node 2.
+
+// Example 2:
+// Input: List1 = [1,2,7], List 2 = [2,8,1]
+
+// List A: 1 → 2 → 7
+
+// List B: 2 → 8 → 1
+
+// Output: Null
+// Explanation: Here, both lists do not intersect and thus no intersection node is present.
