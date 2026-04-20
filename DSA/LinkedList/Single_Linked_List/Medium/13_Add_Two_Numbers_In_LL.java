dedb9718a56626314ab59a020663c579cc31f260
abhinay1249 <abhinaymarise@gmail.com>
2026-04-20 23:50:57 +0530

    Singly Linked List-M-13

--- DSA/LinkedList/Single_Linked_List/Medium/13_Add_Two_Numbers_In_LL.java
+++ DSA/LinkedList/Single_Linked_List/Medium/13_Add_Two_Numbers_In_LL.java
@@ -1,1 +1,7 @@
 // Problem Statement: Add two numbers represented as Linked Lists.
+
+// Example 1:
+// Input: num1 = 243, num2 = 564
+// Output:sum = 807; L = [7,0,8]
+// Explanation: Since the digits are stored in reverse order, reverse the numbers first to get the or original number and then 
+// add them as → 342 + 465 = 807. 
