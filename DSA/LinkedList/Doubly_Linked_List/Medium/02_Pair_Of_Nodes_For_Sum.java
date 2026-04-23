6170ab01a1be866401f70d47afee6d246117aaae
abhinay1249 <abhinaymarise@gmail.com>
2026-04-23 07:43:27 +0530

    Doubly Linked List-M-02

---
+++ DSA/LinkedList/Doubly_Linked_List/Medium/02_Pair_Of_Nodes_For_Sum.java
@@ -0,0 +1,10 @@
+// Problem Statement: Given a doubly linked list that is sorted and a sum, find all pair of nodes whose sum is equal to the given sum.
+
+// Example 1:
+// Input: head = [1,2,3,4,9], sum = 5
+// Output: [[1,4],[2,3]]
+
+// Example 2:
+// Input: head = [1,2,3,4,5], sum = 10
+// Output: []
+
