ac8e048664f05081e4f0686862d1b9230251ed81
abhinay1249 <abhinaymarise@gmail.com>
2026-04-24 08:06:50 +0530

    Doubly Linked List-M-03 Approach

--- DSA/LinkedList/Doubly_Linked_List/Medium/03_Remove_Duplicates_From_DLL.java
+++ DSA/LinkedList/Doubly_Linked_List/Medium/03_Remove_Duplicates_From_DLL.java
@@ -12,3 +12,81 @@
 // Input: head -> 1 <-> 1 <-> 1 <-> 1 <-> 1 <-> 2
 // Output: head -> 1 <-> 2
 // Explanation: All duplicate occurences of 1 is deleted
+
+
+class Node{
+    int data;
+    Node next;
+    Node prev;
+    
+    Node(int data, Node next, Node prev){
+        this.data = data;
+        this.next = next;
+        this.prev = prev;
+    }
+    
+    Node(int data){
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+    
+    // =========================== Convert Array into Linked List =========================
+
+        private static Node convert(int[] nums){
+            int length = nums.length;
+            Node head = new Node(nums[0]);
+            Node prevNode = head;
+
+            for(int index = 1 ; index < length ; index++){
+                Node temp = new Node(nums[index]);
+                prevNode.next = temp;
+                prevNode = temp;
+            }
+            return head;
+        }
+    
+    // ============================== Print the Linked List ================================
+
+        private static void print(Node head){
+            Node temp = head;
+
+            while(temp != null){
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+    // ============================== Only Approach =======================================
+
+        public static Node removeDuplicates(Node head){
+            
+            if(head == null || head.next == null){
+                return head;
+            }
+            
+            Node temp = head;
+            Node nextNode = temp;
+            
+            while(temp != null){
+                nextNode = temp.next;
+                while(nextNode != null && nextNode.data == temp.data){
+                    nextNode = nextNode.next;
+                }
+                temp.next = nextNode;
+                if(nextNode!=null){
+                    nextNode.prev = temp;
+                }
+                
+                temp = nextNode;
+            }
+            return head;
+        }
+    public static void main(String[] args) {
+        int[] nums = {1,1,2,3,3,4,4,4,5,5,5,5,5,5,5,5,6};
+        
+        Node head = convert(nums);
+        Node result = removeDuplicates(head);
+        print(result);
+    }
+}
