a7e649fcc77b235a09483e9e2abab702eefa177e
abhinay1249 <abhinaymarise@gmail.com>
2026-04-24 07:51:58 +0530

    Doubly Linked List-M-03

--- DSA/LinkedList/Doubly_Linked_List/Medium/03_Remove_Duplicates_From_DLL.java
+++ DSA/LinkedList/Doubly_Linked_List/Medium/03_Remove_Duplicates_From_DLL.java
@@ -1,4 +1,4 @@
-// Problem Statement: Given the head of a doubly linked list with its values sorted in non-decreasing order. 
+// Problem Statement: Given the head of a doubly linked list with its values "Sorted" in non-decreasing order. 
 // Remove all duplicate occurrences of any value in the list so that only distinct values are present in the list.
 
 // Return the head of the modified linked list.
