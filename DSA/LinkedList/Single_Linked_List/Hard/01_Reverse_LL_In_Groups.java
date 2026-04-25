eff02338f32f3caa29676d00f1614200583fdec1
abhinay1249 <abhinaymarise@gmail.com>
2026-04-25 14:19:50 +0530

    Singly Linked List - H - 01

--- DSA/LinkedList/Single_Linked_List/Hard/01_Reverse_LL_In_Groups.java
+++ DSA/LinkedList/Single_Linked_List/Hard/01_Reverse_LL_In_Groups.java
@@ -12,3 +12,117 @@
 // Output : 3 -> 2 -> 1 -> 4 -> 5
 // Explanation :The groups 1 -> 2 -> 3 were reversed as 3 -> 2 -> 1.
 // Note that 4 -> 5 was not reversed.
+
+class Node{
+    
+    int data;
+    Node next;
+    
+    Node(int data, Node next){
+        this.data = data;
+        this.next = next;
+    }
+    
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+
+    // ============================ Convert Array into Linked List ===========================
+    
+        private static Node convert(int[] nums){
+            int length = nums.length;
+            Node head = new Node(nums[0]);
+            Node mover = head;
+
+            for(int index = 1 ; index < length ; index++){
+                Node temp = new Node(nums[index]);
+                mover.next = temp;
+                mover = temp;
+            }
+
+            return head;
+        }
+
+    // =============================== Print Linked List ========================================
+
+        private static void print(Node head){
+            Node temp = head;
+
+            while(temp != null){
+                System.out.print(temp.data +" ");
+                temp = temp.next;
+            }
+        }
+
+    // =============================== Only Approach ========================================
+
+        private static Node findKthNode(Node head, int k){
+            Node temp = head;
+            int count = 1;
+
+            while(temp!= null && count < k){
+                temp = temp.next;
+                count++;
+            }
+            return temp;
+        }
+
+        private static Node reverseLL(Node head){
+            Node temp = head;
+            Node prevNode = null;
+            Node front = null;
+
+            while(temp != null){
+                front = temp.next;
+                temp.next = prevNode;
+                prevNode = temp;
+                temp = front;
+            }
+
+            return prevNode;
+        }
+
+        public static Node reverseKthGroup(Node head, int k){
+            if(head == null || head.next == null){
+                return head;
+            }
+
+            Node temp = head;
+            Node prevNode = null;
+
+            while(temp != null){
+
+                Node kthNode = findKthNode(temp,k);
+
+                if(kthNode == null){
+                    if(prevNode != null){
+                       prevNode.next = temp;                         // T.C = O(2N), S.C = O(1) 
+                    }                                                
+                    break;
+                }
+
+                Node nextNode = kthNode.next;
+                kthNode.next = null;
+                reverseLL(temp);
+
+                if(temp == head){
+                    head = kthNode;
+                }else{
+                    prevNode.next = kthNode;
+                }
+
+                prevNode = temp;
+                temp = nextNode;
+            }
+            return head;
+        }
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7,8,9,10};
+        int k = 3;
+        
+        Node head = convert(nums);
+        Node result = reverseKthGroup(head,k);
+        print(result);
+    }
+}
