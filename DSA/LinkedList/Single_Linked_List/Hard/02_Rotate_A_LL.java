1d47c8c040fe31cba43c54df558038069127ba5f
abhinay1249 <abhinaymarise@gmail.com>
2026-04-25 18:23:30 +0530

    Singly Linked List - H - 02 Brute Force

--- DSA/LinkedList/Single_Linked_List/Hard/02_Rotate_A_LL.java
+++ DSA/LinkedList/Single_Linked_List/Hard/02_Rotate_A_LL.java
@@ -14,3 +14,116 @@
 // List after 2 shift to right: head -> 4 -> 5 -> 1 -> 2 -> 3.
 // List after 3 shift to right: head -> 3 -> 4 -> 5 -> 1 -> 2.
 // List after 4 shift to right: head -> 2 -> 3 -> 4 -> 5 -> 1. 
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int data, Node next){
+        this.data = data;
+        this.next = next;
+    }
+    
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+
+    // ============================ Convert Array into Linked List ===========================
+    
+        private static Node convert(int[] nums){
+            int length = nums.length;
+            Node head = new Node(nums[0]);
+            Node mover = head;
+
+            for(int index = 1 ; index < length ; index++){
+                Node temp = new Node(nums[index]);
+                mover.next = temp;
+                mover = temp;
+            }
+
+            return head;
+        }
+
+    // =============================== Print Linked List ========================================
+
+        private static void print(Node head){
+            Node temp = head;
+
+            while(temp != null){
+                System.out.print(temp.data +" ");
+                temp = temp.next;
+            }
+        }
+
+    // =============================== Brute Force Approach ========================================
+
+        private static Node reverseLL(Node head){
+            Node temp = head;
+            Node prevNode = null;
+            Node front = null;
+
+            while(temp != null){
+                front = temp.next;
+                temp.next = prevNode;
+                prevNode = temp;
+                temp = front;
+            }
+
+            return prevNode;
+        }
+
+        private static int findLength(Node head){
+            Node temp = head;
+            int count = 0;
+
+            while(temp != null){
+                count++;
+                temp = temp.next;
+            }
+            return count;
+        }
+
+        public static Node rotateLL(Node head, int k){
+            if(head == null || head.next == null){
+                return head;
+            }
+
+            int length = findLength(head);
+
+            k %= length;
+
+            if(k==0){
+                return head;
+            }
+            Node reverseHead = reverseLL(head);
+            Node temp = reverseHead;
+            Node newHead = null;                                    // T.C = O(3N), S.C = O(1)
+            Node nextNode = null;
+
+            int count = 1;
+
+            while(temp != null){
+                if(count == k){
+                    nextNode = temp.next;
+                    temp.next = null;
+                    newHead = reverseLL(reverseHead);
+                    reverseLL(nextNode);
+                    break;
+                }
+                count++;
+                temp = temp.next;
+            }
+            reverseHead.next = head;
+
+            return newHead;
+        }
+    public static void main(String[] args) {
+        int[] nums = {10,20};
+        int k = 1;
+        
+        Node head = convert(nums);
+        Node result = rotateLL(head,k);
+        print(result);
+    }
+}
