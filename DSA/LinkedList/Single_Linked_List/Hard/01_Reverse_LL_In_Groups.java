f60535a42fc180f3bd80f3d6f1ae66ad31792c8a
abhinay1249 <abhinaymarise@gmail.com>
2026-04-25 11:36:46 +0530

    Singly Linked List - H - 01

---
+++ DSA/LinkedList/Single_Linked_List/Hard/01_Reverse_LL_In_Groups.java
@@ -0,0 +1,3 @@
+// Problem Statement: Given the head of a singly linked list containing integers, reverse the nodes of the list in groups of k and return the head of the modified list. 
+// If the number of nodes is not a multiple of k, then the remaining nodes at the end should be kept as is and not reversed.
+// Do not change the values of the nodes, only change the links between nodes.
