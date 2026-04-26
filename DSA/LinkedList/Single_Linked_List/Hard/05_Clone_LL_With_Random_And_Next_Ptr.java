21da9d99755eb4b27832b9bb495b72162cd18550
abhinay1249 <abhinaymarise@gmail.com>
2026-04-26 14:45:58 +0530

    Singly Linked List - H - 05

--- DSA/LinkedList/Single_Linked_List/Hard/05_Clone_LL_With_Random_And_Next_Ptr.java
+++ DSA/LinkedList/Single_Linked_List/Hard/05_Clone_LL_With_Random_And_Next_Ptr.java
@@ -3,3 +3,19 @@
 // ‘next’ which points to the next node in the list.
 // ‘random’ which points to a random node in the list or ‘null’.
 // Create a ‘deep copy’ of the given linked list and return it.
+
+// Example 1:
+
+// Input: [[1, -1], [2, 0], [3, 4], [4, 1], [5, 2]]
+// Output: 1 2 3 4 5, true
+// Explanation: All the nodes in the new list have same corresponding values as original nodes.
+// All the random pointers point to their corresponding nodes in the new list.
+// 'true' represents that the nodes and references were created new.
+
+// Example 2: 
+// Input: [[5, -1], [3, -1], [2, 1], [1, 1]]
+// Output: 5 3 2 1, true
+// Explanation: All the nodes in the new list have same corresponding values as original nodes.
+// All the random pointers point to their corresponding nodes in the new list.
+// 'true' represents that the nodes and references were created new.
+// [[5, -1], [3, -1], [2, -1], [1, -1]] will be incorrect, although it has the same values.
