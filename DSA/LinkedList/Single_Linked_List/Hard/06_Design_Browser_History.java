1500d8c1a22afa1af5f312aaa5e1d8d3f76430d9
abhinay1249 <abhinaymarise@gmail.com>
2026-04-26 19:26:55 +0530

    Singly Linked List - H - 06

--- DSA/LinkedList/Single_Linked_List/Hard/06_Design_Browser_History.java
+++ DSA/LinkedList/Single_Linked_List/Hard/06_Design_Browser_History.java
@@ -33,3 +33,76 @@
 // browserHistory.forward(2);                // You are in "linkedin.com", you cannot move forward any steps.
 // browserHistory.back(2);                   // You are in "linkedin.com", move back two steps to "facebook.com" then to "google.com". return "google.com"
 // browserHistory.back(7);                   // You are in "google.com", you can move back only one step to "leetcode.com". return "leetcode.com"
+
+
+    // === ========================= Only Approach ===========================
+
+        class Node{
+            String data;
+            Node next;
+            Node back;
+
+            Node(String data, Node next, Node back){
+                this.data = data;
+                this.next = next;
+                this.back = back;
+            }
+
+            Node(String data){
+                this.data = data;
+                this.next = null;
+                this.back = null;
+            }
+        }
+
+        class BrowserHistory {
+            Node current = null;
+            public BrowserHistory(String homepage) {
+                current = new Node(homepage);
+            }
+
+            public void visit(String url) {
+                Node newNode = new Node(url);
+                current.next =  newNode;
+                newNode.back = current;
+                current = newNode;
+            }
+
+            public String back(int steps) {
+                while(steps > 0){
+                    if(current.back != null){
+                        current = current.back;
+                    }else{
+                        break;
+                    }
+                    steps--;
+                }
+                return current.data;
+            }
+
+            public String forward(int steps) {
+                while(steps > 0){
+                    if(current.next != null){
+                        current = current.next;
+                    }else{
+                        break;
+                    }
+                    steps--;
+                }
+                return current.data;
+            }
+
+    public static void main(String[] args) {
+        BrowserHistory browserHistory = new BrowserHistory("leetcode.com");
+        browserHistory.visit("google.com");                             // You are in "leetcode.com". Visit "google.com"
+        browserHistory.visit("facebook.com");                           // You are in "google.com". Visit "facebook.com"
+        browserHistory.visit("youtube.com");                            // You are in "facebook.com". Visit "youtube.com"
+        System.out.println(browserHistory.back(1));                   // You are in "youtube.com", move back to "facebook.com" return "facebook.com"
+        System.out.println(browserHistory.back(1));                   // You are in "facebook.com", move back to "google.com" return "google.com"
+        System.out.println(browserHistory.forward(1));                // You are in "google.com", move forward to "facebook.com" return "facebook.com"
+        browserHistory.visit("linkedin.com");                           // You are in "facebook.com". Visit "linkedin.com"
+        System.out.println(browserHistory.forward(2));                // You are in "linkedin.com", you cannot move forward any steps.
+        System.out.println(browserHistory.back(2));                   // You are in "linkedin.com", move back two steps to "facebook.com" then to "google.com". return "google.com"
+        System.out.println(browserHistory.back(7));                   // You are in "google.com", you can move back only one step to "leetcode.com". return "leetcode.com"
+    }
+}
