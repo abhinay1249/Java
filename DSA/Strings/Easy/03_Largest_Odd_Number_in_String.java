43741a22183b4e1d7bb275e9b6eed6b6d8a28115
abhinay1249 <abhinaymarise@gmail.com>
2026-04-30 23:09:10 +0530

    Strings-E-03

--- DSA/Strings/Easy/03_Largest_Odd_Number_in_String.java
+++ DSA/Strings/Easy/03_Largest_Odd_Number_in_String.java
@@ -2,11 +2,24 @@
 // the largest-valued odd integer (as a string) that is a substring of the given string s.
 // The number returned should not have leading zero's. But the given input string may have leading zero.
 
+// Example 1
+// Input: s = "5347"
+// Output: "5347"
+// Explanation: The odd numbers formed by the given string are → 5, 3, 53, 347, 5347. 
+// The largest odd number without leading zeroes is 5347.
+
+// Example 2
+// Input: s = "0214638"
+// Output: "21463"
+// Explanation: The odd numbers formed by the string are → 1, 3, 21, 63, 463, 1463, 21463. 
+// We can't use numbers starting with 0, so the largest valid odd number is 21463.
+
+
 class Main {
 
     // ======================= Brute-Force Approach ===============================                                                     
 
-        static String largestOddNumberInString(String s){
+        public static String largestOddNumberInString(String s){
 
             int pointer = s.length()-1;
             int idx = -1;
