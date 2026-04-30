3e89ef15064778fbb212220568ab599b335de70c
abhinay1249 <abhinaymarise@gmail.com>
2026-04-30 23:16:11 +0530

    Strings-E-06

--- DSA/Strings/Easy/06_Rotate_String.java
+++ DSA/Strings/Easy/06_Rotate_String.java
@@ -2,6 +2,24 @@
 // A shift on s consists of moving the leftmost character of s to the rightmost position. 
 // For example, if s = "abcde", then it will be "bcdea" after one shift.
 
+// Example 1:
+
+// Input: s = "rotation", goal = "tionrota"
+// Output: true
+// Explanation: After multiple left shifts on "rotation", we get:
+//     1st shift → "otationr"
+//     2nd shift → "tationro"
+//     3rd shift → "ationrot"
+//     4th shift → "tionrota"
+//     So the goal string can be obtained by rotating the original string.
+
+// Example 2:
+// Input: s = "hello", goal = "lohelx"
+// Output: false
+// Explanation: Even after all possible rotations of "hello", we cannot form "lohelx" due to the presence of an extra character 'x'. 
+// Hence, it's not possible.
+            
+
 class Main{
 
     // ======================= Brute-Force Approach ===============================
