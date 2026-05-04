a12a869e0e28ba83795db60b740291c17e41fb0e
abhinay1249 <abhinaymarise@gmail.com>
2026-05-04 23:47:58 +0530

    Binary Search - E - 04

--- DSA/BinarySearch/1D-Arrays/04_FloorAndCeiLInSortedArray.java
+++ DSA/BinarySearch/1D-Arrays/04_FloorAndCeiLInSortedArray.java
@@ -2,6 +2,16 @@
 // The floor of x is the largest element in the array which is smaller than or equal to x. 
 // The ceiling of x is the smallest element in the array greater than or equal to x
 
+// Example 1:
+// Input Format: n = 6, arr[] ={3, 4, 4, 7, 8, 10}, x= 5
+// Result: 4 7
+// Explanation: The floor of 5 in the array is 4, and the ceiling of 5 in the array is 7.
+
+// Example 2:
+// Input Format: n = 6, arr[] ={3, 4, 4, 7, 8, 10}, x= 8
+// Result: 8 8
+// Explanation: The floor of 8 in the array is 8, and the ceiling of 8 in the array is also 8.
+
 
 class Main {
     
