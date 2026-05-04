f855404a093e688a81afef8fc0b27f380902f27e
abhinay1249 <abhinaymarise@gmail.com>
2026-05-04 23:57:43 +0530

    Binary Search - E - 08

--- DSA/BinarySearch/1D-Arrays/08_SearchInRotatedSortedArray-2.java
+++ DSA/BinarySearch/1D-Arrays/08_SearchInRotatedSortedArray-2.java
@@ -2,6 +2,17 @@
 // Now the array is rotated at some pivot point unknown to you. 
 // Return True if k is present and otherwise, return False.
 
+// Example 1:
+
+// Input Format: arr = [7, 8, 1, 2, 3, 3, 3, 4, 5, 6], k = 3
+// Result: True
+// Explanation: The element 3 is present in the array. So, the answer is True.
+
+// Example 2:
+
+// Input Format: arr = [7, 8, 1, 2, 3, 3, 3, 4, 5, 6], k = 10
+// Result: False
+// Explanation: The element 10 is not present in the array. So, the answer is False.
 
 class Main {
 
