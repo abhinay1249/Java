fe0808afc5a854b476e9375747ecf5fe8b1cb55d
abhinay1249 <abhinaymarise@gmail.com>
2026-05-04 23:56:49 +0530

    Binary Search - E - 07

--- DSA/BinarySearch/1D-Arrays/07_SearchInRotatedSortedArray.java
+++ DSA/BinarySearch/1D-Arrays/07_SearchInRotatedSortedArray.java
@@ -2,7 +2,16 @@
 // The array is rotated at some pivot point that is unknown. 
 // Find the index at which k is present and if k is not present return -1.
 
+// Examples
 
+// Input:nums = [4, 5, 6, 7, 0, 1, 2], k = 0
+// Output :4
+// Explanation : Here, the target is 0. We can see that 0 is present in the given rotated sorted array, nums. Thus, we get output as 4, which is the index at which 0 is present in the array.
+
+// Input: nums = [4, 5, 6, 7, 0, 1, 2], k = 3
+// Output :-1
+// Explanation :Here, the target is 3. Since 3 is not present in the given rotated sorted array. 
+// Thus, we get the output as -1.
 
 class Main {
 
