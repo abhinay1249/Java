b1175db2157e60c622c8b1486ae81ae259689142
abhinay1249 <abhinaymarise@gmail.com>
2026-05-06 20:54:20 +0530

    Arrays-E-10

--- DSA/Arrays/Easy/10_UnionOfSortedArrays.java
+++ DSA/Arrays/Easy/10_UnionOfSortedArrays.java
@@ -28,32 +28,32 @@ class Main {
 
     // ======================= Brute Force Approach USING MAP =======================================
 
-    static int[] unionOfSortedArrays(int[] arr_1,int[] arr_2){
-        
-        Map<Integer,Integer> freq = new HashMap<>();
-        
-        for(int i = 0; i < arr_1.length;i++){
-            freq.put(arr_1[i],freq.getOrDefault(arr_1[i],0)+1);
+        static int[] unionOfSortedArrays(int[] arr_1,int[] arr_2){
+
+            Map<Integer,Integer> freq = new HashMap<>();
+
+            for(int i = 0; i < arr_1.length;i++){
+                freq.put(arr_1[i],freq.getOrDefault(arr_1[i],0)+1);
+            }
+            for(int i = 0 ; i < arr_2.length;i++){
+                freq.put(arr_2[i],freq.getOrDefault(arr_2[i],0)+1);
+            }
+
+            int[] unionOfArrays = new int[freq.size()];
+            int idx = 0;
+
+            for(int i : freq.keySet()){              // T.C = O(N) + O(M) + O(X) + O(X LOG X) where X = No of Unique Elements, S.C = O(X)
+                unionOfArrays[idx]=i;
+                idx++;
+            }
+
+            Arrays.sort(unionOfArrays);
+
+            for(int i:unionOfArrays){
+                System.out.print(i+" ");
+            }
+            return unionOfArrays;
         }
-        for(int i = 0 ; i < arr_2.length;i++){
-            freq.put(arr_2[i],freq.getOrDefault(arr_2[i],0)+1);
-        }
-        
-        int[] unionOfArrays = new int[freq.size()];
-        int idx = 0;
-        
-        for(int i : freq.keySet()){
-            unionOfArrays[idx]=i;
-            idx++;
-        }
-        
-        Arrays.sort(unionOfArrays);
-        
-        for(int i:unionOfArrays){
-            System.out.print(i+" ");
-        }
-        return unionOfArrays;
-    }
     
     // ======================= Brute Force Approach USING SET =======================================
     
