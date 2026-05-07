43833d54cf95a852d38a232e3bb14720063f4a47
abhinay1249 <abhinaymarise@gmail.com>
2026-05-07 22:55:16 +0530

    Arrays-E-15

--- DSA/Arrays/Easy/15_LongestSubArrayOfSum.java
+++ DSA/Arrays/Easy/15_LongestSubArrayOfSum.java
@@ -2,6 +2,7 @@
 // find the length of the longest sub-array that sums to k. If no such sub-array exists, return 0.
 
 // Example 1:
+
 // Input: nums = [10, 5, 2, 7, 1, 9], k = 15  
 // Output: 4  
 // Explanation:The longest sub-array with a sum equal to 15 is [5, 2, 7, 1], which has a length of 4. 
@@ -9,6 +10,7 @@
 // Therefore, the length of this sub-array is 4.
 
 // Example 2:
+
 // Input: nums = [-3, 2, 1], k = 6  
 // Output: 0  
 // Explanation: There is no sub-array in the array that sums to 6. Therefore, the output is 0.
