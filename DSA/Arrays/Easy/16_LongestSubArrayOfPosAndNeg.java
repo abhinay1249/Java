cb06a47098c3b1b8c4c9dfcb0ef0da2b4db94a26
abhinay1249 <abhinaymarise@gmail.com>
2026-05-07 23:26:22 +0530

    Arrays-E-16

--- DSA/Arrays/Easy/16_LongestSubArrayOfPosAndNeg.java
+++ DSA/Arrays/Easy/16_LongestSubArrayOfPosAndNeg.java
@@ -7,77 +7,77 @@ class Main {
     
 	// ======================= Brute Force Approach =======================================
 
-    static int longestSubArray(int[] nums, int value){
+    	static int longestSubArray(int[] nums, int value){
 
-	    int maxSubArrayLength = 0;
-	    
-	    for(int i = 0; i < nums.length ; i++){
-	    	for(int j = i ; j < nums.length ; j++){
-	    		int sumOfElements = 0;
-	    		
-	    		for(int k = i ; k <= j; k++){
-	    			sumOfElements += nums[k];
-	    		}
-	    		if(sumOfElements == value){
-	    			maxSubArrayLength = Math.max(maxSubArrayLength, j-i+1);
-	    		}
-	    	}
-	    }
-	    if(maxSubArrayLength == 0){
-	    	return 0;
-	    }
-	    return maxSubArrayLength;
-    }
+		    int maxSubArrayLength = 0;
+		
+		    for(int i = 0; i < nums.length ; i++){
+		    	for(int j = i ; j < nums.length ; j++){
+		    		int sumOfElements = 0;
+				
+		    		for(int k = i ; k <= j; k++){
+		    			sumOfElements += nums[k];
+		    		}
+		    		if(sumOfElements == value){
+		    			maxSubArrayLength = Math.max(maxSubArrayLength, j-i+1);
+		    		}
+		    	}
+		    }
+		    if(maxSubArrayLength == 0){
+		    	return 0;
+		    }
+		    return maxSubArrayLength;
+    	}
 
 	// ======================= Better Approach ============================
 
-    static int longestSubArray_1(int[] nums, int value){
+    	static int longestSubArray_1(int[] nums, int value){
 
-	    int maxSubArrayLength = 0;
-	    
-	    for(int i = 0; i < nums.length ; i++){
-			int sumOfElements = 0;
-	    	for(int j = i ; j < nums.length ; j++){
-	    		sumOfElements += nums[j];
-	    		if(sumOfElements == value){
-	    			maxSubArrayLength = Math.max(maxSubArrayLength, j-i+1);
-	    		}
-	    	}
-	    }
-	    if(maxSubArrayLength == 0){
-	    	return 0;
-	    }
-	    return maxSubArrayLength;
-    }
+		    int maxSubArrayLength = 0;
+		
+		    for(int i = 0; i < nums.length ; i++){
+				int sumOfElements = 0;
+		    	for(int j = i ; j < nums.length ; j++){
+		    		sumOfElements += nums[j];
+		    		if(sumOfElements == value){
+		    			maxSubArrayLength = Math.max(maxSubArrayLength, j-i+1);
+		    		}
+		    	}
+		    }
+		    if(maxSubArrayLength == 0){
+		    	return 0;
+		    }
+		    return maxSubArrayLength;
+    	}
 
 	// ======================= Optimal Approach USING PREFIX SUM =======================
 
-    static int longestSubArray_2(int[] nums, int k){
+    	static int longestSubArray_2(int[] nums, int k){
 
-	    int prefixSum = 0;
-	    int maxSubArrayLength= 0;
-	    Map<Integer, Integer> prefixSumMap = new TreeMap<>();
-    
-	    for(int i = 0; i < nums.length ; i++){
-	    	prefixSum+=nums[i];
-    
-	    	if(prefixSum == k){
-	    		maxSubArrayLength = Math.max(maxSubArrayLength, i+1);	
-	    	}
+		    int prefixSum = 0;
+		    int maxSubArrayLength= 0;
+		    Map<Integer, Integer> prefixSumMap = new TreeMap<>();
+		
+		    for(int i = 0; i < nums.length ; i++){
+		    	prefixSum+=nums[i];
+			
+		    	if(prefixSum == k){
+		    		maxSubArrayLength = Math.max(maxSubArrayLength, i+1);	
+		    	}
 
-	    	else if(prefixSumMap.containsKey(prefixSum - k)){
-	    		maxSubArrayLength = Math.max(maxSubArrayLength, i - prefixSumMap.get(prefixSum-k));
-	    	}
-            if(!prefixSumMap.containsKey(prefixSum)){
-	    	    prefixSumMap.putIfAbsent(prefixSum,i);
-	        }
-	    }
+		    	else if(prefixSumMap.containsKey(prefixSum - k)){
+		    		maxSubArrayLength = Math.max(maxSubArrayLength, i - prefixSumMap.get(prefixSum-k));
+		    	}
+    	        if(!prefixSumMap.containsKey(prefixSum)){
+		    	    prefixSumMap.putIfAbsent(prefixSum,i);
+		        }
+		    }
 
-        if(maxSubArrayLength == 0){
-	    	return 0;
-	    }
-        return maxSubArrayLength;    
-    }
+    	    if(maxSubArrayLength == 0){
+		    	return 0;
+		    }
+    	    return maxSubArrayLength;    
+    	}
     public static void main(String[] args) {
         int[] nums = {-1,0,0,0,3,0,-1,1,1,2,-1,4,-1};
         int k = 1;
