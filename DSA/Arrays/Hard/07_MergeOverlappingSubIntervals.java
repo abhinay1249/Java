7c9625b6ab608377c5fb40ff64cf47435b121c98
abhinay1249 <abhinaymarise@gmail.com>
2026-05-13 08:07:59 +0530

    Arrays-H-07

--- DSA/Arrays/Hard/07_MergeOverlappingSubIntervals.java
+++ DSA/Arrays/Hard/07_MergeOverlappingSubIntervals.java
@@ -42,7 +42,7 @@ class Main{
                         continue;
                     }
                 }
-                
+
                 for(int idx_2 = idx_1+1 ; idx_2 < length ; idx_2++){
                     if(end>=nums[idx_2][0]){
                         end = Math.max(end, nums[idx_2][1]);
@@ -50,6 +50,7 @@ class Main{
                         break;
                     }
                 }
+                
                 mergeInterval.add(Arrays.asList(start,end));
             }
             return mergeInterval;
