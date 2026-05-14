f2ddd1eb005e91fbdfacbf9efad2d91464d90273
abhinay1249 <abhinaymarise@gmail.com>
2026-05-14 23:49:23 +0530

    Doubly Linked List - E - 01

--- DSA/LinkedList/Doubly_Linked_List/Easy/01_Array_To_Linked_List.java
+++ DSA/LinkedList/Doubly_Linked_List/Easy/01_Array_To_Linked_List.java
@@ -16,8 +16,8 @@ class Node{
     }
     
     // ============================== Convert Array into Linked List ==========================
-    
-        static Node convert(int[] nums){
+
+        public static Node convert(int[] nums){
             
             int length = nums.length;
             
