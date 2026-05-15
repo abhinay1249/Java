bd30ac5513f1299e29b05a23ec9aeca3ab0d05fe
abhinay1249 <abhinaymarise@gmail.com>
2026-05-15 23:54:19 +0530

    Binary Search - M -  02

--- DSA/BinarySearch/Medium/01_SquareRootOfNumber.java
+++ DSA/BinarySearch/Medium/01_SquareRootOfNumber.java
@@ -2,6 +2,16 @@
 // Your task is to find and return its square root. 
 // If ‘n’ is not a perfect square, then return the floor value of sqrt(n).
 
+// Examples
+
+// Input: N = 36
+// Output: 6
+// Explanation: Square root of 36 is 6. 
+
+// Input: N = 28
+// Output: 5
+// Explanation: Square root of 28 is approximately 5.292. So, the floor value will be 5. 
+
 class Main {
     
     // =========================== Brute Force Approach ==============================
