2ab8c0fb95f7633423943d1ac772cfde79b39d54
abhinay1249 <abhinaymarise@gmail.com>
2026-05-15 23:17:12 +0530

    Binary Search - 2D - 01

--- DSA/BinarySearch/2D-Arrays/01_FindRowsWithMaximumOne.java
+++ DSA/BinarySearch/2D-Arrays/01_FindRowsWithMaximumOne.java
@@ -2,12 +2,28 @@
 // All the rows are sorted in ascending order. Your task is to find the index of the row with the maximum number of ones. 
 // Note: If two rows have the same number of ones, consider the one with a smaller index. If there's no row with at least single 1, return -1
 
+// Example 1:
+// Input Format: n = 3, m = 3, 
+// mat[] = 
+// 1 1 1
+// 0 0 1
+// 0 0 0
+// Result: 0
+// Explanation: The row with the maximum number of ones is 0 (0 - indexed).
+
+// Example 2:
+// Input Format: n = 2, m = 2 , 
+// mat[] = 
+// 0 0
+// 0 0
+// Result: -1
+// Explanation:  The matrix does not contain any 1. So, -1 is the answer.
 
 class Main {
 
     // =========================== Brute Force Approach ==============================
     
-        static int rowWithMaximumOne(int[][]mat,int rowsLength,int colsLength){
+        public static int rowWithMaximumOne(int[][]mat,int rowsLength,int colsLength){
 
             int rowCount = -1;
             int row = -1;
@@ -32,7 +48,7 @@ class Main {
 
     // =========================== Optimal Approach ==============================
 
-        static int rowWithMaximumOne_1(int[][]mat,int rowsLength,int colsLength){
+        public static int rowWithMaximumOne_1(int[][]mat,int rowsLength,int colsLength){
         
             int rowCount = -1;
             int row = -1;
