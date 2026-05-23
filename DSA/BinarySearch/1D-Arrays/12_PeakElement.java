8ae4392092b00c17d37ac4d683defd7a279b5948
abhinay1249 <abhinaymarise@gmail.com>
2026-05-23 19:05:33 +0530

    Binary Search - 1D - 12

--- DSA/BinarySearch/1D-Arrays/12_PeakElement.java
+++ DSA/BinarySearch/1D-Arrays/12_PeakElement.java
@@ -2,6 +2,16 @@
 // Formally, if arr[i] is the peak element, arr[i - 1] < arr[i] and arr[i + 1] < arr[i]. 
 // Find the index(0-based) of a peak element in the array. If there are multiple peak numbers, return the index of any peak number.
 
+// Examples
+
+// Input: arr[] = {1,2,3,4,5,6,7,8,5,1}
+// Output: 7
+// Explanation: There is only 1 peak element, 8,  that is at index 7.
+
+// Input: arr[] = {1,2,1,3,5,6,4} 
+// Output: 1 
+// Explanation : There are 2 peak numbers that are at indices 1 and 5. We can return any of them.
+
 
 class Main {
 
