0404d5be92f3e59117376bf2900eeba50080db07
abhinay1249 <abhinaymarise@gmail.com>
2026-05-23 19:05:11 +0530

    Binary Search - 1D - 12

--- DSA/BinarySearch/1D-Arrays/12_PeakElement.java
+++ DSA/BinarySearch/1D-Arrays/12_PeakElement.java
@@ -1,3 +1,8 @@
+// Problem Statement: Given an array of length N, peak element is defined as the element greater than both of its neighbors. 
+// Formally, if arr[i] is the peak element, arr[i - 1] < arr[i] and arr[i + 1] < arr[i]. 
+// Find the index(0-based) of a peak element in the array. If there are multiple peak numbers, return the index of any peak number.
+
+
 class Main {
 
     // ======================= Brute Force Approach ===========================
