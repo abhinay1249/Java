a476f865f22abc02c1ce80e62ab20b5e17ac63c1
abhinay1249 <abhinaymarise@gmail.com>
2026-05-28 23:40:29 +0530

    Binary Search - M - 09

--- DSA/BinarySearch/Medium/09_AllocateMinimumPages.java
+++ DSA/BinarySearch/Medium/09_AllocateMinimumPages.java
@@ -11,6 +11,21 @@
 // If the allocation of books is not possible. return -1
 
 
+// Example 1:
+
+// Input Format: n = 4, m = 2, arr[] = {12, 34, 67, 90}
+// Result: 113
+// Explanation: The allocation of books will be 12, 34, 67 | 90. 
+// One student will get the first 3 books and the other will get the last one.
+
+
+// Example 2:
+
+// Input Format: n = 5, m = 4, arr[] = {25, 46, 28, 49, 24}
+// Result: 71
+// Explanation: The allocation of books will be 25, 46 | 28 | 49 | 24.
+
+
 import java.util.*;
 
 class Main {
