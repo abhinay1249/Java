248269d41be8be67ff7652c21b626c270e673f62
abhinay1249 <abhinaymarise@gmail.com>
2026-05-30 23:17:06 +0530

    Binary Search - M - 13

--- DSA/BinarySearch/Medium/13_MedianOf2SortedArrays.java
+++ DSA/BinarySearch/Medium/13_MedianOf2SortedArrays.java
@@ -2,6 +2,21 @@
 // The median is defined as the middle value of a sorted list of numbers. 
 // In case the length of the list is even, the median is the average of the two middle elements.
 
+// Example 1
+
+// Input: arr1 = [2, 4, 6], arr2 = [1, 3, 5]
+// Output: 3.5
+// Explanation: The array after merging arr1 and arr2 will be [ 1, 2, 3, 4, 5, 6 ].
+//  As the length of the merged list is even, the median is the average of the two middle elements.
+//   Here two medians are 3 and 4. So the median will be the average of 3 and 4, which is 3.5.
+
+
+// Example 2
+
+// Input: arr1 = [2, 4, 6], arr2 = [1, 3]
+// Output: 3.0
+// Explanation: The array after merging arr1 and arr2 will be [ 1, 2, 3, 4, 6 ]. The median is simply 3.
+
 
 import java.util.*;
 
