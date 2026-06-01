0be8acd9aada8e55ed35e31e06b4f9b940c15940
abhinay1249 <abhinaymarise@gmail.com>
2026-06-01 22:01:30 +0530

    Strings - E - 03

--- DSA/Strings/Easy/03_Largest_Odd_Number_in_String.java
+++ DSA/Strings/Easy/03_Largest_Odd_Number_in_String.java
@@ -16,80 +16,92 @@
 // Explanation: The odd numbers formed by the string are → 1, 3, 21, 63, 463, 1463, 21463. 
 // We can't use numbers starting with 0, so the largest valid odd number is 21463.
 
-
 class Main {
 
-    // ======================= Brute-Force Approach ===============================                                                     
+    // ======================= Brute-Force Approach ===============================
 
-        public static String largestOddNumberInString(String s){
+    public static String largestOddNumberInString(String s) {
 
-            int pointer = s.length()-1;
-            int idx = -1;
+        int pointer = s.length() - 1;
+        int idx = -1;
 
-            while(pointer >= 0){
-                char ch = s.charAt(pointer);
-                int number = Integer.valueOf(ch);           
+        while (pointer >= 0) {
+            char ch = s.charAt(pointer);
+            int number = Integer.valueOf(ch);
 
-                if(number % 2 == 0){
-                    pointer--;
-                }else{                                               // T.C = O(N) +  O(N), S.C = O(N)
-                    idx = pointer;
-                    break;
-                }
+            if (number % 2 == 0) {
+                pointer--;
+            } else { // T.C = O(N) + O(N), S.C = O(N)
+                idx = pointer;
+                break;
             }
-
-            StringBuilder word = new StringBuilder();
-
-            for(int index = 0 ; index <= idx ; index++){
-                char ch = s.charAt(index);
-
-                word.append(ch);
-            }
-            
-            return word.toString().replaceFirst("^0+","");
         }
+
+        StringBuilder word = new StringBuilder();
+
+        for (int index = 0; index <= idx; index++) {
+            char ch = s.charAt(index);
+
+            word.append(ch);
+        }
+
+        return word.toString().replaceFirst("^0+", "");
+    }
 
     // ======================= Optimal Approach - 1===============================
 
-        public static String largestOddNumberInString_1(String s){
-            
-            int index = -1;
-            int pointer = s.length()-1;
-            while(pointer >= 0){
-                char ch = s.charAt(pointer);
-                int num = ch - '0';
-                if(num % 2 == 0){
-                    pointer--;                                      // T.C = O(N), S.C = O(1)
-                }else{
-                    index = pointer;
-                    break;
-                }
-            }    
-            return s.substring(0,index+1).replaceFirst("^0+","");
+    public static String largestOddNumberInString_1(String s) {
+
+        int index = -1;
+        int pointer = s.length() - 1;
+        while (pointer >= 0) {
+            char ch = s.charAt(pointer);
+            int num = ch - '0';
+            if (num % 2 == 0) {
+                pointer--; // T.C = O(N), S.C = O(1)
+            } else {
+                index = pointer;
+                break;
+            }
         }
+        return s.substring(0, index + 1).replaceFirst("^0+", "");
+    }
 
-    // ======================= Optimal Approach  - 2===============================
+    // ======================= Optimal Approach - 2===============================
 
-        public static String largestOddNumberInString_2(String s){
-            
-            int index = -1;
-            int pointer = s.length()-1;
-            while(pointer >= 0){
-                char ch = s.charAt(pointer);
-                int num = ch - '0';
-                if(num % 2 == 0){
-                    pointer--;                                      // T.C = O(N), S.C = O(1)
-                }else{
-                    index = pointer;
+        public static String largestOddNumberInString_2(String num) {
+
+            int index = -1, idx = -1;
+
+            for (int i = 0; i < num.length(); i++) {
+                char ch = num.charAt(i);
+
+                if (ch != '0') {
+                    index = i;
                     break;
                 }
-            }    
-            return s.substring(0,index+1).replaceFirst("^0+","");
+            }
+
+            for (int i = num.length() - 1; i >= index; i--) {
+                char ch = num.charAt(i);
+
+                int x = Integer.parseInt(String.valueOf(ch));
+
+                if (x % 2 == 1) {
+                    idx = i;
+                    break;
+                }
+            }
+
+            if (idx == -1)
+                return "";
+
+            return num.substring(index, idx + 1);
         }
 
     public static void main(String[] args) {
-       String s = "002643183929089473";
-       String result = largestOddNumberInString_1(s);
-       System.out.println(result);
+        String s = "002643183929089473";
+        String result = largestOddNumberInString_1(s);
+        System.out.println(result);
     }
 }
