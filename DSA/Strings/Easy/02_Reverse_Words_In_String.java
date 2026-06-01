b2cc8947fb528e7041b58532dcaeb609e381de70
abhinay1249 <abhinaymarise@gmail.com>
2026-06-01 19:28:19 +0530

    Strings - E - 02

--- DSA/Strings/Easy/02_Reverse_Words_In_String.java
+++ DSA/Strings/Easy/02_Reverse_Words_In_String.java
@@ -3,13 +3,15 @@
 // Return a string with the words in reverse order, concatenated by a single space.
 
 
-// Examples
+// Example 1
 
 // Input: s = "welcome to the jungle"
 // Output: "jungle the to welcome"
 // Explanation: The words in the input string are "welcome", "to", "the", and "jungle". 
 // Reversing the order of these words gives "jungle", "the", "to", and "welcome". 
 // The output string should have exactly one space between each word.
+
+// Example 2
 
 // Input: s = " amazing coding skills "
 // Output: "skills coding amazing"
