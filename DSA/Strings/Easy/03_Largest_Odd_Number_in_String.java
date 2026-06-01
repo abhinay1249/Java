9ac65246f76b35c5a709ca88fba33713a50222bf
abhinay1249 <abhinaymarise@gmail.com>
2026-06-01 19:27:50 +0530

    Strings - E - 03

--- DSA/Strings/Easy/03_Largest_Odd_Number_in_String.java
+++ DSA/Strings/Easy/03_Largest_Odd_Number_in_String.java
@@ -3,12 +3,14 @@
 // The number returned should not have leading zero's. But the given input string may have leading zero.
 
 // Example 1
+
 // Input: s = "5347"
 // Output: "5347"
 // Explanation: The odd numbers formed by the given string are → 5, 3, 53, 347, 5347. 
 // The largest odd number without leading zeroes is 5347.
 
 // Example 2
+
 // Input: s = "0214638"
 // Output: "21463"
 // Explanation: The odd numbers formed by the string are → 1, 3, 21, 63, 463, 1463, 21463. 
