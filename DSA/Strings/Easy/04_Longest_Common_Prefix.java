e5b7f7109cf6ffffc0fd1968dd31de2e967f0c4f
abhinay1249 <abhinaymarise@gmail.com>
2026-06-02 12:44:43 +0530

    Strings - E - 04

--- DSA/Strings/Easy/04_Longest_Common_Prefix.java
+++ DSA/Strings/Easy/04_Longest_Common_Prefix.java
@@ -20,33 +20,6 @@ class Main {
 
     // ======================= Brute-Force Approach ===============================
 
-        public static String longestCommonPrefix(String[] s){
-
-            int elementLength = s[0].length();
-            
-            if(elementLength == 0){
-                return "";
-            }
-
-            int length = s.length;
-
-            StringBuilder prefix = new StringBuilder();                             // T.C = O(M * N-1) , S.C = O(Characters)=O(M)
-
-            for(int character = 0 ; character < elementLength ; character++){
-                char ch = s[0].charAt(character);
-
-                for(int strings = 1 ; strings < length ; strings++){
-                   if(character >= s[strings].length() || s[strings].charAt(character) != ch){
-                        return prefix.toString();
-                    }
-                }
-                prefix.append(ch);                      
-            }
-            return prefix.toString();
-        }
-
-    // ======================= Optimal Approach ===============================
-
         public static String longestCommonPrefix_1(String[] s){
 
             int elementLength = s[0].length();
@@ -73,6 +46,33 @@ class Main {
                 }
             }
             return s[0].substring(0,minLength);
+        }
+
+    // ======================= Optimal Approach ===============================
+
+        public static String longestCommonPrefix(String[] s){
+
+            int elementLength = s[0].length();
+            
+            if(elementLength == 0){
+                return "";
+            }
+
+            int length = s.length;
+
+            StringBuilder prefix = new StringBuilder();                             // T.C = O(M * N-1) , S.C = O(Characters)=O(M)
+
+            for(int character = 0 ; character < elementLength ; character++){
+                char ch = s[0].charAt(character);
+
+                for(int strings = 1 ; strings < length ; strings++){
+                   if(character >= s[strings].length() || s[strings].charAt(character) != ch){
+                        return prefix.toString();
+                    }
+                }
+                prefix.append(ch);                      
+            }
+            return prefix.toString();
         }
         
     public static void main(String[] args) {
