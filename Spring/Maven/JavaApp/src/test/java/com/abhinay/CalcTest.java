909fad69e84b4ea1e4c8a82950add2f7af6a9c66
abhinay1249 <abhinaymarise@gmail.com>
2026-06-08 21:53:39 +0530

    Spring - Maven - JavaApp

---
+++ Spring/Maven/JavaApp/src/test/java/com/abhinay/CalcTest.java
@@ -0,0 +1,12 @@
+package com.abhinay;
+
+import org.junit.Test;
+
+public class CalcTest {
+    @Test
+    void divideTest(){
+        Calc c = new Calc();
+        int result = c.divide(10,2);
+        int expected = 5;
+    }
+}
