8f5136cd6f5c814fee63a3cc879902bfefb52072
abhinay1249 <abhinaymarise@gmail.com>
2026-06-14 22:44:16 +0530

    Arrays - E - 11

--- DSA/Arrays/Easy/11_IntersectionOfSortedArrays.java
+++ DSA/Arrays/Easy/11_IntersectionOfSortedArrays.java
@@ -10,6 +10,12 @@
 // Output: {2,3,4,5}
 // Explanation: Common Elements in arr1 and arr2  are:  2,3,4,5
 
+// Example 2:
+
+// Input:n = 10, m = 7, arr1[] = {1,2,3,4,5,6,7,8,9,10}, arr2[] = {2,3,4,4,5,11,12}
+// Output: {2,3,4,5}
+// Explanation: Common Elements in arr1 and arr2  are:  2,3,4,5
+
 
 import java.util.ArrayList;
 import java.util.List;
