86fef218d31a9f8568b9e44de3b751af2482903d
abhinay1249 <abhinaymarise@gmail.com>
2026-06-14 22:31:37 +0530

    Arrays - E - 04

--- DSA/Arrays/Easy/04_RemoveDuplicates.java
+++ DSA/Arrays/Easy/04_RemoveDuplicates.java
@@ -5,11 +5,13 @@
 // then the first k elements of the array should hold the final result. It does not matter what you leave beyond the first k elements.
 
 
-// Examples
+// Example 1:
 
 // Input: arr[]=[1,1,2,2,2,3,3]
 // Output: [1,2,3,_,_,_,_]
 // Explanation: Total number of unique elements are 3, i.e[1,2,3] and Therefore return 3 after assigning [1,2,3] in the beginning of the array.
+
+// Example 2:
 
 // Input: arr[]=[1,1,1,2,2,3,3,3,3,4,4]
 // Output: [1,2,3,4,_,_,_,_,_,_,_]
