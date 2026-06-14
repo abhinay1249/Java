342703c9af2220689790b30a68c6ef0e183ac080
abhinay1249 <abhinaymarise@gmail.com>
2026-06-14 22:26:19 +0530

    Arrays - E - 02

--- DSA/Arrays/Easy/02_SecondLargestOrSmallest.java
+++ DSA/Arrays/Easy/02_SecondLargestOrSmallest.java
@@ -66,20 +66,20 @@ class Main {
     // ======================= Better Approach =======================================
     
         public static int secondLargest(int[] arr){
-            
+
             int n = arr.length;
-            
+
             if(n==0 || n==1) return -1;
-            
+
             int largest = arr[0];
-            
+
             for(int i = 1;i<n;i++){
                 if(arr[i]>largest){
                     largest = arr[i];
                 }
             }
             int slargest = Integer.MIN_VALUE;
-            
+
             for(int i = 0; i <n;i++){
                 if(arr[i]<largest){
                     slargest = Math.max(slargest,arr[i]);
@@ -90,45 +90,45 @@ class Main {
     
     // ======================= Optimal Approach =======================================
     
-    static int secondLargestElement(int[] arr){
-        
-        int n = arr.length;
-        
-        if(n==0 || n==1) return -1;
-        
-        int largest = arr[0];
-        int slargest = Integer.MIN_VALUE;
-        
-        for(int i = 1;i<n;i++){
-            if(arr[i]>largest){
-                slargest = largest;
-                largest = arr[i];
-            }else if(arr[i]<largest && arr[i]> slargest){
-                slargest = arr[i];
+        public static int secondLargestElement(int[] arr){
+            
+            int n = arr.length;
+            
+            if(n==0 || n==1) return -1;
+            
+            int largest = arr[0];
+            int slargest = Integer.MIN_VALUE;
+            
+            for(int i = 1;i<n;i++){
+                if(arr[i]>largest){
+                    slargest = largest;
+                    largest = arr[i];
+                }else if(arr[i]<largest && arr[i]> slargest){
+                    slargest = arr[i];
+                }
             }
+            return slargest;
         }
-        return slargest;
-    }
-    
-    static int secondSmallestElement(int[] arr){
         
-        int n = arr.length;
-        
-        if(n==0 || n==1) return -1;
-        
-        int smallest = arr[0];
-        int ssmallest = Integer.MAX_VALUE;
-        
-        for(int i = 1;i<n;i++){
-            if(arr[i]<smallest){
-                ssmallest = smallest;
-                smallest = arr[i];
-            }else if(arr[i]!=smallest && arr[i]<ssmallest){
-                ssmallest = arr[i];
+        public static int secondSmallestElement(int[] arr){
+            
+            int n = arr.length;
+            
+            if(n==0 || n==1) return -1;
+            
+            int smallest = arr[0];
+            int ssmallest = Integer.MAX_VALUE;
+            
+            for(int i = 1;i<n;i++){
+                if(arr[i]<smallest){
+                    ssmallest = smallest;
+                    smallest = arr[i];
+                }else if(arr[i]!=smallest && arr[i]<ssmallest){
+                    ssmallest = arr[i];
+                }
             }
+            return ssmallest;
         }
-        return ssmallest;
-    }
     
     
     public static void main(String[] args) {
