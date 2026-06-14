f3621417dfb85c4853e98be44c00d7410c428ccd
abhinay1249 <abhinaymarise@gmail.com>
2026-06-14 22:35:38 +0530

    Arrays - E - 07

--- DSA/Arrays/Easy/07_RightRotateByPlaces.java
+++ DSA/Arrays/Easy/07_RightRotateByPlaces.java
@@ -18,54 +18,55 @@ class Main {
     
     // ======================== Brute Force Approach ===========================================
 
-    static void rightRotateByPlaces(int[]arr,int k){
-        int n = arr.length;
-        k%=n;
-        if(n==0 || n==1) return;
-        
-        int[] temp = new int[k];
-        
-        
-        for(int i = n-1;i>=n-k;i--){
-            temp[i-(n-k)]=arr[i];
+        public static void rightRotateByPlaces(int[]arr,int k){
+            int n = arr.length;
+            k%=n;
+            if(n==0 || n==1) return;
+
+            int[] temp = new int[k];
+
+
+            for(int i = n-1;i>=n-k;i--){
+                temp[i-(n-k)]=arr[i];
+            }
+            for(int i = n-k-1; i>=0;i--){
+                arr[i+k]=arr[i];    
+            }
+            for(int i = 0;i<temp.length;i++){
+                arr[i]=temp[i];
+            }
         }
-        for(int i = n-k-1; i>=0;i--){
-            arr[i+k]=arr[i];    
-        }
-        for(int i = 0;i<temp.length;i++){
-            arr[i]=temp[i];
-        }
-    }
     
     
     // ======================= Optimal Approach =======================================
 
-    static void reverse(int start, int end, int[] arr){
-        while(start<end){
-            int temp = arr[start];
-            arr[start]=arr[end];
-            arr[end]=temp;
-            start++;
-            end--;
+        private static void reverse(int start, int end, int[] arr){
+            while(start<end){
+                int temp = arr[start];
+                arr[start]=arr[end];
+                arr[end]=temp;
+                start++;
+                end--;
+            }
         }
-    }
-
-    static void rightRotateByPlaces_1(int[]arr,int k){
-        int n = arr.length;
-        k%=n;
-        
-        if(n==0 || n==1) return;
-        
-        reverse(0,n-k-1,arr);
-        reverse(n-k,n-1,arr);
-        reverse(0,n-1,arr);
-    }
+    
+        public static void rightRotateByPlaces_1(int[]arr,int k){
+            int n = arr.length;
+            k%=n;
+            
+            if(n==0 || n==1) return;
+            
+            reverse(0,n-k-1,arr);
+            reverse(n-k,n-1,arr);
+            reverse(0,n-1,arr);
+        }
 
 
     public static void main(String[] args) {
         int[] arr ={1,2,3,4,5,6,7};
         int[] arr_1 ={1,2,3,4,5,6,7};
         int k = 5;
+        
         rightRotateByPlaces(arr,k);
         rightRotateByPlaces_1(arr_1,k);
         
