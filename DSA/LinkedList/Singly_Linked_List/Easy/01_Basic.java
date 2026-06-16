603a7eaea950439b6dde87b88328df0e075a393d
abhinay1249 <abhinaymarise@gmail.com>
2026-06-16 23:49:25 +0530

    Linked List - SLL - E - 01

--- DSA/LinkedList/Singly_Linked_List/Easy/01_Basic.java
+++ DSA/LinkedList/Singly_Linked_List/Easy/01_Basic.java
@@ -1,10 +1,19 @@
 //Problem Statement: Create a singly linked list and print the data of the last node.
 
-// Example:-
+// Example 1:-
+
 // Input: [4,1,5,3,2]
 // Output: 2
 // Explanation: The last node of the linked list contains the data 2,
 // which is the last element of the input array. Therefore, the output is 2.
+
+// Example 2:-
+
+// Input: [10,20,30,40,50]
+// Output: 50
+// Explanation: The last node of the linked list contains the data 50,
+// which is the last element of the input array. Therefore, the output is 50.
+
 
 class Node{
 
