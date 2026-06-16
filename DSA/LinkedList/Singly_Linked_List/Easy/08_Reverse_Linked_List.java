1b05c44972a1c96a6956d5a4489b4fe597dc2701
abhinay1249 <abhinaymarise@gmail.com>
2026-06-16 23:57:16 +0530

    Linked List - SLL - E - 08

--- DSA/LinkedList/Singly_Linked_List/Easy/08_Reverse_Linked_List.java
+++ DSA/LinkedList/Singly_Linked_List/Easy/08_Reverse_Linked_List.java
@@ -1,10 +1,21 @@
 // Problem Statement: Given the head of a singly linked list, write a program to reverse the linked list
 // and return the head pointer to the reversed list.
 
+// Example 1:-
+
 // Input-  LL: 1   3   2   4 
 // Output: 4
 // Explanation: After reversing the linked list, 
 // the new head will point to the tail of the old linked list.
+
+
+// Example 2:-
+
+// Input-  LL: 1   2   3   4   5
+// Output: 5
+// Explanation: After reversing the linked list,
+// the new head will point to the tail of the old linked list.
+
 
 import java.util.*;
 
