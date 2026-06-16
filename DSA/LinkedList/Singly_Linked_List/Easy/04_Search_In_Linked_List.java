d1f50b91c5053e654a965e8bcd3732bab707aa67
abhinay1249 <abhinaymarise@gmail.com>
2026-06-16 23:51:51 +0530

    Linked List - SLL - E - 04

--- DSA/LinkedList/Singly_Linked_List/Easy/04_Search_In_Linked_List.java
+++ DSA/LinkedList/Singly_Linked_List/Easy/04_Search_In_Linked_List.java
@@ -1,9 +1,17 @@
 // Problem Statement: Given the head of a linked list and an integer value, find out whether the integer is present in the linked list or not. 
 // Return true if it is present, or else return false.
 
-// Example:-
+// Example 1:-
+
 // Input: 12 -> 43 -> 134 -> 141 -> 431 -> 0, value = 134
 // Output: true
+// Explanation: The linked list contains the value 134, so the output is true.
+
+// Example 2:-
+
+// Input: 1 -> 2 -> 3 -> 4 -> 5, value = 10
+// Output: false
+// Explanation: The linked list does not contain the value 10, so the output is false.
 
 
 class Node{
