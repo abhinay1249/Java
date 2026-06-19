27688d3f13cdb6c119afcde44c4c59d5721b8e33
abhinay1249 <abhinaymarise@gmail.com>
2026-06-19 23:39:35 +0530

    Arrays-H-08

--- DSA/Arrays/Hard/08_MergeSortedArrays.java
+++ DSA/Arrays/Hard/08_MergeSortedArrays.java
@@ -2,18 +2,20 @@
 // The final sorted array should be stored inside the array nums1 and it should be done in-place.
 // Array nums1 has a length of m + n, where the first m elements denote the elements of nums1 and rest are 0s whereas nums2 has a length of n.
 
-// Examples
+// Example 1:
 
 // Input : nums1 = [-5, -2, 4, 5, 0, 0, 0], nums2 = [-3, 1, 8]
 // Output : [-5, -3, -2, 1, 4, 5, 8]
 // Explanation : The merged array is: [-5, -3, -2, 1, 4, 5, 8], where [-5, -2, 4, 5] are from nums1 and [-3, 1, 8] are from nums2
+
+// Example 2:
 
 // Input : nums1 = [0, 2, 7, 8, 0, 0, 0], nums2 = [-7, -3, -1]
 // Output :  [-7, -3, -1, 0, 2, 7, 8]
 // Explanation :  The merged array is: [-7, -3, -1, 0, 2, 7, 8], where [0, 2, 7, 8] are from nums1 and [-7, -3, -1] are from nums2
 
 
-import java.util.*;
+import java.util.Arrays;
 
 class Main{
 
@@ -67,9 +69,12 @@ class Main{
     public static void main(String[] args) {
         int[] nums1 = {0, 2, 7, 8, 0, 0, 0};
         int[] nums2 = {-7, -3, -1};
+
         int m = 4;
         int n = 3;
+        
         int[] result = mergeTwoSortedArray_1(nums1,nums2,m,n);
+        
         for(int values: result){
             System.out.print(values+" ");
         }
