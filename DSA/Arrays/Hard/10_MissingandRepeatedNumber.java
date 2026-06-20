46123d882b3daed06bd643ae1c1555cc983bca6c
abhinay1249 <abhinaymarise@gmail.com>
2026-06-20 23:38:37 +0530

    Arrays-H-10

--- DSA/Arrays/Hard/10_MissingandRepeatedNumber.java
+++ DSA/Arrays/Hard/10_MissingandRepeatedNumber.java
@@ -3,7 +3,6 @@
 
 // Note: You are not allowed to modify the original array.
 
-
 // Example 1:
 
 // Input : nums = [3, 5, 4, 1, 1]  
@@ -15,6 +14,7 @@
 // Input : nums = [1, 2, 3, 6, 7, 5, 7]  
 // Output : [7, 4]  
 // Explanation : 7 appears twice in the array, and 4 is missing from the array. So the output is [7, 4].
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -198,8 +198,6 @@ class Main {
             return numbers;
         }
     
-
-
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 5, 6, 6}; 
         int[] result = missingAndRepeated_2(nums);
