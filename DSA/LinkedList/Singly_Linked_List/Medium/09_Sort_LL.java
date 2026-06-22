2a55b37a0cafade4b79719cdd3d7bf28719b7215
abhinay1249 <abhinaymarise@gmail.com>
2026-06-22 23:32:35 +0530

    LinkedList - SLL - M - 09

--- DSA/LinkedList/Singly_Linked_List/Medium/09_Sort_LL.java
+++ DSA/LinkedList/Singly_Linked_List/Medium/09_Sort_LL.java
@@ -1,20 +1,22 @@
 // Problem Statement: Given a linked list, sort its nodes based on the data value in them. 
 // Return the head of the sorted linked list.
 
-// Examples:- 
+// Example 1:
 
 // Input: 3->4->2->1->5 
 // Output: 1->2->3->4->5
-
 // Explanation: The input linked list when sorted from [3, 4, 2, 1, 5] results in a linked list with values [1, 2, 3, 4, 5].
+
+// Example 2:
 
 // Input: 40->20->60->10->50->30
 // Output: 10->20->30->40->50->60
-
 // Explanation: The input linked list when sorted from [40, 20, 60, 10, 50, 30] results in a linked list with values [10, 20, 30, 40, 50, 60].
 
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 class Node{
     
