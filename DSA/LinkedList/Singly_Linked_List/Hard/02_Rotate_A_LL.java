ea62e68d9b73c98562566eed48710c3af5386ae3
abhinay1249 <abhinaymarise@gmail.com>
2026-06-26 23:20:10 +0530

    LinkedList -SLL -H -02

--- DSA/LinkedList/Singly_Linked_List/Hard/02_Rotate_A_LL.java
+++ DSA/LinkedList/Singly_Linked_List/Hard/02_Rotate_A_LL.java
@@ -1,4 +1,5 @@
-// Problem Statement: Given the head of a singly linked list containing integers, shift the elements of the linked list to the right by k places and return the head of the modified list. 
+// Problem Statement: Given the head of a singly linked list containing integers, shift the elements of the linked list to the
+//  right by k places and return the head of the modified list. 
 // Do not change the values of the nodes, only change the links between nodes.
 
 // Example 1:
@@ -16,6 +17,7 @@
 // List after 2 shift to right: head -> 4 -> 5 -> 1 -> 2 -> 3.
 // List after 3 shift to right: head -> 3 -> 4 -> 5 -> 1 -> 2.
 // List after 4 shift to right: head -> 2 -> 3 -> 4 -> 5 -> 1. 
+
 
 class Node{
     int data;
