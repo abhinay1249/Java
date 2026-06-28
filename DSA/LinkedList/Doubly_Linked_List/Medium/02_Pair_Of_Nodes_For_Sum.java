66fd69b0e4622cc036c1990b3536a36d69eae419
abhinay1249 <abhinaymarise@gmail.com>
2026-06-28 19:20:57 +0530

    LinkedList - DLL -M - 02

--- DSA/LinkedList/Doubly_Linked_List/Medium/02_Pair_Of_Nodes_For_Sum.java
+++ DSA/LinkedList/Doubly_Linked_List/Medium/02_Pair_Of_Nodes_For_Sum.java
@@ -1,4 +1,7 @@
-// Problem Statement: Given a doubly linked list that is sorted and a sum, find all pair of nodes whose sum is equal to the given sum.
+// Problem Statement: Given the head of a sorted doubly linked list of positive distinct integers 
+// and a target integer, return a 2D array containing all unique pairs of nodes (a, b) such that a + b == target.
+// Each pair should be returned as a 2-element array [a, b] with a < b. 
+// The list is sorted in ascending order. If there are no such pairs, return an empty list.
 
 // Example 1:
 
