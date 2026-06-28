2b19af323977148614d5b2ac1a7947aa574816bc
abhinay1249 <abhinaymarise@gmail.com>
2026-06-28 23:27:28 +0530

    Binary Search - 1D Arrays - 11

--- DSA/BinarySearch/1D-Arrays/11_SingleElementInSortedArray.java
+++ DSA/BinarySearch/1D-Arrays/11_SingleElementInSortedArray.java
@@ -1,9 +1,13 @@
 // Problem Statement: Given an array of N integers. 
 // Every number in the array except one appears twice. Find the single number in the array.
 
+// Example 1:
+
 // Input : arr[] = {1,1,2,2,3,3,4,5,5,6,6}
 // Output: 4
 // Explanation: Only the number 4 appears once in the array.
+
+// Example 2:
 
 // Input: arr[] = {1,1,3,5,5}
 // Output : 3
