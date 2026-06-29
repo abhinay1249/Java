000df6974b09694b798f1cab4d1815445b6952fb
abhinay1249 <abhinaymarise@gmail.com>
2026-06-29 23:34:43 +0530

    LinkedList -SLL -H - 02

--- DSA/LinkedList/Singly_Linked_List/Hard/02_Rotate_A_LL.java
+++ DSA/LinkedList/Singly_Linked_List/Hard/02_Rotate_A_LL.java
@@ -36,6 +36,7 @@ class Node{
     // ============================ Convert Array into Linked List =========================
     
         private static Node convert(int[] nums){
+            
             int length = nums.length;
             Node head = new Node(nums[0]);
             Node mover = head;
