df6448073fdf3f155dfab7ddd10357716157424e
abhinay1249 <abhinaymarise@gmail.com>
2026-07-01 23:33:05 +0530

    Recursion -B - 01

--- DSA/Recursion/Basics/01_String_To_Number.java
+++ DSA/Recursion/Basics/01_String_To_Number.java
@@ -11,3 +11,17 @@
 //    If the computed number is outside this range, return -2147483648 if the number is less than -2147483648, 
 //    or return 2147483647 if the number is greater than 2147483647.
 // 5. Finally, return the computed number after applying all the above steps.
+
+
+// Example 1:
+
+// Input: s = " -12345"  
+// Output: -12345  
+// Explanation: Ignore leading whitespaces. The sign '-' is encountered, indicating the number is negative.  
+// Digits 12345 are read and converted to -12345.
+
+// Example 2:
+
+// Input: s = "4193 with words"  
+// Output: 4193  
+// Explanation: Read the digits 4193 and stop when encountering the first non-digit character (w).
