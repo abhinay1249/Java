78f2784197a7d2074677a365d502772f4ac3f089
abhinay1249 <abhinaymarise@gmail.com>
2026-07-02 23:06:01 +0530

    Recursion - B - 01

--- DSA/Recursion/Basics/01_String_To_Number.java
+++ DSA/Recursion/Basics/01_String_To_Number.java
@@ -25,3 +25,68 @@
 // Input: s = "4193 with words"  
 // Output: 4193  
 // Explanation: Read the digits 4193 and stop when encountering the first non-digit character (w).
+
+
+
+class Main{
+
+    private static int computeResult(String s, int index, int result, int sign){
+        
+        if(index < s.length() || s.charAt(index) >= '0' && s.charAt(index) <= '9'){
+            
+            return sign * result;
+        }
+        
+        int number = 0;
+    
+        int digit = s.charAt(index) - '0';
+        
+        if(number == (Integer.MAX_VALUE/10)){
+            if(sign == 1){
+                if(digit >= 7){
+                    return Integer.MAX_VALUE;
+                }
+            }else if(sign == -1){
+                if(digit >= 8){
+                    return Integer.MIN_VALUE;
+                }
+            }
+        }
+            
+        if(number > (Integer.MAX_VALUE/10)){
+            if(sign == 1){
+                return Integer.MAX_VALUE;
+            }else if(sign == -1){
+                return Integer.MIN_VALUE;
+            }
+        }
+        
+        result *= 10;
+        result += digit;
+        computeResult(s,index++,result, sign);
+        return 0;
+    }
+
+    public static int myAtoi(String s){
+        int length = s.length();
+
+        int sign = 1;
+
+        for(int index = 0 ; index < length ; index++){
+            if(s.charAt(index) == '-'){
+                sign = -1;
+            }else if(s.charAt(index) == '+'){
+                sign = 1;
+            }else if(s.charAt(index) >= '0' && s.charAt(index) <= '9'){
+                int result = 0; 
+                return computeResult(s, index, result, sign);
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args){
+        String s = " `-12345";
+        System.out.println(myAtoi(s)); 
+    }
+}
