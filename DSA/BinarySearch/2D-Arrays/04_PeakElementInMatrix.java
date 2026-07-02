8404355af2383ab5a54e458e9d931e7a1a9755ab
abhinay1249 <abhinaymarise@gmail.com>
2026-07-02 23:28:06 +0530

    Binary Search - 2D - 04

--- DSA/BinarySearch/2D-Arrays/04_PeakElementInMatrix.java
+++ DSA/BinarySearch/2D-Arrays/04_PeakElementInMatrix.java
@@ -4,16 +4,18 @@
 
 // Note: As there can be many peak values, 1 is given as output if the returned index is a peak number, otherwise 0.
 
-// Example 1: Input: mat = [[5, 10, 8], [4, 25, 7], [3, 9, 6]]
+// Example 1: 
+// Input: mat = [[5, 10, 8], [4, 25, 7], [3, 9, 6]]
 // Output: [1, 1]
 // Explanation: The value at index [1, 1] is 25, which is a peak because all its neighbors (10, 7, 4, 9) are smaller.
 
-// Example 2: Input: mat = [[1, 2, 3], [6, 5, 4], [7, 8, 9]]
+// Example 2: 
+// Input: mat = [[1, 2, 3], [6, 5, 4], [7, 8, 9]]
 // Output: [2, 2]
 // Explanation: The value at index [2, 2] is 9, which is a peak as it is greater than its neighbors (8, 4).
 
 
-import java.util.*;
+import java.util.Arrays;
 
 class Main {
 
