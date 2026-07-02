8548cf82b187a14739d0c062d871ef4d5b83685e
abhinay1249 <abhinaymarise@gmail.com>
2026-07-02 23:33:59 +0530

    Binary Search - 2D - 04

--- DSA/BinarySearch/2D-Arrays/04_PeakElementInMatrix.java
+++ DSA/BinarySearch/2D-Arrays/04_PeakElementInMatrix.java
@@ -25,7 +25,7 @@ class Main {
 
     // =========================== Brute Force Approach ==============================
 
-        public static int[] searchElement(int[][] matrix, int target){
+        private static int[] searchElement(int[][] matrix, int target){
 
             for(int row = 0 ; row < matrix.length; row++){
                 for(int col = 0 ; col < matrix[0].length ; col++){
