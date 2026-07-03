f5d621934d36c832e26aa1602f252b15c8e9afe8
abhinay1249 <abhinaymarise@gmail.com>
2026-07-03 23:41:54 +0530

    Recursion - B - 02

--- DSA/Recursion/Basics/02_Power_Of_Number.java
+++ DSA/Recursion/Basics/02_Power_Of_Number.java
@@ -1,1 +1,13 @@
+// Problem Statement: Implement the power function pow(x, n) , which calculates the x raised to n i.e. xn.
 
+// Example 1:
+
+// Input: x = 2.0000, n = 10  
+// Output: 1024.0000  
+// Explanation: The answer is calculated as 2^10, which equals 1024.
+
+// Example 2:
+
+// Input: x = 2.0000, n = -2  
+// Output: 0.2500  
+// Explanation: The answer is calculated as 2^(-2), which is equal to 1/4 = 0.25.
