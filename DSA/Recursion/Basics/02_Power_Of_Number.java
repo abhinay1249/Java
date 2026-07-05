018165a3daa6ee6b93c91fa8d71f27640668a7cb
abhinay1249 <abhinaymarise@gmail.com>
2026-07-05 12:18:14 +0530

    Recursion - Basics - 02

--- DSA/Recursion/Basics/02_Power_Of_Number.java
+++ DSA/Recursion/Basics/02_Power_Of_Number.java
@@ -11,3 +11,38 @@
 // Input: x = 2.0000, n = -2  
 // Output: 0.2500  
 // Explanation: The answer is calculated as 2^(-2), which is equal to 1/4 = 0.25.
+
+
+class Main{
+    
+    public static double pow(double x, int n) {
+        if (n == 0) {
+            return 1.0;
+        }
+
+        long range = n;
+
+        if (n < 0) {
+            range = -1 * range;
+        }
+
+        double result = 1;
+
+        for(int index = 0 ; index < range; index++) {
+            result *= x;
+        }
+        
+        if(n < 0) {
+            result = 1 / result;
+        }
+
+        return result;
+
+    }
+    
+    public static void main(String[] args) {
+        double x = 2.0000;
+        int n = 10;
+        System.out.println(pow(x, n));
+    }
+}
