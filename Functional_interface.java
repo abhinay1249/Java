7e875b1c24d1be058701bdc7676084d1150cfe5b
abhinay1249 <abhinaymarise@gmail.com>
2026-07-05 13:47:15 +0530

    Function Interface

--- Functional_interface.java
+++ Functional_interface.java
@@ -1,32 +1,32 @@
 @FunctionalInterface
+
 interface Car  //All methods in an interface are implicitly public
 {
     void drive();
      
-    default void type()
-    {
+    default void type(){
         System.out.println("Vehicle");
     }
 
-    static void speed()
-    {
+    static void speed(){
         System.out.println("highest is 300");
     }
 }
 
 public class Functional_interface
 {
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
-        Car obj= new Car()
-         {
+        Car obj= new Car(){
+
             public void drive()
             {
                 System.out.println("Audi is being drove");
             }
-         }; 
-         Car.speed();
-         obj.drive();
+        }; 
+        
+        Car.speed();
+        obj.drive();
         obj.type(); //non static method should be called by an object
 
     }
