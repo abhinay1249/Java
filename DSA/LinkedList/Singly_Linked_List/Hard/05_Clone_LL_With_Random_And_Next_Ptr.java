fa4323a1c3a90080f6fdaa1e5c4ebe099e9ab7da
abhinay1249 <abhinaymarise@gmail.com>
2026-07-10 12:00:00 +0530

    Linked List - H - 05

--- DSA/LinkedList/Singly_Linked_List/Hard/05_Clone_LL_With_Random_And_Next_Ptr.java
+++ DSA/LinkedList/Singly_Linked_List/Hard/05_Clone_LL_With_Random_And_Next_Ptr.java
@@ -140,6 +140,7 @@ class Node{
         }
 
     public static void main(String[] args) {
+        
         Node head = new Node(1);
         head.next = new Node(2);
         head.next.next = new Node(3);
