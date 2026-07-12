531794c4bee6ffa78943df3e504c65bbaa320f38
abhinay1249 <abhinaymarise@gmail.com>
2026-07-12 12:34:06 +0530

    Recursion - Medium - 01

---
+++ DSA/Recursion/Medium/01_Generate_All_Binary_Strings.java
@@ -0,0 +1,5 @@
+// Problem Statement: Given an integer n, return all binary strings of length n that do not contain consecutive 1s. 
+// Return the result in lexicographically increasing order.
+
+// A binary string is a string consisting only of characters '0' and '1'.
+
