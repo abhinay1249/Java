279c98eaf42f5e046c416850eb220ec4ed254212
abhinay1249 <abhinaymarise@gmail.com>
2026-07-12 14:58:56 +0530

    Recursion - Medium - 02

---
+++ DSA/Recursion/Medium/02_Generate_Paranthesis.java
@@ -0,0 +1,13 @@
+// Problem Statement: Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+// Example 1:
+
+// Input: n = 3
+// Output: ["((()))", "(()())", "(())()", "()(())", "()()()"]
+// Explanation : Need to generate a list of parantheses of that n size and inserted into the list.
+
+// Example 2:
+
+// Input: n = 1
+// Output: ["()"]
+// Explanation : Need to generate a list of parantheses of that n size and inserted into the list.
