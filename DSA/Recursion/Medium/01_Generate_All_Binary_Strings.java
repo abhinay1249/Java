704149c94c43e853bf49e857349c0eb63940d2b0
abhinay1249 <abhinaymarise@gmail.com>
2026-07-12 12:36:16 +0530

    Recursion - Medium - 01

--- DSA/Recursion/Medium/01_Generate_All_Binary_Strings.java
+++ DSA/Recursion/Medium/01_Generate_All_Binary_Strings.java
@@ -3,3 +3,14 @@
 
 // A binary string is a string consisting only of characters '0' and '1'.
 
+// Example 1:
+
+// Input: n = 3  
+// Output: ["000", "001", "010", "100", "101"]  
+// Explanation: All binary strings of length 3 that do not contain consecutive 1s.
+
+// Example 2:
+
+// Input: n = 2  
+// Output: ["00", "01", "10"]  
+// Explanation: All binary strings of length 2 that do not contain consecutive 1s.
