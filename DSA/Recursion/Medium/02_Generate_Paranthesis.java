1bd7b8b73918e7159e85fc7bbdf560f0c0541dc9
abhinay1249 <abhinaymarise@gmail.com>
2026-07-13 23:58:54 +0530

    Recursion - Medium - 02

--- DSA/Recursion/Medium/02_Generate_Paranthesis.java
+++ DSA/Recursion/Medium/02_Generate_Paranthesis.java
@@ -30,7 +30,7 @@ class Main{
                 }
 
                 if(counter < 0){
-                    return false;                                   // T.C = O(), S.C = O()
+                    return false;                                   // T.C = O(2^2N * N), S.C = O(N^2)
                 }
             }
 
@@ -53,7 +53,9 @@ class Main{
 
     // =================================== Optimal Approach ====================================
 
-        public static List<String> generateParanthesis_1(String s, List<String> paranthesis, int n)
+        public static List<String> generateParanthesis_1(String s, List<String> paranthesis, int n){
+
+        }
 
     public static void main(String[] args) {
 
