097c4bbafe1d11ae197934efafa187685b259fb2
abhinay1249 <abhinaymarise@gmail.com>
2026-07-13 21:00:18 +0530

    Recursion - Medium - 02

--- DSA/Recursion/Medium/02_Generate_Paranthesis.java
+++ DSA/Recursion/Medium/02_Generate_Paranthesis.java
@@ -12,8 +12,53 @@
 // Output: ["()"]
 // Explanation : Need to generate a list of parantheses of that n size and inserted into the list.
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Main{
+
+    private static boolean validStrings(String s){
+        int counter = 0;
+
+        for(char ch : s.toCharArray()){
+            if(ch == '('){
+                counter++;
+            }else{
+                counter--;
+            }
+
+            if(counter < 0){
+                return false;
+            }
+        }
+
+        return counter == 0;
+    }
+
+    public static List<String> generateParanthesis(String s, List<String> paranthesis, int n){
+
+        if(s.length() == n){
+            if(validStrings(s)){
+                paranthesis.add(s);
+            }
+            return paranthesis;
+        }
+
+        generateParanthesis(s+'(', paranthesis, n);
+        generateParanthesis(s+')', paranthesis, n);
+
+        return paranthesis;
+    }
+
     public static void main(String[] args) {
-        
+
+        int n = 3;
+        List<String> paranthesis = new ArrayList<>();
+        List<String> result = generateParanthesis("", paranthesis, n);
+
+        for(int index = 0 ; index < result.size() ; index++){
+            System.out.print(result.get(index)+" ");
+        }
+
     }
 }
