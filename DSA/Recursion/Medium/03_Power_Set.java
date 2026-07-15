d0b8786f8c3919ca751c48878d96fab3bca8ee99
abhinay1249 <abhinaymarise@gmail.com>
2026-07-15 22:39:51 +0530

    Recursion -Medium - 03

--- DSA/Recursion/Medium/03_Power_Set.java
+++ DSA/Recursion/Medium/03_Power_Set.java
@@ -1,1 +1,13 @@
 // Problem Description: Given a string, find all the possible subsequences of the string.
+
+// Example 1:
+
+// Input: str = "abc"
+// Output: [a, ab, abc, ac, b, bc, c]
+// Explanation: Given string has 7 subsequences.
+
+// Example 2:
+
+// Input: str = "aa"
+// Output: [a, a, aa] 
+// Explanation: Given string has 3 subsequences.
