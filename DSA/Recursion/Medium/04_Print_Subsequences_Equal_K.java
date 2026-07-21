7ac60f248869e36853d8b6035e2d94c2d267c914
abhinay1249 <abhinaymarise@gmail.com>
2026-07-21 23:09:56 +0530

    Recursion - M - 04

--- DSA/Recursion/Medium/04_Print_Subsequences_Equal_K.java
+++ DSA/Recursion/Medium/04_Print_Subsequences_Equal_K.java
@@ -12,3 +12,14 @@
 // Input : nums = [4, 2, 10, 5, 1, 3] , k = 5
 // Output : [ [4, 1] , [2, 3] , [5] ]
 // Explanation : The possible subsets with sum k are [4, 1] , [2, 3] , [5].
+
+
+class Main{
+    public static void main(String[] args) {
+        
+        int[] nums = {1,2,1};
+        int k = 2;
+
+
+    }
+}
