3733ed02d0ab2dca67617beead2f8872ef2ffa48
abhinay1249 <abhinaymarise@gmail.com>
2026-07-21 23:08:42 +0530

    Recursion - M - 04

--- DSA/Recursion/Medium/04_Print_Subsequences_Equal_K.java
+++ DSA/Recursion/Medium/04_Print_Subsequences_Equal_K.java
@@ -1,3 +1,14 @@
-public class 04_Print_Subsequences_Equal_K {
-    
-}
+// Problem Statement: Given an array nums and an integer k.
+// Print the non-empty subsequences of nums such that the sum of all elements in the subsequence is equal to k.
+
+// Example 1:
+
+// Input : nums = [4, 9, 2, 5, 1] , k = 10
+// Output : [ [9, 1] , [4, 5, 1] ]
+// Explanation : The possible subsets with sum k are [9, 1] , [4, 5, 1].
+
+// Example 2:
+
+// Input : nums = [4, 2, 10, 5, 1, 3] , k = 5
+// Output : [ [4, 1] , [2, 3] , [5] ]
+// Explanation : The possible subsets with sum k are [4, 1] , [2, 3] , [5].
