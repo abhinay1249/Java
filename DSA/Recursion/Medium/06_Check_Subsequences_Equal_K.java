ccfd2cb876ba6927db77d2a171a1c778a6cde37a
abhinay1249 <abhinaymarise@gmail.com>
2026-07-23 23:25:17 +0530

    Recursion -M -06

--- DSA/Recursion/Medium/06_Check_Subsequences_Equal_K.java
+++ DSA/Recursion/Medium/06_Check_Subsequences_Equal_K.java
@@ -1,2 +1,14 @@
 // Problem Statement: Given an array nums and an integer k. 
 // Return true if there exist subsequences such that the sum of all elements in subsequences is equal to k else false.
+
+// Example 1:
+
+// Input : nums = [1, 2, 3, 4, 5] , k = 8
+// Output : Yes
+// Explanation : The subsequences like [1, 2, 5] , [1, 3, 4] , [3, 5] sum up to 8.
+
+// Example 2:
+
+// Input : nums = [4, 3, 9, 2] , k = 10
+// Output : No
+// Explanation : No subsequence can sum up to 10.
