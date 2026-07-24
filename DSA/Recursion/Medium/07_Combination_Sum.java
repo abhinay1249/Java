67a1973cbe9ea7c45d50bca63b2421ba79ab9802
abhinay1249 <abhinaymarise@gmail.com>
2026-07-24 23:28:30 +0530

    Recursion - Medium - 07

--- DSA/Recursion/Medium/07_Combination_Sum.java
+++ DSA/Recursion/Medium/07_Combination_Sum.java
@@ -12,8 +12,8 @@
 // Input: array = [2,3,6,7], target = 7
 // Output: [[2,2,3],[7]]
 // Explanation: 2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
-//              7 is a candidate, and 7 = 7.
-//              These are the only two combinations.
+// 7 is a candidate, and 7 = 7.
+// These are the only two combinations.
 
 
 // Example 2:
