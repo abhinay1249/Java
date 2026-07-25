2f897cdfa1e2c7d38d9e949ebfdf80adffd5ce2d
abhinay1249 <abhinaymarise@gmail.com>
2026-07-25 23:27:47 +0530

    Recursion - Medium - 08

--- DSA/Recursion/Medium/08_Combination_Sum-2.java
+++ DSA/Recursion/Medium/08_Combination_Sum-2.java
@@ -2,4 +2,14 @@
 // find all unique combinations in candidates where the candidate numbers sum to target. 
 // Each number in candidates may only be used once in the combination.
 
+// Example 1:
 
+// Input: candidates = [10,1,2,7,6,1,5], target = 8
+// Output: [ [1,1,6], [1,2,5], [1,7], [2,6]]
+// Explanation: These are the unique combinations whose sum is equal to target.
+ 
+// Example 2:
+
+// Input: candidates = [2,5,2,1,2], target = 5
+// Output: [[1,2,2],[5]]
+// Explanation: These are the unique combinations whose sum is equal to target.
