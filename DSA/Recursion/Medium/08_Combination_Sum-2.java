bf0222b208e69a6e53c85eeb3d826a1698f07762
abhinay1249 <abhinaymarise@gmail.com>
2026-07-26 11:02:43 +0530

    Recursion - Medium - 08

--- DSA/Recursion/Medium/08_Combination_Sum-2.java
+++ DSA/Recursion/Medium/08_Combination_Sum-2.java
@@ -19,7 +19,7 @@ import java.util.*;
 class Main{
 
 
-    // =================================== Brute Force Approach ========================================
+    // =================================== Brute Force Approach - 1========================================
 
         private static Set<List<Integer>> recursiveCombinationSum2(int index, Set<List<Integer>> hs, int target, int sum, int[] nums, List<Integer> ds){
 
@@ -60,13 +60,57 @@ class Main{
         }
 
 
+    // =========================================== Brute Force Approach - 2 ==============================================
+
+        private static Set<List<Integer>> recursiveCombinationSum2_1(int index, Set<List<Integer>> hs, int target, int[] nums, List<Integer> ds){
+
+
+            if(index == nums.length){
+                if(target == 0){
+                    List<Integer> temp = new ArrayList<>(ds);
+                    Collections.sort(temp);
+                    hs.add(temp);
+                }                                                   // T.C = O(2^N * N LOG N), S.C = O(N * 2^N)
+                return hs;
+            }
+
+            if(target >= nums[index]){
+                ds.add(nums[index]);
+                recursiveCombinationSum2_1(index+1, hs, target - nums[index], nums, ds);
+                ds.remove(ds.size()-1);
+            }
+
+            recursiveCombinationSum2_1(index+1, hs, target, nums, ds);
+
+            return hs;
+
+        }
+
+        public static List<List<Integer>> combinationSum2_1(int[] nums, int target){
+
+            int index = 0;
+            Set<List<Integer>> hs = new HashSet<>();
+            List<Integer> ds = new ArrayList<>();
+
+            recursiveCombinationSum2_1(index, hs, target, nums, ds);
+
+            List<List<Integer>> al = new ArrayList<>(hs);
+
+            return al;
+
+        }
+
+
     public static void main(String[] args){
         int[] nums = {10,1,2,7,6,1,5};
         int target = 8;
 
         List<List<Integer>> al = combinationSum2(nums,target);
+        List<List<Integer>> al_1 = combinationSum2_1(nums,target);
+
 
         System.out.println(al);
+        System.out.println(al_1);
 
     }
 }
