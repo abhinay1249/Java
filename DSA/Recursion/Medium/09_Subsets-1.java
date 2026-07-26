48a6510884b2e7dd2eea0abbaf576939c6fd7dcd
abhinay1249 <abhinaymarise@gmail.com>
2026-07-26 15:15:02 +0530

    Recursion - Medium - 09

--- DSA/Recursion/Medium/09_Subsets-1.java
+++ DSA/Recursion/Medium/09_Subsets-1.java
@@ -10,8 +10,18 @@
 
 // Example 2:
 
-// Input: N=3,arr[]= {3,1,2}
+// Input: N = 3, arr[]= {3,1,2}
 // Output: 0,1,2,3,3,4,5,6
 // Explanation: We have to find all the subset’s sum and print them.
 // In this case the generated subsets are [ [], [1], [2], [2,1], [3], [3,1], [3,2].
 // [3,2,1],so the sums we get will be  0,1,2,3,3,4,5,6
+
+
+class Main{
+
+
+
+    public static void main(String[] args){
+        
+    }
+}
