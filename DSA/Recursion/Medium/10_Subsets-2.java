604ec0e8f7d995efe96a4c4a174c78163cc9d544
abhinay1249 <abhinaymarise@gmail.com>
2026-07-26 18:59:11 +0530

    Recursion - Medium - 10

---
+++ DSA/Recursion/Medium/10_Subsets-2.java
@@ -0,0 +1,49 @@
+ 
+ 
+
+import java.util.*;
+
+class Main{
+    
+    private static Set<List<Integer>> recursiveSubsets(int index, Set<List<Integer>> hs, int[] nums, List<Integer> ds){
+        
+        if(index == nums.length){
+            List<Integer> sortedList = new ArrayList<>(ds);
+            Collections.sort(sortedList);
+            hs.add(sortedList);
+            return hs;
+        }
+
+        ds.add(nums[index]);
+        recursiveSubsets(index+1, hs, nums, ds);
+        ds.remove(ds.size()-1);
+        recursiveSubsets(index+1, hs, nums, ds);
+
+        return hs;
+
+    }
+
+
+    public static void subsetsWithDup(int[] nums) {
+
+        int index = 0;
+        Set<List<Integer>> hs = new HashSet<>();
+
+        List<Integer> ds = new ArrayList<>();
+
+        recursiveSubsets(index, hs, nums, ds);
+
+        List<List<Integer>> al = new ArrayList<>(hs);
+
+        System.out.println(al);        
+    }
+
+    public static void main(String[] args){
+        
+        int[] nums = {1,2,2};
+
+        subsetsWithDup(nums);
+
+    }
+}
+ 
