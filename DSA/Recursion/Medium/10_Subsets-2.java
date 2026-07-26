831e5c4ca65040976e800188b4fe8d44424b7c11
abhinay1249 <abhinaymarise@gmail.com>
2026-07-26 19:01:24 +0530

    Recursion - Medium - 10

--- DSA/Recursion/Medium/10_Subsets-2.java
+++ DSA/Recursion/Medium/10_Subsets-2.java
@@ -1,46 +1,59 @@
 // Problem Statement: Given an integer array nums, which can have duplicate entries, provide the power set. 
 // Duplicate subsets cannot exist in the solution set. Return the answer in any sequence.  
 
+// Example 1:
 
+// Input: array[] = [1,2,2]
+// Output: [ [ ],[1],[1,2],[1,2,2],[2],[2,2] ]
+// Explanation: We can have subsets ranging from  length 0 to 3. which are listed above. 
+// Also the subset [1,2] appears twice but is printed only once as we require only unique subsets.
+
+// Example 2:
+
+// Input: array[] = [1]
+// Output: [ [ ], [1] ]
+// Explanation: Only two unique subsets are available.
 
 
 
 import java.util.*;
 
 class Main{
+
+    // =================================== Brute Force Approach =======================================
     
-    private static Set<List<Integer>> recursiveSubsets(int index, Set<List<Integer>> hs, int[] nums, List<Integer> ds){
-        
-        if(index == nums.length){
-            List<Integer> sortedList = new ArrayList<>(ds);
-            Collections.sort(sortedList);
-            hs.add(sortedList);
+        private static Set<List<Integer>> recursiveSubsets(int index, Set<List<Integer>> hs, int[] nums, List<Integer> ds){
+
+            if(index == nums.length){
+                List<Integer> sortedList = new ArrayList<>(ds);
+                Collections.sort(sortedList);
+                hs.add(sortedList);
+                return hs;
+            }
+
+            ds.add(nums[index]);
+            recursiveSubsets(index+1, hs, nums, ds);
+            ds.remove(ds.size()-1);
+            recursiveSubsets(index+1, hs, nums, ds);
+
             return hs;
+
         }
 
-        ds.add(nums[index]);
-        recursiveSubsets(index+1, hs, nums, ds);
-        ds.remove(ds.size()-1);
-        recursiveSubsets(index+1, hs, nums, ds);
 
-        return hs;
+        public static void subsetsWithDup(int[] nums) {
 
-    }
+            int index = 0;
+            Set<List<Integer>> hs = new HashSet<>();
 
+            List<Integer> ds = new ArrayList<>();
 
-    public static void subsetsWithDup(int[] nums) {
+            recursiveSubsets(index, hs, nums, ds);
 
-        int index = 0;
-        Set<List<Integer>> hs = new HashSet<>();
+            List<List<Integer>> al = new ArrayList<>(hs);
 
-        List<Integer> ds = new ArrayList<>();
-
-        recursiveSubsets(index, hs, nums, ds);
-
-        List<List<Integer>> al = new ArrayList<>(hs);
-
-        System.out.println(al);        
-    }
+            System.out.println(al);        
+        }
 
     public static void main(String[] args){
 
