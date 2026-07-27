6dbf22d5fd9ee006172bf150328c28b96682b7ce
abhinay1249 <abhinaymarise@gmail.com>
2026-07-27 23:05:42 +0530

    Recursion - Medium - 11

--- DSA/Recursion/Medium/11_Combination_Sum-3.java
+++ DSA/Recursion/Medium/11_Combination_Sum-3.java
@@ -1,3 +1,7 @@
-public class 11_Combination_Sum-3 {
-    
-}
+// Problem Statement: Determine all possible set of k numbers that can be added together to equal n while meeting the following requirements:
+
+// 1. There is only use of numerals 1 through 9.
+// 2. A single use is made of each number.
+
+// Return list of every feasible combination that is allowed. 
+// The combinations can be returned in any order, but the list cannot have the same combination twice.
