6484fec63d31f707fe40883e15abdbb146c1e561
abhinay1249 <abhinaymarise@gmail.com>
2026-07-29 00:23:37 +0530

    Recursion -M -12

--- DSA/Recursion/Medium/12_Letter_Combinations_Of_Phone_Number.java
+++ DSA/Recursion/Medium/12_Letter_Combinations_Of_Phone_Number.java
@@ -1,2 +1,15 @@
 // Problem Statement: Given a string consisting of digits from 2 to 9 (inclusive). 
 // Return all possible letter combinations that the number can represent.
+
+// Example 1:
+
+// Input: digits = "34"
+// Output: [ "dg", "dh", "di", "eg", "eh", "ei", "fg", "fh", "fi" ]
+// Explanation: The 3 is mapped with "def" and 4 is mapped with "ghi".
+// So all possible combinations by replacing the digits with characters are shown in the output.
+
+// Example 2:
+
+// Input: digits = "3"
+// Output: [ "d", "e", "f" ]
+// Explanation: The 3 is mapped with "def".
