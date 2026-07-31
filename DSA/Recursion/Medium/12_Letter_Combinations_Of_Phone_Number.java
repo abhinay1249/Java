5e3729bdd7b51efd8906030cde76c9323c7d6f60
abhinay1249 <abhinaymarise@gmail.com>
2026-07-31 23:58:17 +0530

    Recursion -M -12

--- DSA/Recursion/Medium/12_Letter_Combinations_Of_Phone_Number.java
+++ DSA/Recursion/Medium/12_Letter_Combinations_Of_Phone_Number.java
@@ -38,7 +38,7 @@ class Main{
         phoneNumbers.put('6',"mno");
         phoneNumbers.put('7',"pqrs");
         phoneNumbers.put('8',"tuv");
-        phoneNumbers.put('8',"abc");
+        phoneNumbers.put('8',"wxyz");
 
 
 
