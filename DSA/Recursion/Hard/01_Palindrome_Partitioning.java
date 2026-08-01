7faa36aed05ac94c7843129b553e75e76771e80a
abhinay1249 <abhinaymarise@gmail.com>
2026-08-01 14:00:32 +0530

    Recursion -H -01

---
+++ DSA/Recursion/Hard/01_Palindrome_Partitioning.java
@@ -0,0 +1,21 @@
+// Problem Statement : Given a string s partition string s such that every substring of partition is palindrome. 
+// Return all possible palindrome partition of string s.
+
+// Example 1
+
+// Input : s = "aabaa"
+// Output : [ [ "a", "a", "b", "a", "a"] , [ "a", "a", "b", "aa"] , 
+// [ "a", "aba", "a"] , [ "aa", "b", "a", "a"] , [ "aa", "b", "aa" ] , [ "aabaa" ] ]
+// Explanation : Above all are the possible ways in which the string can be partitioned so that each substring is a palindrome.
+
+// Example 2
+
+// Input : s = "baa"
+// Output : [ [ "b", "a", "a"] , [ "b", "aa" ] ]
+// Explanation : Above all are the possible ways in which the string can be partitioned so that each substring is a palindrome.
+
+class Main{
+    public static void main(String[] args) {
+        
+    }
+}
