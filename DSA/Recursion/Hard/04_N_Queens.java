e400945a5676cb9f2b465f29668ed39adae8f887
abhinay1249 <abhinaymarise@gmail.com>
2026-08-05 22:20:47 +0530

    Recursion - H - 03

--- DSA/Recursion/Hard/04_N_Queens.java
+++ DSA/Recursion/Hard/04_N_Queens.java
@@ -1,3 +1,15 @@
 // Problem Statement: The n-queens is the problem of placing n queens on n × n chessboard such that no two queens can attack each other.
 //  Given an integer n, return all distinct solutions to the n -queens puzzle. 
 // Each solution contains a distinct boards configuration of the queen's placement, where ‘Q’ and ‘.’ indicate queen and empty space respectively.
+
+// Example 1:
+
+// Input: N = 4
+// Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+// Explanation: There exist two distinct solutions to the 4-queens puzzle as shown below
+
+// Example 2:
+
+// Input : N = 1
+// Output: [["Q"]]
+// Explanation : There is only one way to place 1 queen on 1x1 chessboard.
