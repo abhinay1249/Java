dec6c73243ff32f5325eff88cbb86c901f75db26
abhinay1249 <abhinaymarise@gmail.com>
2026-08-09 23:43:18 +0530

    Recursion -M -02

--- DSA/Recursion/Medium/02_Generate_Paranthesis.java
+++ DSA/Recursion/Medium/02_Generate_Paranthesis.java
@@ -2,14 +2,14 @@
 
 // Example 1:
 
-// Input: n = 3
-// Output: ["((()))", "(()())", "(())()", "()(())", "()()()"]
+// Input : n = 3
+// Output : ["((()))", "(()())", "(())()", "()(())", "()()()"]
 // Explanation : Need to generate a list of parantheses of that n size and inserted into the list.
 
 // Example 2:
 
-// Input: n = 1
-// Output: ["()"]
+// Input : n = 1
+// Output : ["()"]
 // Explanation : Need to generate a list of parantheses of that n size and inserted into the list.
 
 import java.util.ArrayList;
