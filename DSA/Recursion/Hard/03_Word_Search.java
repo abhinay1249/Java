774158e65bbcdd487b2c5779a1bd56189fe6516e
abhinay1249 <abhinaymarise@gmail.com>
2026-08-09 21:56:21 +0530

    Recursion -H -03

--- DSA/Recursion/Hard/03_Word_Search.java
+++ DSA/Recursion/Hard/03_Word_Search.java
@@ -1,4 +1,4 @@
-// Problem Statement: Given an m x n grid of characters board and a string word, return true if the word exists in the grid. 
+// Problem Statement: Given an M x N grid of characters board and a string word, return true if the word exists in the grid. 
 // The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. 
 // The same letter cell may not be used more than once.
 
@@ -6,7 +6,7 @@
 
 // Input : [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]] word = "ABCCED"
 // Output : true
-// Explanation: We can easily find the given word in the matrix.
+// Explanation : We can easily find the given word in the matrix.
 
 // Example 2:
 
