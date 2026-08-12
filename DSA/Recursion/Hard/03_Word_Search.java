d718c480a76f661b49920ea0a8b730297f406019
abhinay1249 <abhinaymarise@gmail.com>
2026-08-12 23:59:27 +0530

    Recursion - H -03

--- DSA/Recursion/Hard/03_Word_Search.java
+++ DSA/Recursion/Hard/03_Word_Search.java
@@ -2,7 +2,7 @@
 // The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. 
 // The same letter cell may not be used more than once.
 
-// Example 1:
+// Example 1 :
 
 // Input : [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]] word = "ABCCED"
 // Output : true
