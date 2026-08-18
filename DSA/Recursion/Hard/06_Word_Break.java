6883d6d45c52351d73d8702bcd24ff717d36b845
abhinay1249 <abhinaymarise@gmail.com>
2026-08-18 12:00:00 +0530

    Recursion

--- DSA/Recursion/Hard/06_Word_Break.java
+++ DSA/Recursion/Hard/06_Word_Break.java
@@ -1,6 +1,10 @@
 // Problem Statement : Given a string s and a dictionary of strings wordDict, 
 // return true if s can be segmented into a space-separated sequence of one or more dictionary words otherwise return false.
 
-public class 06_Word_Break {
-    
-}
+// Example 1
+
+// Input : s = "takeuforward" , wordDict = ["take" , "forward" , "you", "u"]
+
+// Output : true
+
+// Explanation : Return true because "takeuforward" can be segmented as "take" , "u" , "forward".
