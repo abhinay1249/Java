01b3a15c95a1133cff1787b1bd569349ae067c74
abhinay1249 <abhinaymarise@gmail.com>
2026-08-21 23:52:02 +0530

    Binary Search - 2D Arrays - 05

--- DSA/BinarySearch/2D-Arrays/05_MedianOfRowWiseSortedMatrix.java
+++ DSA/BinarySearch/2D-Arrays/05_MedianOfRowWiseSortedMatrix.java
@@ -10,7 +10,7 @@
 
 // Example 2:
 
-// Input: matrix = [ [1,2,3],[4,5,6],[7,8,9] ]
+// Input: matrix = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ]
 // Output: 5
 // Explanation: The sorted form of the matrix is [1,2,3,4,5,6,7,8,9]. So the median is 5.
 
