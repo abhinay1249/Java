112aef5b53f91aaf337ce862cc8c2fdf90fd9c16
abhinay1249 <abhinaymarise@gmail.com>
2026-08-25 23:57:30 +0530

    Recursion -H -06

--- DSA/Recursion/Hard/06_Word_Break.java
+++ DSA/Recursion/Hard/06_Word_Break.java
@@ -3,7 +3,7 @@
 
 // Example 1
 
-// Input : s = "takeuforward", wordDict = ["take", "forward" , "you", "u"]
+// Input : s = "takeuforward", wordDict = ["take", "forward", "you", "u"]
 // Output : true
 // Explanation : Return true because "takeuforward" can be segmented as "take" , "u" , "forward".
 
