eef9ef24bd7ddbf95d9bf613dbfefd6439b448b1
abhinay1249 <abhinaymarise@gmail.com>
2026-08-25 23:58:35 +0530

    Recursion -H -06

--- DSA/Recursion/Hard/06_Word_Break.java
+++ DSA/Recursion/Hard/06_Word_Break.java
@@ -1,5 +1,5 @@
 // Problem Statement : Given a string s and a dictionary of strings wordDict, 
-// return true if s can be segmented into a space-separated sequence of one or more dictionary words otherwise return false.
+// Return true if s can be segmented into a space-separated sequence of one or more dictionary words otherwise return false.
 
 // Example 1
 
