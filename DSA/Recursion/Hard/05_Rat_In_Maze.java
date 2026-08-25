6ee718de2bf9ea2ab8ae3bbf47c82d5d13469fc6
abhinay1249 <abhinaymarise@gmail.com>
2026-08-25 23:53:38 +0530

    Recursion -H -05

--- DSA/Recursion/Hard/05_Rat_In_Maze.java
+++ DSA/Recursion/Hard/05_Rat_In_Maze.java
@@ -13,6 +13,8 @@
 // The first path is (0, 0) => (1, 0) => (2, 0) => (2, 1) => (3, 1) => (3, 2) => (3, 3).
 // The second path is (0,0) => (1,0) => (1,1) => (2,1) => (3,1) => (3,2) => (3,3).
 
+// Example 2:
+
 import java.util.ArrayList;
 import java.util.List;
 
