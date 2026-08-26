d4008eb20670b21566ad85de9f02fb9c906fe28a
abhinay1249 <abhinaymarise@gmail.com>
2026-08-26 12:00:00 +0530

    Recursion -H -07

--- DSA/Recursion/Hard/07_M_Coloring.java
+++ DSA/Recursion/Hard/07_M_Coloring.java
@@ -2,7 +2,16 @@
 // such that no two adjacent vertices of the graph are colored with the same color.
 
 // Example 1:
+
 // Input: N = 4, M = 3, E = 5  
 // Edges[] = { (0, 1), (1, 2), (2, 3), (3, 0), (0, 2) }  
 // Output: 1  
 // Explanation: It is possible to color the given graph using 3 colors, so the answer is 1 (possible).
+
+// Example 2:
+
+Input: N = 3, M = 2, E = 3  
+// Edges[] = { (0, 1), (1, 2), (0, 2) }  
+// Output: 0  
+// Explanation: It is not possible to color the graph using 2 colors as it forms a triangle, which requires at least 3 colors.
+//  Hence, the answer is 0 (not possible).
