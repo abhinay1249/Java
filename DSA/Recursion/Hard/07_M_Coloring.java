c0ae918b91b44e7e9eb43068862e192d3b3cc362
abhinay1249 <abhinaymarise@gmail.com>
2026-08-26 12:00:00 +0530

    Recursion -H -07

--- DSA/Recursion/Hard/07_M_Coloring.java
+++ DSA/Recursion/Hard/07_M_Coloring.java
@@ -2,3 +2,13 @@
 // such that no two adjacent vertices of the graph are colored with the same color.
 
 // Example 1:
+// Input: N = 4, M = 3, E = 5  
+Edges[] = {  
+  (0, 1),  
+  (1, 2),  
+  (2, 3),  
+  (3, 0),  
+  (0, 2)  
+}  
+Output: 1  
+Explanation:
