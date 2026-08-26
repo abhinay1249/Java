a17e53285fccf065a2e2695f3753d49ced3e7056
abhinay1249 <abhinaymarise@gmail.com>
2026-08-26 23:59:12 +0530

    Recursion - H - 05

--- DSA/Recursion/Hard/08_Sudoku_Solver.java
+++ DSA/Recursion/Hard/08_Sudoku_Solver.java
@@ -1,4 +1,5 @@
-// Problem Statement: Create a program that fills in the blank cells in a Sudoku puzzle to solve it. Every sudoku solution needs to follow to these guidelines:
+// Problem Statement: Create a program that fills in the blank cells in a Sudoku puzzle to solve it.
+// Every sudoku solution needs to follow to these guidelines:
 // 1) In every row, the numbers 1 through 9 must appear exactly once.
 // 2) In every column, the numbers 1 through 9 must appear exactly once.
 // 3) In each of the grid's nine 3x3 sub-boxes, the numbers 1 through 9 must appear exactly once.
