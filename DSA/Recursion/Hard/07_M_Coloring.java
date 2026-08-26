b91f79d012e9381638c84733a17f3039284969bb
abhinay1249 <abhinaymarise@gmail.com>
2026-08-26 12:00:00 +0530

    Recursion -H -07

--- DSA/Recursion/Hard/07_M_Coloring.java
+++ DSA/Recursion/Hard/07_M_Coloring.java
@@ -3,12 +3,6 @@
 
 // Example 1:
 // Input: N = 4, M = 3, E = 5  
-Edges[] = {  
-  (0, 1),  
-  (1, 2),  
-  (2, 3),  
-  (3, 0),  
-  (0, 2)  
-}  
-Output: 1  
-Explanation:
+// Edges[] = { (0, 1), (1, 2), (2, 3), (3, 0), (0, 2) }  
+// Output: 1  
+// Explanation: It is possible to color the given graph using 3 colors, so the answer is 1 (possible).
