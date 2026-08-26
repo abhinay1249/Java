f8149012ba38d9851234f2590b01f51e7d11c57a
abhinay1249 <abhinaymarise@gmail.com>
2026-08-26 23:56:07 +0530

    Recursion - H - 05

--- DSA/Recursion/Hard/05_Rat_In_Maze.java
+++ DSA/Recursion/Hard/05_Rat_In_Maze.java
@@ -26,7 +26,7 @@ class Main{
 
     // ===================================== Only Approach ===========================================
 
-        private static void recursionPaths(int row, List<String> al, int[][] grid, int n){
+        private static void recursionPaths(int row, int col, List<String> al, int[][] grid, int n){
 
             if(row == n || col == n){
 
