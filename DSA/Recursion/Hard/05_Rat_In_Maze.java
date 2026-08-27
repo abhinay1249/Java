6b1d193fa5ece6b6f36631c580a0acd4ec11cff3
abhinay1249 <abhinaymarise@gmail.com>
2026-08-27 23:49:59 +0530

    Recursion - H -05

--- DSA/Recursion/Hard/05_Rat_In_Maze.java
+++ DSA/Recursion/Hard/05_Rat_In_Maze.java
@@ -26,22 +26,11 @@ class Main{
 
     // ===================================== Only Approach ===========================================
 
-        private static void recursionPaths(List<String> al, int[][] grid, int n, StringBuilder sb){
-
-
-        }
-
         public static List<String> ratInMaze(int n, int[][] grid){
 
             if(grid[0][0] == 0 || grid[n-1][n-1] == 0){
                 return new ArrayList<>();
             }
-
-            List<String> al = new ArrayList<>();
-
-            StringBuilder sb = new StringBuilder();
-
-            recursionPaths(al, grid, n, sb);
 
         }
 
