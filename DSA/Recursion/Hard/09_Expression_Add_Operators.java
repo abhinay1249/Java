ca3867287dc3e0b176d90c6d59a06bb65d994c7b
abhinay1249 <abhinaymarise@gmail.com>
2026-08-28 23:47:25 +0530

    Recursion - H - 09

--- DSA/Recursion/Hard/09_Expression_Add_Operators.java
+++ DSA/Recursion/Hard/09_Expression_Add_Operators.java
@@ -6,6 +6,21 @@
 
 // Note that a number can contain multiple digits.
 
+// Example 1:
+
+// Input: num = "123", target = 6
+// Output: ["1*2*3","1+2+3"]
+// Explanation: Both "1*2*3" and "1+2+3" evaluate to 6.
+
+Example 2:
+
+Input:
+ num = "232", target = 8
+Output:
+ ["2*3+2","2+3*2"]
+Explanation:
+Both "2*3+2" and "2+3*2" evaluate to 8.
+
 
 class Main{
     public static void main(String[] args) {
