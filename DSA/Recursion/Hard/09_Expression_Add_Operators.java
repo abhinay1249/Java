c7553624d69573397ca8e1719e14ee8a58addc81
abhinay1249 <abhinaymarise@gmail.com>
2026-08-28 23:32:17 +0530

    Recursion - H - 05

--- DSA/Recursion/Hard/09_Expression_Add_Operators.java
+++ DSA/Recursion/Hard/09_Expression_Add_Operators.java
@@ -1,3 +1,5 @@
-public class 09_Expression_Add_Operator {
-    
-}
+// Problem Statement: Given a string num that contains only digits and an integer target, return all possibilities to insert the binary operators '+', '-', and/or '*' between the digits of num so that the resultant expression evaluates to the target value.
+
+// Note that operands in the returned expressions should not contain leading zeros.
+
+// Note that a number can contain multiple digits.
