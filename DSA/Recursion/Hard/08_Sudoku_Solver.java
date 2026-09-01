75d8c0d73347cb6d48d74da33b2806f960a5b8c1
abhinay1249 <abhinaymarise@gmail.com>
2026-09-01 23:48:26 +0530

    Recursion - H - 08

--- DSA/Recursion/Hard/08_Sudoku_Solver.java
+++ DSA/Recursion/Hard/08_Sudoku_Solver.java
@@ -26,7 +26,13 @@ class Main{
 
         for(int row = 0 ; row < board.length ; row++){
             for(int col = 0 ; col < board[0].length ; col++){
-                
+
+                if(board[row][col] == '.'){
+                    for(char num = '1' ; num < '9' ; num++){
+                        
+                    }
+                }
+
             }
         }
 
