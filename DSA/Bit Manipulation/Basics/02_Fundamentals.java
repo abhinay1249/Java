2f044c7bb1851291f06545838514beda85515aac
abhinay1249 <abhinaymarise@gmail.com>
2026-09-05 16:14:31 +0530

    Bit Manipulation - Basics - 02

---
+++ DSA/Bit Manipulation/Basics/02_Fundamentals.java
@@ -0,0 +1,7 @@
+// Problem Statement : Given below there are few set of fundamental operations performed using Bit Manipulation.
+
+// Example 1:
+
+// Input : a = 6, b= 5
+// Output : a = 5, b = 6
+// Explanation : Swapped using Bit Manipulation
