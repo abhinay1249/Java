99a6863864fd8af6e15e4aa1c40dc43c483e89ba
abhinay1249 <abhinaymarise@gmail.com>
2026-09-05 16:20:53 +0530

    Bit Manipulation - Basics - 02

--- DSA/Bit Manipulation/Basics/02_Fundamentals.java
+++ DSA/Bit Manipulation/Basics/02_Fundamentals.java
@@ -5,3 +5,27 @@
 // Input : a = 6, b= 5
 // Output : a = 5, b = 6
 // Explanation : Swapped using Bit Manipulation
+
+class Main{
+
+    public static void swapTwoNumbers(int num1, int num2){
+
+        num1 = num1 ^ num2;
+        num2 = num1 ^ num2;
+        num1 = num1 ^ num2;
+
+        System.out.println(num1);
+        System.out.println(num2);
+
+    }
+
+
+    public static void main(String[] args) {
+
+        int num_1 = 2;
+        int num_2 = 10;
+        
+        swapTwoNumbers(num_1, num_2);
+
+    }
+}
