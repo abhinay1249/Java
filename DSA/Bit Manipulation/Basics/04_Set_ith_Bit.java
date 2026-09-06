1e96ed630b263d4b8894abe93a908fe26b812324
abhinay1249 <abhinaymarise@gmail.com>
2026-09-06 09:15:22 +0530

    Bit Manipulation - Basics - 04

---
+++ DSA/Bit Manipulation/Basics/04_Set_ith_Bit.java
@@ -0,0 +1,15 @@
+// Problem Statement: Given two integers n and i, Make sure that the respective ith Bit need to be set.
+
+// Example 1:
+
+// Input : N = 10, i = 0
+// Output : 1011
+// Explanation : Initially the 0th bit will be 0 zero for 10, after making the 0th bit to be set then it will be 11 in decimal.
+
+// Example 2:
+
+// Input : N = 13, i = 1
+// Ouput : 1111
+// Explanation : Initially the ith bit will be 0 zero for 13, after making the ith bit to be set then it will be 15 in decimal.
+
+
