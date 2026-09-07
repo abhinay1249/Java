a8add593addc62cde7e8b3499dc5ec2839df78d8
abhinay1249 <abhinaymarise@gmail.com>
2026-09-07 07:32:01 +0530

    Bit Manipulation - Basics - 05

---
+++ DSA/Bit Manipulation/Basics/05_Clear_ith_Bit.java
@@ -0,0 +1,15 @@
+// Problem Statement: Given two integers n and i, Make sure that the respective ith Bit need to be clear(need to be zero).
+
+// Example 1:
+
+// Input : N = 10, i = 1
+// Output : 1000
+// Explanation : Initially the 1th bit will be 1 for 10, after making the 1th bit to be clear then it will be 8 in decimal.
+
+// Example 2:
+
+// Input : N = 13, i = 2
+// Ouput : 1001
+// Explanation : Initially the ith bit will be 1 zero for 13, after making the ith bit to be clear then it will be 9 in decimal.
+
+
