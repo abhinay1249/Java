7bba13a14d06f3461599017bc4fa81db7fcd8b89
abhinay1249 <abhinaymarise@gmail.com>
2026-09-07 23:10:34 +0530

    Bit Manipulation - Basics - 06

---
+++ DSA/Bit Manipulation/Basics/06_Toggle_ith_Bit.java
@@ -0,0 +1,13 @@
+// Problem Statement: Given two integers n and i, Make sure that the respective ith Bit need to be toggle(change to other value).
+
+// Example 1:
+
+// Input : N = 10, i = 1
+// Output : 1000
+// Explanation : Initially the 1th bit will be 1 for 10, after making the 1th bit to toggle then it will be 0.
+
+// Example 2:
+
+// Input : N = 13, i = 2
+// Ouput : 1001
+// Explanation : Initially the ith bit will be one, after making the ith bit to toggle then it will be 0.
