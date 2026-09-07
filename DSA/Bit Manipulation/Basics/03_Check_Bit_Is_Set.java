2657db80bb599f944b5481ca5c1202e61ba36bc1
abhinay1249 <abhinaymarise@gmail.com>
2026-09-07 23:40:24 +0530

    Bit Manipulation - Basics - 02

--- DSA/Bit Manipulation/Basics/03_Check_Bit_Is_Set.java
+++ DSA/Bit Manipulation/Basics/03_Check_Bit_Is_Set.java
@@ -3,15 +3,15 @@
 
 // Example 1:
 
-// Input: n = 5, i = 0
-// Output: true
-// Explanation: Binary representation of 5 is 101. The 0-th bit from LSB is set (1).
+// Input : n = 5, i = 0
+// Output : true
+// Explanation : Binary representation of 5 is 101. The 0-th bit from LSB is set (1).
 
 // Example 2:
 
-// Input: n = 10, i = 1
-// Output: true
-// Explanation: Binary representation of 10 is 1010. The 1-st bit from LSB is set (1).
+// Input : n = 10, i = 1
+// Output : true
+// Explanation : Binary representation of 10 is 1010. The 1-st bit from LSB is set (1).
 
 class Main {
 
