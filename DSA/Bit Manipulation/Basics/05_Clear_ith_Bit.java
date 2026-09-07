bad7e29a9b6ea8f897c2cbcfe34593de73b3b617
abhinay1249 <abhinaymarise@gmail.com>
2026-09-07 07:42:38 +0530

    Bit Manipulation - Basics - 05

--- DSA/Bit Manipulation/Basics/05_Clear_ith_Bit.java
+++ DSA/Bit Manipulation/Basics/05_Clear_ith_Bit.java
@@ -12,4 +12,81 @@
 // Ouput : 1001
 // Explanation : Initially the ith bit will be 1 zero for 13, after making the ith bit to be clear then it will be 9 in decimal.
 
+class Main {
 
+    private static String reverseString(String str){
+
+        StringBuilder sb = new StringBuilder(str);
+
+        int start = 0, end = str.length()-1;
+
+        while(start < end){
+            char temp = sb.charAt(start);
+
+            sb.setCharAt(start, sb.charAt(end));
+            sb.setCharAt(end, temp);
+
+            start++;
+            end--;
+        }
+
+        return sb.toString();
+    }
+    
+
+    private static String convertIntoBinary(int number){
+
+        StringBuilder sb = new StringBuilder();
+
+        while(number != 0){
+
+            if(number % 2 == 1){
+                sb.append("1");
+            }else{
+                sb.append("0");
+            }
+            number/=2;
+
+        }
+
+        return reverseString(sb.toString());
+
+    }
+
+    public static String clearithBit(int number, int bitNumber){
+
+        String binaryNumber = convertIntoBinary(number);
+
+        StringBuilder sb = new StringBuilder(binaryNumber);
+
+        int length = binaryNumber.length()-1;
+
+        for(int index = length ; index >= 0; index--){
+            if((length - index) == bitNumber){
+                if(sb.charAt(index) == '1'){
+                    sb.setCharAt(index, '0');
+                }else{
+                    break;
+                }
+            }
+        }
+
+        return sb.toString();
+
+    }
+    
+    
+    public static void main(String[] args) {
+        
+        int number = 13;
+        int bitNumber = 0;
+
+        System.out.println(convertIntoBinary(number));
+
+        String num1 = clearithBit(number, bitNumber);
+
+        System.out.println(num1);
+
+
+    }
+}
