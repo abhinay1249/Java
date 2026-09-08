54a5cf70297765f5f31af4e8940d64d7eebb8644
abhinay1249 <abhinaymarise@gmail.com>
2026-09-08 07:18:27 +0530

    Bit Manipulation - Basics - 07

---
+++ DSA/Bit Manipulation/Basics/07_Remove_the_LastSet_Bit.java
@@ -0,0 +1,78 @@
+// Problem Statement : Given the problem statement, Where the respective right most set bit is removed(changed to zero)
+
+class Main {
+
+    
+
+        private static String reverseString(String str){
+
+            StringBuilder sb = new StringBuilder(str);
+
+            int start = 0, end = str.length()-1;
+
+            while(start < end){
+                char temp = sb.charAt(start);
+
+                sb.setCharAt(start, sb.charAt(end));
+                sb.setCharAt(end, temp);
+
+                start++;
+                end--;
+            }
+
+            return sb.toString();
+
+        }
+
+        private static String convertIntoBinary(int number){
+
+            StringBuilder sb = new StringBuilder();
+
+            while(number != 0){
+
+                if(number % 2 == 1){
+                    sb.append("1");                                     
+                }else{
+                    sb.append("0");
+                }
+                number/=2;
+
+            }
+
+            return reverseString(sb.toString());
+
+        }
+
+        public static String removeTheLastSetBit(int number){
+
+            String binaryNumber = convertIntoBinary(number);
+            
+            StringBuilder sb = new StringBuilder(binaryNumber);
+
+            int length = binaryNumber.length()-1;
+
+            for(int index = length ; index >= 0 ; index--){
+                if(binaryNumber.charAt(index) == '1'){
+                    sb.setCharAt(index, '0');
+                    break;
+                }
+            }
+
+            return sb.toString();
+
+        }
+
+
+    public static void main(String args[]){
+
+        int number = 8;
+
+        System.out.println(convertIntoBinary(number));
+
+        String result = removeTheLastSetBit(number);
+
+        System.out.println(result);
+
+
+    }
+}
