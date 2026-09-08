ae54ef495655c8da7e6be98e68c9bc0829b791b8
abhinay1249 <abhinaymarise@gmail.com>
2026-09-08 08:13:37 +0530

    Bit Manipulation - Basics - 08

--- DSA/Bit Manipulation/Basics/08_Check_Number_Is_Power_Of_2.java
+++ DSA/Bit Manipulation/Basics/08_Check_Number_Is_Power_Of_2.java
@@ -1,1 +1,88 @@
+// Problem Statement: Given an integer n, return true if it is a power of two.
+// Otherwise, return false. An integer n is a power of two if there exists an integer x such that n == 2ˣ.
 
+// Example 1:
+
+// Input : n = 16
+// Output : true
+// Explanation : 2⁴ = 16, so 16 is a power of two.
+
+// Example 2:
+
+// Input : n = 3
+// Output: false
+// Explanation : 3 is not a power of two.
+
+class Main {
+
+    // ============================== Brute Force Approach =======================================
+
+        private static String reverseString(String str){
+
+            StringBuilder sb = new StringBuilder(str);
+
+            int start = 0, end = str.length()-1;
+
+            while(start < end){
+                char temp = sb.charAt(start);
+
+                sb.setCharAt(start, sb.charAt(end));
+                sb.setCharAt(end, temp);
+
+                start++;
+                end--;
+            }
+
+            return sb.toString();
+
+        }
+
+        private static String convertIntoBinary(int number){
+
+            StringBuilder sb = new StringBuilder();
+
+            while(number != 0){
+
+                if(number % 2 == 1){
+                    sb.append("1");                                     
+                }else{
+                    sb.append("0");
+                }
+                number/=2;
+
+            }
+
+            return reverseString(sb.toString());                            
+
+        }
+
+        public static boolean checkNumber(int number){
+
+            String binaryNumber = convertIntoBinary(number);
+
+            int length = binaryNumber.length()-1;
+
+            int counter = 0;
+
+            for(int index = length ; index >= 0 ; index--){
+                if(binaryNumber.charAt(index) == '1'){
+                    counter++;
+                }
+            }
+
+            if(counter == 1) return true;
+
+            return false;
+
+        }
+
+    public static void main(String args[]){
+
+        int number = 13;
+
+        boolean result = checkNumber(number);
+
+        System.out.println(result);
+
+    }
+}
