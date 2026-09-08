3da17e9eaeb712ef53d8f439d484947d08d7d5a8
abhinay1249 <abhinaymarise@gmail.com>
2026-09-08 22:46:25 +0530

    Bit Manipulation - Basics - 10

---
+++ DSA/Bit Manipulation/Basics/10_Check_Odd_Even.java
@@ -0,0 +1,15 @@
+// Problem Statement: Given a non-negative integer n, determine whether it is odd. 
+// Return true if the number is odd, otherwise return false. A number is odd if it is not divisible by 2 (i.e., n % 2 != 0).
+
+// Example 1:
+
+// Input : n = 7
+// Output : true
+// Explanation : 7 is not divisible by 2. Hence, it is odd.
+
+// Example 2:
+
+// Input : n = 10
+// Output : false
+// Explanation : 10 is divisible by 2. Hence, it is not odd.
+
