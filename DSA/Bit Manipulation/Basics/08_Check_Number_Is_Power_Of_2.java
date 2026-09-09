f6424f6684dda0d6ca20eaae57782a9688edaef5
abhinay1249 <abhinaymarise@gmail.com>
2026-09-09 17:32:15 +0530

    Bit Manipulation - Basics - 08

--- DSA/Bit Manipulation/Basics/08_Check_Number_Is_Power_Of_2.java
+++ DSA/Bit Manipulation/Basics/08_Check_Number_Is_Power_Of_2.java
@@ -1,5 +1,5 @@
-// Problem Statement: Given an integer n, return true if it is a power of two.
-// Otherwise, return false. An integer n is a power of two if there exists an integer x such that n == 2ˣ.
+// Problem Statement: Given an integer n, return true if it is a power of two. Otherwise, return false.
+// An integer n is a power of two if there exists an integer x such that n == 2ˣ.
 
 // Example 1:
 
