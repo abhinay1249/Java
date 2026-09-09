617d09bd6b1c85c001ae627c38522b04ae965833
abhinay1249 <abhinaymarise@gmail.com>
2026-09-09 17:30:14 +0530

    Bit Manipulation - Basics - 09

--- DSA/Bit Manipulation/Basics/09_Count_Number_Of_Set_Bits.java
+++ DSA/Bit Manipulation/Basics/09_Count_Number_Of_Set_Bits.java
@@ -3,15 +3,15 @@
 
 // Example 1:
 
-// Input: n = 5
-// Output: 2
-// Explanation: The binary representation of 5 is 101, which has 2 set bits.
+// Input : n = 5
+// Output : 2
+// Explanation : The binary representation of 5 is 101, which has 2 set bits.
 
 // Example 2:
 
-// Input: n = 15
-// Output: 4
-// Explanation: The binary representation of 15 is 1111, which has 4 set bits.
+// Input : n = 15
+// Output : 4
+// Explanation : The binary representation of 15 is 1111, which has 4 set bits.
 
 class Main {
 
