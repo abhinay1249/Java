fbba6369fe6907e14247de0555e697f7db454bef
abhinay1249 <abhinaymarise@gmail.com>
2026-09-10 23:12:12 +0530

    Bit Manipulation - Medium - 01

--- DSA/Bit Manipulation/Medium/01_Divide_Without_Operators.java
+++ DSA/Bit Manipulation/Medium/01_Divide_Without_Operators.java
@@ -4,3 +4,7 @@
 // As an illustration, 8.345 and -2.7335 would be reduced to 8 and -2 respectively.
 // Note: Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231, 231 − 1]. 
 // For this problem, if the quotient is strictly greater than 231 - 1, then return 231 - 1, and if the quotient is strictly less than -231, then return -231.
+
+class Main {
+    
+}
