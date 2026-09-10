6bb039f7a158334d0fa52b0dfa217dc6020a910b
abhinay1249 <abhinaymarise@gmail.com>
2026-09-10 23:15:27 +0530

    Bit Manipulation - Medium - 01

--- DSA/Bit Manipulation/Medium/01_Divide_Without_Operators.java
+++ DSA/Bit Manipulation/Medium/01_Divide_Without_Operators.java
@@ -5,9 +5,25 @@
 // Note: Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231, 231 − 1]. 
 // For this problem, if the quotient is strictly greater than 231 - 1, then return 231 - 1, and if the quotient is strictly less than -231, then return -231.
 
+// Example 1:
+
+// Input: Dividend = 10, Divisor = 3
+// Output: 3
+// Explanation: 10/3 = 3.33, truncated to 3.
+
+// Example 2:
+// Input:
+//  Dividend = 7, Divisor = -3
+// Output:
+//  -2
+// Explanation:
+//  7/-3 = -2.33, truncated to -2.
+
+
+
 class Main {
 
-    
+
 
 
     public static void main(String[] args) {
