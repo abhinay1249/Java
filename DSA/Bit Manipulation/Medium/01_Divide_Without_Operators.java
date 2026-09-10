34a937be37d9720b9bf0fc09c287f6db29e59443
abhinay1249 <abhinaymarise@gmail.com>
2026-09-10 23:12:01 +0530

    Bit Manipulation - Medium - 01

---
+++ DSA/Bit Manipulation/Medium/01_Divide_Without_Operators.java
@@ -0,0 +1,6 @@
+// Problem Statement: Given the two integers, dividend and divisor. 
+// Divide without using the mod, division, or multiplication operators and return the quotient.
+// The fractional portion of the integer division should be lost as it truncates toward zero.
+// As an illustration, 8.345 and -2.7335 would be reduced to 8 and -2 respectively.
+// Note: Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231, 231 − 1]. 
+// For this problem, if the quotient is strictly greater than 231 - 1, then return 231 - 1, and if the quotient is strictly less than -231, then return -231.
