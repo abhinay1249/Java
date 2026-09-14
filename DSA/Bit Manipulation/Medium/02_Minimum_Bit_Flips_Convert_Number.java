41c42cf934d94283248acf7451f3cc5fd57c5f8b
abhinay1249 <abhinaymarise@gmail.com>
2026-09-14 23:54:39 +0530

    Bit Manipulation - Medium - 01

--- DSA/Bit Manipulation/Medium/02_Minimum_Bit_Flips_Convert_Number.java
+++ DSA/Bit Manipulation/Medium/02_Minimum_Bit_Flips_Convert_Number.java
@@ -1,1 +1,2 @@
-
+// Problem Statement: Given two integers start and goal. Flip the minimum number of bits of start integer to convert it into Integer.
+// A bits flip in the number val is to choose any bit in binary representation of val and flipping it from either 0 to 1 or 1 to 0.
