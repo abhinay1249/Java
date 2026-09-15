0862aeb20c32a27a6e7f2357d95439af165e3ab0
abhinay1249 <abhinaymarise@gmail.com>
2026-09-15 23:54:38 +0530

    Bit Manipulation - Medium -02

--- DSA/Bit Manipulation/Medium/02_Minimum_Bit_Flips_Convert_Number.java
+++ DSA/Bit Manipulation/Medium/02_Minimum_Bit_Flips_Convert_Number.java
@@ -19,9 +19,9 @@ class Main {
 
     // =================================== Approach ============================================
 
-    public static int minimumBitFlips(int start, int goal){
-
-    }
+        public static int minimumBitFlips(int start, int goal){
+        
+        }
 
     public static void main(String[] args) {
         
