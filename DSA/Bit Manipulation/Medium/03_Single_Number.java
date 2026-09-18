25243ecf1d8caae4bd3267958dea9a512c7a2aac
abhinay1249 <abhinaymarise@gmail.com>
2026-09-18 23:49:59 +0530

    Bit Manipulation - Medium - 03

--- DSA/Bit Manipulation/Medium/03_Single_Number.java
+++ DSA/Bit Manipulation/Medium/03_Single_Number.java
@@ -2,13 +2,13 @@
 
 // Example 1:
 
-// Input Format: arr[] = {2,2,1}
+// Input : arr[] = {2,2,1}
 // Result: 1
 // Explanation: In this array, only the element 1 appear once and so it is the answer.
 
 // Example 2:
 
-// Input Format: arr[] = {4,1,2,1,2}
+// Input : arr[] = {4,1,2,1,2}
 // Result: 4
 // Explanation: In this array, only element 4 appear once and the other elements appear twice. So, 4 is the answer.
 
