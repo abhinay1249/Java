d02983d09537f07835c951b5bd8cd3fc1e3778c6
abhinay1249 <abhinaymarise@gmail.com>
2026-09-23 00:01:27 +0530

    Bit Manipulation - Medium - 07

--- DSA/Bit Manipulation/Medium/07_Single_Number-3.java
+++ DSA/Bit Manipulation/Medium/07_Single_Number-3.java
@@ -13,13 +13,13 @@
 
 // Input : nums = [-1,0]
 // Output : [-1,0]
-// Explanation:  [-1, 0] is also a valid answer.
+// Explanation :  [-1, 0] is also a valid answer.
 
 // Example 3:
 
-// Input: nums = [0,1]
-// Output: [1,0]
-// Explanation:  [1, 0] is also a valid answer.
+// Input : nums = [0,1]
+// Output : [1,0]
+// Explanation :  [1, 0] is also a valid answer.
 
 class Main {
 
