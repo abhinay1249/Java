ec83946165142623e5d853344fcf86a678ad09b8
abhinay1249 <abhinaymarise@gmail.com>
2026-09-23 00:03:17 +0530

    Bit Manipulation - Medium - 04

--- DSA/Bit Manipulation/Medium/04_Power_Set.java
+++ DSA/Bit Manipulation/Medium/04_Power_Set.java
@@ -2,17 +2,17 @@
 
 // Example 1:
 
-// Input: nums = [1, 2, 3]
-// Output: [[ ], [1], [2], [3], [1, 2], [2, 3], [3, 1], [1, 2, 3]]
-// Explanation:  A power set of an array is an array of all possible subarrays, including an empty array.
+// Input : nums = [1, 2, 3]
+// Output : [[ ], [1], [2], [3], [1, 2], [2, 3], [3, 1], [1, 2, 3]]
+// Explanation :  A power set of an array is an array of all possible subarrays, including an empty array.
 // It contains all combinations of elements from the original array.
 // By iterating through all possible combinations of the elements in the input array we are able to get the power set of the array.
 
 // Example 2:
 
-// Input: nums = [5, 7, 8]
-// Output: [[ ], [5], [7], [8], [5, 7], [7, 8], [8, 5], [5, 7, 8]]
-// Explanation:  A power set of an array is an array of all possible subarrays, including an empty array.
+// Input : nums = [5, 7, 8]
+// Output : [[ ], [5], [7], [8], [5, 7], [7, 8], [8, 5], [5, 7, 8]]
+// Explanation :  A power set of an array is an array of all possible subarrays, including an empty array.
 // It contains all combinations of elements from the original array.
 // By iterating through all possible combinations of the elements in the input array we are able to get the power set of the array.
 
