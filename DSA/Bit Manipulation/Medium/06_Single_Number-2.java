4797c0ef39a53401847df466a225206194e9e033
abhinay1249 <abhinaymarise@gmail.com>
2026-09-22 23:26:44 +0530

    Bit Manipulation - Medium - 06

--- DSA/Bit Manipulation/Medium/06_Single_Number-2.java
+++ DSA/Bit Manipulation/Medium/06_Single_Number-2.java
@@ -93,7 +93,7 @@ class Main {
             int twos = 0;
 
             for(int index = 0 ; index < nums.length ; index++){
-                ones = (ones ^ nums[index]) & (~twos);                          // T.C = O(), S.C = O()
+                ones = (ones ^ nums[index]) & (~twos);                          // T.C = O(N), S.C = O(1)
                 twos = (twos ^ nums[index]) & (~ones);
             }
 
